1297703490a68f73ce66c07e4636fc4f46640b09
Ivan Dalmet <ivan@dalmet.fr>
2020-11-04 11:44:26 +0100

    fix: storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
