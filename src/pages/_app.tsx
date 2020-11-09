910f7a9fd358850198994cde0f3fd8c7bf70bcc5
Ivan Dalmet <ivan@dalmet.fr>
2020-11-09 17:16:06 +0100

    feat: Add Error boundary on next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Providers } from '@/Providers';
 import { Viewport } from '@/components/Viewport';
+import { ErrorBoundary } from '@/errors';
 
 const App = ({ Component, pageProps }) => {
   return (
     <Providers>
-      <Viewport>
-        <Component {...pageProps} />
-      </Viewport>
+      <ErrorBoundary>
+        <Viewport>
+          <Component {...pageProps} />
+        </Viewport>
+      </ErrorBoundary>
     </Providers>
   );
 };
