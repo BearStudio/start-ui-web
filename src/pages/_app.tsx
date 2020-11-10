dc0bbf149277825c6125a4b6ff1708e5a4707f2a
Ivan Dalmet <ivan@dalmet.fr>
2020-11-10 13:06:23 +0100

    feat: Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import Head from 'next/head';
+import { useTheme } from '@chakra-ui/core';
 import { Providers } from '@/Providers';
 import { Viewport } from '@/components/Viewport';
 import { ErrorBoundary } from '@/errors';
 
+const AppHead = () => {
+  const theme = useTheme();
+
+  return (
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/safari-pinned-tab.svg"
+        color={theme.colors.brand?.['600']}
+      />
+      <meta
+        name="msapplication-TileColor"
+        content={theme.colors.brand?.['600']}
+      />
+      <meta name="theme-color" content={theme.colors.brand?.['600']} />
+    </Head>
+  );
+};
+
 const App = ({ Component, pageProps }) => {
   return (
     <Providers>
+      <AppHead />
       <ErrorBoundary>
         <Viewport>
           <Component {...pageProps} />
