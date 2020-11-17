44e60f5ca396a6cf57519bfc09b4e1c96615c5e7
Ivan Dalmet <ivan@dalmet.fr>
2020-11-17 10:06:33 +0100

    fix: Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@emotion/core": toPath("node_modules/@emotion/react"),
+      "emotion-theming": toPath("node_modules/@emotion/react"),
+    };
     return config;
   },
 };
