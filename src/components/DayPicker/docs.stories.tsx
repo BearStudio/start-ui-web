c4b622115c2154c530e0574a1780dab4419a91ad
amineziraoui <amine@bearstudio.fr>
2020-12-28 18:07:04 +0100

    Fix feedbacks mdx

---
+++ src/components/DayPicker/docs.stories.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+
+import { DayPicker } from '.';
+
+export default {
+  title: 'components/Daypicker',
+};
+export const Default = () => (
+  <Box name="Basic" height="400px">
+    <DayPicker />
+  </Box>
+);
