24aa48c7cb162d769b1017fb419d25155ca03100
Ivan Dalmet <ivan@dalmet.fr>
2020-12-28 12:31:53 +0100

    fix: Use page component on entity example

--- src/app/entity/PageEntityCreate.tsx
+++ src/app/entity/PageEntityCreate.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Box, Heading, HStack, IconButton } from '@chakra-ui/react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
 
-import { Page, PageBody, PageHeader } from '@/app/layout';
+import { Page, PageBody, PageFooter, PageHeader } from '@/app/layout';
 
 export const PageEntityCreate = () => {
+  const history = useHistory();
   return (
-    <Page>
-      <PageHeader>PageEntityCreate Component</PageHeader>
+    <Page containerSize="md" isFocusMode>
+      <PageHeader>
+        <HStack spacing="4">
+          <Box ml={{ base: 0, lg: '-3.5rem' }}>
+            <IconButton
+              aria-label="Go Back"
+              icon={<FiArrowLeft fontSize="lg" />}
+              variant="ghost"
+              onClick={() => history.goBack()}
+            />
+          </Box>
+          <Box flex="1">
+            <Heading size="sm">PageEntityCreate Component</Heading>
+          </Box>
+        </HStack>
+      </PageHeader>
       <PageBody>
         <Link to="/entity">Go to Entity</Link>
       </PageBody>
+      <PageFooter>Footer</PageFooter>
     </Page>
   );
 };
--- src/app/entity/PageEntityList.tsx
+++ src/app/entity/PageEntityList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Heading } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 import { Page, PageBody, PageHeader } from '@/app/layout';
@@ -7,7 +8,9 @@ import { Page, PageBody, PageHeader } from '@/app/layout';
 export const PageEntityList = () => {
   return (
     <Page>
-      <PageHeader>PageEntityList Component</PageHeader>
+      <PageHeader>
+        <Heading size="md">PageEntityList Component</Heading>
+      </PageHeader>
       <PageBody>
         <Link to="/entity/create">Create Entity</Link>
       </PageBody>
