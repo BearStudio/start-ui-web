da9f3da06554f896d359cb5580e403ae09ac18ff
Ivan Dalmet <ivan@dalmet.fr>
2021-02-17 13:58:41 +0100

    feat: Add basic styleguide with custom buttons

---
+++ src/theme/components/button.ts
@@ -0,0 +1,43 @@
+export default {
+  variants: {
+    // Custom variants
+    primary: () => ({
+      color: 'white',
+      bg: 'brand.600',
+      _hover: {
+        bg: 'brand.700',
+      },
+    }),
+    secondary: () => ({
+      color: 'brand.600',
+      bg: 'brand.50',
+      _hover: {
+        bg: 'brand.100',
+        color: 'brand.700',
+      },
+    }),
+    danger: () => ({
+      color: 'error.600',
+      bg: 'error.50',
+      _hover: {
+        bg: 'error.100',
+        color: 'error.700',
+      },
+    }),
+
+    // Default variants
+    solid: ({ colorScheme }) => ({
+      bg: colorScheme === 'gray' ? `${colorScheme}.100` : `${colorScheme}.600`,
+      _hover: {
+        bg:
+          colorScheme === 'gray' ? `${colorScheme}.200` : `${colorScheme}.700`,
+      },
+    }),
+    ghost: ({ colorScheme }) => ({
+      bg: `${colorScheme}.50`,
+      _hover: {
+        bg: `${colorScheme}.100`,
+      },
+    }),
+  },
+};
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Badge } from './badge';
+export { default as Button } from './button';
 export { default as Input } from './input';
 export { default as Select } from './select';
 export { default as Textarea } from './textarea';
