ad9ab3d01a0bd533c8c83c26add0ef347f5e8810
Ivan Dalmet <ivan@dalmet.fr>
2021-02-17 13:38:34 +0100

    feat: New 403 page

--- src/errors/Error403.tsx
+++ src/errors/Error403.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import { Button, Stack, Center, Heading, Text } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
+
 export const Error403 = () => {
-  return <>Error403 Component</>;
+  const history = useHistory();
+  return (
+    <Center flex="1" p="8">
+      <Stack align="center" textAlign="center">
+        <Heading>Error 403</Heading>
+        <Text color="gray.600">Sorry, you can't access this page.</Text>
+        <Button onClick={() => history.goBack()}>Go Back</Button>
+      </Stack>
+    </Center>
+  );
 };
