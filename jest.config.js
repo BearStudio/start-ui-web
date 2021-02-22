c95153f4749091effc37623ca6658edfaafa2476
Jessy BAER <jessy@bearstudio.fr>
2021-02-22 10:18:07 +0100

    Fix: Add mock in test for external css

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
   },
 };
