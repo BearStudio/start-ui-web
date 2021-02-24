fe68425fd2b99e88e12b74d98f54612438746041
Ivan Dalmet <ivan@dalmet.fr>
2021-02-24 11:05:33 +0100

    fix: Tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/test/styleMock.js',
   },
 };
--- src/__tests__/app/App.spec.tsx
+++ src/__tests__/app/App.spec.tsx
@@ -1,8 +1,14 @@
 import { App } from '@/app/App';
 import { render, screen, act } from '@/test/utils';
 
+beforeEach(() => {
+  window.history.pushState({}, '', '/app');
+});
+
 test('Mount App without errors', async () => {
-  await act(async () => render(<App />));
+  await act(async () => {
+    render(<App />);
+  });
   expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /log in/i })).toBeInTheDocument();
 });
