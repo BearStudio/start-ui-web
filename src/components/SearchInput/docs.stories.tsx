fffb9906d0036a5fe86dde727fdeae946f1a6bfc
Ivan Dalmet <ivan@dalmet.fr>
2021-03-16 15:26:51 +0100

    feat: SearchInput component

---
+++ src/components/SearchInput/docs.stories.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import { Stack, Text } from '@chakra-ui/react';
+
+import { SearchInput } from '.';
+
+export default {
+  title: 'Components/SearchInput',
+};
+
+export const Uncontrolled = () => {
+  return <SearchInput onChange={(value) => console.log(value)} />;
+};
+
+export const Controlled = () => {
+  const [value, setValue] = useState('');
+  return (
+    <Stack spacing={4}>
+      <SearchInput value={value} onChange={setValue} />
+      <Text>{value}</Text>
+    </Stack>
+  );
+};
+
+export const Disabled = () => {
+  const [value, setValue] = useState('Search term');
+  return <SearchInput value={value} onChange={setValue} isDisabled />;
+};
+
+export const DebounceDelay = () => {
+  const [value, setValue] = useState('');
+  return (
+    <Stack spacing={4}>
+      <SearchInput value={value} onChange={setValue} delay={1000} />
+      <Text>{value}</Text>
+    </Stack>
+  );
+};
---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import {
+  forwardRef,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+  IconButton,
+  useControllableState,
+} from '@chakra-ui/react';
+import { FiSearch, FiX } from 'react-icons/fi';
+
+import mergeRefs from '@/utils/mergeRefs';
+
+interface SearchInputProps extends Omit<InputProps, 'onChange'> {
+  onChange?(value?: string): void;
+  delay?: number;
+}
+
+export const SearchInput = forwardRef<SearchInputProps, 'input'>(
+  (
+    {
+      value,
+      defaultValue,
+      onChange,
+      delay = 500,
+      placeholder = 'Rechercher...',
+      isDisabled = false,
+      ...rest
+    },
+    ref
+  ) => {
+    const [externalValue, setExternalValue]: any = useControllableState({
+      name: 'SearchInput',
+      value,
+      defaultValue,
+      onChange,
+    });
+    const inputRef = useRef<HTMLInputElement>();
+
+    const [search, setSearch] = useState<string>(externalValue as string);
+
+    const searchRef = useRef(search);
+    searchRef.current = search;
+
+    const setExternalValueRef = useRef<SearchInputProps['onChange']>();
+    setExternalValueRef.current = setExternalValue;
+
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setExternalValueRef.current(search);
+      }, delay);
+
+      return () => clearTimeout(handler);
+    }, [search, delay]);
+
+    useEffect(() => {
+      if (externalValue !== searchRef.current) {
+        setSearch(externalValue);
+      }
+    }, [externalValue]);
+
+    const handleChange = (event) => {
+      setSearch(event.target.value);
+    };
+
+    const handleClear = () => {
+      setSearch('');
+      inputRef?.current?.focus();
+    };
+
+    const handleEscape = (event) => {
+      if (event?.key?.toLowerCase() === 'escape') {
+        handleClear();
+      }
+    };
+
+    return (
+      <InputGroup {...rest}>
+        <Input
+          ref={mergeRefs([inputRef, ref])}
+          onChange={handleChange}
+          value={search}
+          placeholder={placeholder}
+          isDisabled={isDisabled}
+          onKeyDown={handleEscape}
+        />
+        <InputRightElement color={isDisabled ? 'gray.300' : 'brand.600'}>
+          {!isDisabled && search ? (
+            <IconButton
+              onClick={handleClear}
+              variant="@secondary"
+              size="xs"
+              aria-label="Supprimer la recherche en cours"
+            >
+              <FiX />
+            </IconButton>
+          ) : (
+            <FiSearch />
+          )}
+        </InputRightElement>
+      </InputGroup>
+    );
+  }
+);
