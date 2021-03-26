d0e64cb63b978e38c4eb9a87af9cb863cfac3d44
Yoann Fleury <yoann.fleury@yahoo.com>
2021-03-26 12:45:19 +0100

    feat: add ConfimMenuItem (#41)
    * feat: add ConfimMenuItem
    
    Co-authored-by: Ivan Dalmet <ivan@dalmet.fr>
    
    * fix: Docs and Hover & Focus bg color
    
    Co-authored-by: Ivan Dalmet <ivan@dalmet.fr>

---
+++ src/components/ConfirmMenuItem/docs.stories.tsx
@@ -0,0 +1,188 @@
+import React from 'react';
+
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Portal,
+  Badge,
+  HStack,
+} from '@chakra-ui/react';
+import { FiChevronDown, FiActivity } from 'react-icons/fi';
+
+import { Icon } from '@/components';
+
+import { ConfirmMenuItem } from './';
+
+export default {
+  title: 'Components/ConfirmMenuItem',
+};
+
+export const Default = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem
+            onClick={() => {
+              alert('Action');
+            }}
+          >
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const MenuItemLongText = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem>
+            Confirm Action with long texts, lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc sed pellentesque lorem, id dictum
+            odio.
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmText = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem confirmText="Custom Confirm">
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmTextLong = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem
+            confirmText="Confirm with long texts, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed
+            pellentesque lorem, id dictum odio."
+          >
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmIcon = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem confirmIcon={FiActivity}>
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmContent = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem
+            confirmContent={
+              <HStack>
+                <Box>Confirmation</Box>
+                <Badge>Something</Badge>
+              </HStack>
+            }
+          >
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmColorScheme = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem confirmColorScheme="green">
+            Confirm Action
+          </ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export const ConfirmDelay = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<Icon icon={FiChevronDown} />}>
+        Actions
+      </MenuButton>
+      <Portal>
+        <MenuList>
+          <MenuItem>Action</MenuItem>
+          <ConfirmMenuItem confirmDelay={5000}>Confirm Action</ConfirmMenuItem>
+          <MenuItem>Action</MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
---
+++ src/components/ConfirmMenuItem/index.tsx
@@ -0,0 +1,208 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+import {
+  chakra,
+  useMenuItem,
+  MenuItemProps,
+  MenuIcon,
+  MenuCommand,
+  forwardRef,
+  HTMLChakraProps,
+  useMenuState,
+  useStyles,
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+import { FiAlertCircle } from 'react-icons/fi';
+
+import { Icon } from '@/components';
+
+export interface StyledMenuItemProps extends HTMLChakraProps<'button'> {}
+
+const StyledMenuItem = forwardRef<StyledMenuItemProps, 'button'>(
+  (props, ref) => {
+    const { type, ...rest } = props;
+    const styles = useStyles();
+
+    /**
+     * Given another component, use its type if present
+     * Else, use no type to avoid invalid html, e.g. <a type="button" />
+     * Else, fall back to "button"
+     */
+    const btnType = rest.as ? type ?? undefined : 'button';
+
+    const buttonStyles: any = {
+      textDecoration: 'none',
+      color: 'inherit',
+      userSelect: 'none',
+      display: 'flex',
+      width: '100%',
+      alignItems: 'center',
+      textAlign: 'left',
+      flex: '0 0 auto',
+      outline: 0,
+      ...styles.item,
+    };
+
+    return (
+      <chakra.button ref={ref} type={btnType} {...rest} __css={buttonStyles} />
+    );
+  }
+);
+
+export const MenuItem = forwardRef<MenuItemProps, 'button'>((props, ref) => {
+  const {
+    icon,
+    iconSpacing = '0.75rem',
+    command,
+    commandSpacing = '0.75rem',
+    children,
+    ...rest
+  } = props;
+
+  const menuItemProps = useMenuItem(rest, ref) as MenuItemProps;
+
+  const shouldWrap = icon || command;
+
+  const _children = shouldWrap ? (
+    <chakra.span pointerEvents="none" flex="1">
+      {children}
+    </chakra.span>
+  ) : (
+    children
+  );
+
+  return (
+    <StyledMenuItem
+      {...menuItemProps}
+      onClick={(e) => {
+        rest?.onClick?.(e);
+      }}
+    >
+      {icon && (
+        <MenuIcon fontSize="0.8em" marginEnd={iconSpacing}>
+          {icon}
+        </MenuIcon>
+      )}
+      {_children}
+      {command && (
+        <MenuCommand marginStart={commandSpacing}>{command}</MenuCommand>
+      )}
+    </StyledMenuItem>
+  );
+});
+
+interface ConfirmMenuItemProps extends MenuItemProps {
+  confirmDelay?: number;
+  confirmColorScheme?: string;
+  confirmContent?: React.ReactNode;
+  confirmText?: React.ReactNode;
+  confirmIcon?: React.FC;
+}
+
+export const ConfirmMenuItem = forwardRef<ConfirmMenuItemProps, 'button'>(
+  (
+    {
+      children,
+      confirmColorScheme = 'error',
+      confirmContent = '',
+      confirmIcon = FiAlertCircle,
+      confirmText = 'Click to confirm',
+      confirmDelay = 2000,
+      onClick,
+      ...rest
+    },
+    ref
+  ) => {
+    const [isConfirmActive, setIsConfirmActive] = useState(false);
+    const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+
+    const { onClose: onCloseMenu } = useMenuState();
+
+    const handleClickConfirm = (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      if (isConfirmActive) {
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+
+        onClick?.(event);
+        onCloseMenu();
+        setIsConfirmActive(false);
+      } else {
+        setIsConfirmActive(true);
+      }
+    };
+
+    useEffect(() => {
+      if (isConfirmActive) {
+        timeoutRef.current = setTimeout(() => {
+          setIsConfirmActive(false);
+        }, confirmDelay);
+      }
+
+      return () => {
+        clearTimeout(timeoutRef.current);
+      };
+    }, [isConfirmActive, confirmDelay]);
+
+    const stylingProps = isConfirmActive
+      ? {
+          bg: `${confirmColorScheme}.100`,
+          color: 'transparent',
+          transition: '0.2s',
+          _hover: {
+            bg: `${confirmColorScheme}.50`,
+          },
+          _focus: {
+            bg: `${confirmColorScheme}.50`,
+          },
+        }
+      : {};
+
+    return (
+      <MenuItem
+        position="relative"
+        onClick={handleClickConfirm}
+        ref={ref}
+        {...stylingProps}
+        {...rest}
+      >
+        <Flex
+          as="span"
+          alignItems="center"
+          opacity={isConfirmActive ? 0 : undefined}
+        >
+          {children}
+        </Flex>
+
+        {isConfirmActive && (
+          <Flex
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            px={3}
+            as="span"
+            color={`${confirmColorScheme}.500`}
+            fontSize="sm"
+            alignItems="center"
+          >
+            {confirmContent ? (
+              confirmContent
+            ) : (
+              <>
+                <Icon icon={confirmIcon} mr={1} />{' '}
+                <Text isTruncated as="span">
+                  {confirmText}
+                </Text>
+              </>
+            )}
+          </Flex>
+        )}
+      </MenuItem>
+    );
+  }
+);
