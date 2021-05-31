b5cfd6a5f410999483a21beb29bbb7a248369717
Ivan Dalmet <ivan@dalmet.fr>
2021-05-31 16:47:34 +0200

    feat: Improve select component internal types

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,16 +1,32 @@
-import React, { useRef, useState } from 'react';
+import React, { forwardRef, useRef } from 'react';
 
-import { Box, useToken, useTheme, useStyleConfig } from '@chakra-ui/react';
-import ReactSelect, { CommonProps } from 'react-select';
+import {
+  Box,
+  Portal,
+  useToken,
+  BoxProps,
+  useTheme,
+  useStyleConfig,
+} from '@chakra-ui/react';
+import type { CSSObject } from '@emotion/react';
+import ReactSelect from 'react-select';
+import type { Props } from 'react-select';
 import AsyncReactSelect from 'react-select/async';
 import AsyncCreatableReactSelect from 'react-select/async-creatable';
 import CreatableReactSelect from 'react-select/creatable';
 
-export const Select: React.FC<CommonProps> = (props) => {
+const BoxAny: any = Box;
+
+const MenuWithChakraPortal = ({ innerProps }: { innerProps: BoxProps }) => (
+  <Portal>
+    <Box {...innerProps} />
+  </Portal>
+);
+
+export const Select = forwardRef<HTMLElement, Props>((props, ref) => {
   const {
     isAsync,
     isCreatable,
-    isDisabled,
     isError,
     size = 'md',
     noOptionsMessage,
@@ -20,7 +36,6 @@ export const Select: React.FC<CommonProps> = (props) => {
     loadOptions = () => new Promise<void>((resolve) => resolve()),
     defaultOptions = true,
     debounceDelay = 500,
-    noPortal = false,
     styles = {},
     ...otherProps
   } = props;
@@ -45,29 +60,20 @@ export const Select: React.FC<CommonProps> = (props) => {
   ]);
   const [fieldHeight] = useToken('sizes', [stylesFromTheme.field.h]);
 
-  const getSelectType = () => {
-    if (isAsync && isCreatable) {
-      return AsyncCreatableReactSelect;
-    }
-    if (isAsync) {
-      return AsyncReactSelect;
-    }
-    if (isCreatable) {
-      return CreatableReactSelect;
-    }
+  const Element = (() => {
+    if (isAsync && isCreatable) return AsyncCreatableReactSelect;
+    if (isAsync) return AsyncReactSelect;
+    if (isCreatable) return CreatableReactSelect;
     return ReactSelect;
-  };
-
-  const Element = getSelectType();
+  })();
 
   let debounceTimeout = useRef<any>();
 
-  const debounce = (func, delay: number) => {
+  const debounce = (func: () => unknown, delay: number) => {
     clearTimeout(debounceTimeout.current);
     return new Promise((resolve) => {
       debounceTimeout.current = setTimeout(async () => {
-        const result = await func();
-        resolve(result);
+        resolve(func());
       }, delay);
     });
   };
@@ -76,171 +82,133 @@ export const Select: React.FC<CommonProps> = (props) => {
     ? {
         defaultOptions,
         cacheOptions: true,
-        loadOptions: (input) =>
+        loadOptions: (input: unknown) =>
           debounce(() => loadOptions(input), debounceDelay),
       }
     : {};
 
-  const selectStyle = {
-    ...styles,
-    control: (provided, data, ...args) => {
-      const { isFocused } = data;
-      const style = {
-        ...provided,
-        fontSize: fieldFontSize,
-        height: 'fit-content',
-        minHeight: fieldHeight,
-        borderRadius: fieldBorderRadius,
-        borderColor: fieldBorderColor,
-        backgroundColor: fieldBg,
-        ...(isFocused && fieldFocusBorderColor
-          ? {
-              borderColor: fieldFocusBorderColor,
-              boxShadow: `0 0 0 1px ${fieldFocusBorderColor}`,
-              '&:hover': {
-                borderColor: fieldFocusBorderColor,
-              },
-            }
-          : {}),
-        ...(isError
-          ? {
-              borderColor: theme.colors.error[600],
-              boxShadow: `0 0 0 1px ${theme.colors.error[600]}`,
-              '&:hover': {
-                borderColor: theme.colors.error[600],
-              },
-            }
-          : {}),
-        ...(isDisabled
-          ? {
-              backgroundColor: 'white',
-            }
-          : {}),
-      };
-      return styles?.control?.(style, data, ...args) ?? style;
-    },
-    option: (provided, data, ...args) => {
-      const { isFocused, isSelected } = data;
-      const style = {
-        ...provided,
-        fontSize: fieldFontSize,
-        ...(isFocused
-          ? {
-              backgroundColor: theme.colors.gray['100'],
-              color: theme.colors.gray['600'],
-            }
-          : {}),
-        ...(isSelected
-          ? {
-              backgroundColor: theme.colors.brand['50'],
-              color: theme.colors.gray['700'],
-              borderLeft: `2px solid ${theme.colors.brand['600']}`,
-            }
-          : {}),
-        ...(isFocused && isSelected
-          ? {
-              backgroundColor: theme.colors.gray['100'],
-            }
-          : {}),
-        ...{
-          ':active': {
-            backgroundColor: theme.colors.brand['100'],
-          },
-        },
-      };
-      return styles?.option?.(style, data, ...args) ?? style;
-    },
-    menu: (provided, ...args) => {
-      const style = {
-        ...provided,
-        zIndex: 10,
-      };
-      return styles?.menu?.(style, ...args) ?? style;
-    },
-    valueContainer: (provided, ...args) => {
-      const style = {
-        ...provided,
-        minHeight: `calc(${fieldHeight} - 2px)`,
-        padding: '0',
-        paddingLeft: '8px',
-        paddingRight: '8px',
-      };
-      return styles?.valueContainer?.(style, ...args) ?? style;
-    },
-    indicatorsContainer: (provided, ...args) => {
-      const style = {
-        ...provided,
-        height: `calc(${fieldHeight} - 2px)`,
-      };
-      return styles?.indicatorsContainer?.(style, ...args) ?? style;
-    },
-    multiValue: (provided, ...args) => {
-      const style = {
-        ...provided,
-        backgroundColor: theme.colors.brand['100'],
-      };
-      return styles?.multiValue?.(style, ...args) ?? style;
-    },
-    multiValueLabel: (provided, ...args) => {
-      const style = {
-        ...provided,
-        color: theme.colors.brand['800'],
-        fontWeight: 'bold',
-      };
-      return styles?.multiValueLabel?.(style, ...args) ?? style;
-    },
-    multiValueRemove: (provided, ...args) => {
-      const style = {
-        ...provided,
-        color: 'inherit',
-        opacity: 0.5,
-        ...{
-          '&:hover': {
-            background: 'transparent',
-            color: 'inherit',
-            opacity: 1,
-          },
-        },
-      };
-      return styles?.multiValueRemove?.(style, ...args) ?? style;
-    },
-    dropdownIndicator: (provided, ...args) => {
-      const style = {
-        ...provided,
-        paddingLeft: '0',
-        paddingRight: '0.2rem',
-      };
-      return styles?.dropdownIndicator?.(style, ...args) ?? style;
-    },
-    indicatorSeparator: () => ({
-      display: 'none',
-    }),
+  type state = {
+    isDisabled: boolean;
+    isFocused: boolean;
+    isSelected: boolean;
   };
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const getComponentStyles = (
+    componentName: string,
+    callback: (state: state) => CSSObject
+  ) => ({
+    [componentName]: (provided: CSSObject, state: state) => {
+      const componentsStyles = callback(state);
+      const combinedStyles = {
+        ...provided,
+        ...componentsStyles,
+      };
+      return styles?.[componentName]?.(combinedStyles, state) ?? combinedStyles;
+    },
+  });
+
+  const getConditionalStyles = (
+    condition: boolean,
+    conditionalStyles: CSSObject
+  ): CSSObject => (condition ? conditionalStyles : {});
+
+  const selectStyle = {
+    ...styles,
+    ...getComponentStyles('control', ({ isFocused, isDisabled }) => ({
+      fontSize: fieldFontSize,
+      height: 'fit-content',
+      minHeight: fieldHeight,
+      borderRadius: fieldBorderRadius,
+      borderColor: fieldBorderColor,
+      backgroundColor: fieldBg,
+      ...getConditionalStyles(isFocused && fieldFocusBorderColor, {
+        borderColor: fieldFocusBorderColor,
+        boxShadow: `0 0 0 1px ${fieldFocusBorderColor}`,
+        '&:hover': {
+          borderColor: fieldFocusBorderColor,
+        },
+      }),
+      ...getConditionalStyles(isError, {
+        borderColor: theme.colors.error[600],
+        boxShadow: `0 0 0 1px ${theme.colors.error[600]}`,
+        '&:hover': {
+          borderColor: theme.colors.error[600],
+        },
+      }),
+      ...getConditionalStyles(isDisabled, {
+        opacity: 0.6,
+      }),
+    })),
+    ...getComponentStyles('option', ({ isFocused, isSelected }) => ({
+      fontSize: fieldFontSize,
+      ':active': {
+        backgroundColor: theme.colors.brand['100'],
+      },
+      ...getConditionalStyles(isFocused, {
+        backgroundColor: theme.colors.gray['100'],
+        color: theme.colors.gray['600'],
+      }),
+      ...getConditionalStyles(isSelected, {
+        backgroundColor: theme.colors.brand['50'],
+        color: theme.colors.gray['700'],
+        borderLeft: `2px solid ${theme.colors.brand['600']}`,
+      }),
+      ...getConditionalStyles(isFocused && isSelected, {
+        backgroundColor: theme.colors.gray['100'],
+      }),
+    })),
+    ...getComponentStyles('menu', () => ({
+      zIndex: 10,
+    })),
+    ...getComponentStyles('valueContainer', () => ({
+      minHeight: `calc(${fieldHeight} - 2px)`,
+      padding: '0',
+      paddingLeft: '8px',
+      paddingRight: '8px',
+    })),
+    ...getComponentStyles('indicatorsContainer', () => ({
+      height: `calc(${fieldHeight} - 2px)`,
+    })),
+    ...getComponentStyles('multiValue', () => ({
+      backgroundColor: theme.colors.brand['100'],
+    })),
+    ...getComponentStyles('multiValueLabel', () => ({
+      color: theme.colors.brand['800'],
+      fontWeight: 'bold',
+    })),
+    ...getComponentStyles('multiValueRemove', () => ({
+      color: 'inherit',
+      opacity: 0.5,
+      '&:hover': {
+        background: 'transparent',
+        color: 'inherit',
+        opacity: 1,
+      },
+    })),
+    ...getComponentStyles('dropdownIndicator', () => ({
+      paddingLeft: '0',
+      paddingRight: '0.2rem',
+    })),
+    ...getComponentStyles('indicatorSeparator', () => ({
+      display: 'none',
+    })),
+  };
 
   return (
-    <Box
+    <BoxAny
       as={Element}
       styles={selectStyle}
-      isDisabled={isDisabled}
+      components={{ MenuPortal: MenuWithChakraPortal }}
       {...(noOptionsMessage
         ? { noOptionsMessage: () => noOptionsMessage }
         : {})}
       {...(loadingMessage ? { loadingMessage: () => loadingMessage } : {})}
       {...(formatCreateLabel ? { formatCreateLabel } : {})}
-      placeholder={placeholder}
+      placeholder={placeholder ? String(placeholder) : 'Select...'}
       menuPlacement="auto"
-      onKeyDown={(event) => {
-        if (isMenuOpen) {
-          event.stopPropagation();
-        }
-      }}
-      onMenuOpen={() => setIsMenuOpen(true)}
-      onMenuClose={() => setIsMenuOpen(false)}
-      {...(noPortal ? {} : { menuPortalTarget: document.body })}
+      ref={ref}
       {...asyncProps}
       {...otherProps}
     />
   );
-};
+});
