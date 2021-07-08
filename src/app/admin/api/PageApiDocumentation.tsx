2d4faf0faea224d9e660c105a88426022d54e0de
Ivan Dalmet <ivan@dalmet.fr>
2021-07-08 14:15:47 +0200

    wip: Dark Mode

--- src/app/admin/api/PageApiDocumentation.tsx
+++ src/app/admin/api/PageApiDocumentation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
@@ -8,10 +9,13 @@ import { Page, PageContent } from '@/app/layout';
 import { AdminNav } from '../AdminNav';
 
 export const PageApiDocumentation = () => {
+  const bg = useColorModeValue('transparent', 'gray.200');
   return (
     <Page containerSize="xl" nav={<AdminNav />}>
       <PageContent>
-        <SwaggerUI url="/open-api.json" />
+        <Box bg={bg} borderRadius="md">
+          <SwaggerUI url="/open-api.json" />
+        </Box>
       </PageContent>
     </Page>
   );
