727c7d566938859344110b5455d30500e6835b2c
Ivan Dalmet <ivan@dalmet.fr>
2021-08-02 09:13:38 +0200

    fix: Add pagination in users mocks

---
+++ src/utils/arrays.ts
@@ -0,0 +1,16 @@
+const isNumber = (value: any): boolean =>
+  typeof parseFloat(value) === 'number' &&
+  !isNaN(parseFloat(value)) &&
+  !isNaN(value);
+
+export const sortByKey = (
+  sortKey: string,
+  sortOrder: 'asc' | 'desc' = 'asc'
+) => (a, b) => {
+  if (!sortKey) return 0;
+  const aValue = isNumber(a[sortKey]) ? parseFloat(a[sortKey]) : a[sortKey];
+  const bValue = isNumber(b[sortKey]) ? parseFloat(a[sortKey]) : a[sortKey];
+  if (aValue < bValue) return sortOrder === 'desc' ? -1 : 1;
+  if (aValue > bValue) return sortOrder === 'desc' ? 1 : -1;
+  return 0;
+};
