ed82d3cb5720481e30beac14fad5abda0ea43393
Ivan Dalmet <ivan@dalmet.fr>
2021-08-03 13:40:34 +0200

    fix: Add direction on Charka UI theme base on language

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { AuthProvider } from '@/app/auth/AuthContext';
 import '@/config';
 import { mockServer } from '@/mocks/server';
 import theme from '@/theme';
+
+import { RTL_LANGUAGES } from './constants/i18n';
 
 if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
   mockServer();
@@ -15,10 +18,18 @@ if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
 const queryClient = new QueryClient();
 
 export const Providers = ({ children }) => {
+  const { i18n } = useTranslation();
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        <ChakraProvider
+          theme={{
+            ...theme,
+            direction: RTL_LANGUAGES.includes(i18n.language) ? 'rtl' : 'ltr',
+          }}
+        >
+          {children}
+        </ChakraProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
