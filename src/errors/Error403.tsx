4aaa1a68757c2fd059604f4844cbcaca6c309544
Ivan Dalmet <ivan@dalmet.fr>
2021-08-03 13:05:56 +0200

    fix: Fix error pages dark colors

--- src/errors/Error403.tsx
+++ src/errors/Error403.tsx
@@ -4,14 +4,19 @@ import { Button, Stack, Center, Heading, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 
+import { useDarkMode } from '@/hooks/useDarkMode';
+
 export const Error403 = () => {
+  const { colorModeValue } = useDarkMode();
   const { t } = useTranslation();
   const history = useHistory();
   return (
     <Center flex="1" p="8">
       <Stack align="center" textAlign="center">
         <Heading>{t('errors:403.title')}</Heading>
-        <Text color="gray.600">{t('errors:403.description')}</Text>
+        <Text color={colorModeValue('gray.600', 'gray.400')}>
+          {t('errors:403.description')}
+        </Text>
         <Button onClick={() => history.goBack()}>
           {t('errors:403.actions.goBack')}
         </Button>
