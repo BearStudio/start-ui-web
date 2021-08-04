54b227898e01141d845bb87c84b7af87791f5b89
Thomas Halipré <thomas.halipre@insa-rouen.fr>
2021-08-04 17:48:36 +0200

    feat: Add sizes for Popover component

---
+++ src/theme/components/popover.ts
@@ -0,0 +1,77 @@
+export default {
+  sizes: {
+    '3xs': {
+      content: {
+        width: '3xs',
+      },
+    },
+    '2xs': {
+      content: {
+        width: '2xs',
+      },
+    },
+    xs: {
+      content: {
+        width: 'xs',
+      },
+    },
+    sm: {
+      content: {
+        width: 'sm',
+      },
+    },
+    md: {
+      content: {
+        width: 'md',
+      },
+    },
+    lg: {
+      content: {
+        width: 'lg',
+      },
+    },
+    xl: {
+      content: {
+        width: 'xl',
+      },
+    },
+    '2xl': {
+      content: {
+        width: '2xl',
+      },
+    },
+    '3xl': {
+      content: {
+        width: '3xl',
+      },
+    },
+    '4xl': {
+      content: {
+        width: '4xl',
+      },
+    },
+    '5xl': {
+      content: {
+        width: '5xl',
+      },
+    },
+    '6xl': {
+      content: {
+        width: '6xl',
+      },
+    },
+    '7xl': {
+      content: {
+        width: '7xl',
+      },
+    },
+    '8xl': {
+      content: {
+        width: '8xl',
+      },
+    },
+  },
+  defaultProps: {
+    size: 'xs',
+  },
+};
