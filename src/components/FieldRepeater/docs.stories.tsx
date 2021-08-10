5680b24ff5a3990cfcbba92d47641fa0a08e8596
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-08-10 14:46:35 +0200

    fix docs style + add example SetFieldValues + AddWithData

--- src/components/FieldRepeater/docs.stories.tsx
+++ src/components/FieldRepeater/docs.stories.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { Stack, Button, IconButton, Flex, ButtonGroup } from '@chakra-ui/react';
 import { Formiz, useForm } from '@formiz/core';
-import { FiPlus, FiTrash2 } from 'react-icons/fi';
+import { FiTrash2 } from 'react-icons/fi';
 
-import { FieldInput, FieldSelect, FieldTextarea } from '@/components';
+import { FieldInput, FieldSelect } from '@/components';
 
 import { FieldRepeater, FieldRepeaterItem } from './index';
 
@@ -37,35 +37,49 @@ export const Default = () => {
               </FieldRepeaterItem>
             ))}
             <Flex justifyContent="space-between">
-              <ButtonGroup>
-                <Button variant="@primary" onClick={() => add()}>
-                  Add
-                </Button>
-                <Button
-                  variant="@primary"
-                  onClick={() => add(undefined, { username: 'Julie' })}
-                >
-                  Add Julie
-                </Button>
-                <Button
-                  onClick={() =>
-                    form.setFieldsValues({
-                      usernames: [{ username: 'Lea' }, { username: 'Luke' }],
-                    })
-                  }
-                >
-                  Set Fields Values
-                </Button>
-                <Button
-                  onClick={() =>
-                    form.setFieldsValues({
-                      usernames: null,
-                    })
-                  }
-                >
-                  Clear
-                </Button>
-              </ButtonGroup>
+              <Button variant="@primary" onClick={() => add()}>
+                Add
+              </Button>
+              <Button type="submit">Submit</Button>
+            </Flex>
+          </Stack>
+        )}
+      </FieldRepeater>
+    </Formiz>
+  );
+};
+
+export const AddWithData = () => {
+  const form = useForm();
+  return (
+    <Formiz onSubmit={console.log} autoForm connect={form}>
+      <FieldRepeater
+        name="usernames"
+        label="Usernames"
+        helper="This is an helper"
+        required="Usernames are required"
+      >
+        {({ add, remove, collection }) => (
+          <Stack spacing="4">
+            {collection?.map(({ key }, index: number) => (
+              <FieldRepeaterItem key={key} index={index}>
+                <Stack direction="row" alignItems="flex-start">
+                  <FieldInput name="username" required="Username is required" />
+                  <IconButton
+                    aria-label="Delete"
+                    icon={<FiTrash2 />}
+                    onClick={() => remove(index)}
+                  />
+                </Stack>
+              </FieldRepeaterItem>
+            ))}
+            <Flex justifyContent="space-between">
+              <Button
+                variant="@primary"
+                onClick={() => add(undefined, { username: 'Julie' })}
+              >
+                Add Julie
+              </Button>
               <Button type="submit">Submit</Button>
             </Flex>
           </Stack>
@@ -103,11 +117,15 @@ export const WithDefaultValue = () => (
               </Stack>
             </FieldRepeaterItem>
           ))}
-          <Button onClick={() => add()}>Add</Button>
+          <Flex justifyContent="space-between">
+            <Button variant="@primary" onClick={() => add()}>
+              Add
+            </Button>
+            <Button type="submit">Submit</Button>
+          </Flex>
         </Stack>
       )}
     </FieldRepeater>
-    <Button type="submit">Submit</Button>
   </Formiz>
 );
 
@@ -116,7 +134,7 @@ export const WithInitialValues = () => (
     onSubmit={console.log}
     autoForm
     initialValues={{
-      colorText: [{ color: 'red', text: 'fsojfiqjfs' }, { color: 'blue' }],
+      colorText: [{ color: 'red', text: 'Julie' }, { color: 'blue' }],
     }}
   >
     <FieldRepeater
@@ -145,77 +163,75 @@ export const WithInitialValues = () => (
                   ]}
                   required="Color is required"
                 />
-                <FieldTextarea name="text" />
+                <FieldInput name="text" />
               </Stack>
             </FieldRepeaterItem>
           ))}
-          <Button onClick={() => add()}>Add an element</Button>
+          <Flex justifyContent="space-between">
+            <Button variant="@primary" onClick={() => add()}>
+              Add
+            </Button>
+            <Button type="submit">Submit</Button>
+          </Flex>
         </Stack>
       )}
     </FieldRepeater>
-    <Button type="submit" variant="@secondary">
-      Submit
-    </Button>
   </Formiz>
 );
 
-export const OtherExample = () => (
-  <Formiz onSubmit={console.log} autoForm>
-    <FieldRepeater
-      name="colorText"
-      label="Usernames"
-      helper="This is an helper"
-    >
-      {({ add, remove, collection }) => (
-        <Stack>
-          <Button onClick={() => add()}>Add an element</Button>
-          <Stack direction="row">
+export const SetFieldsValues = () => {
+  const form = useForm();
+  return (
+    <Formiz onSubmit={console.log} autoForm connect={form}>
+      <FieldRepeater
+        name="usernames"
+        label="Usernames"
+        helper="This is an helper"
+        required="Usernames are required"
+      >
+        {({ add, remove, collection }) => (
+          <Stack spacing="4">
             {collection?.map(({ key }, index: number) => (
               <FieldRepeaterItem key={key} index={index}>
-                <Stack direction="row">
+                <Stack direction="row" alignItems="flex-start">
+                  <FieldInput name="username" required="Username is required" />
                   <IconButton
-                    aria-label="Add before"
-                    icon={<FiPlus />}
-                    onClick={() => add(index)}
-                    size="sm"
-                  />
-                  <Stack flex={4} alignItems="center">
-                    <Button
-                      variant="@primary"
-                      size="sm"
-                      aria-label="Supprimer"
-                      leftIcon={<FiTrash2 />}
-                      onClick={() => remove(index)}
-                    >
-                      Delete {index}
-                    </Button>
-                    <FieldSelect
-                      name="color"
-                      options={[
-                        { label: 'Red', value: 'red' },
-                        { label: 'Yellow', value: 'yellow' },
-                        { label: 'Blue', value: 'blue' },
-                      ]}
-                      required="Color is required"
-                    />
-                    <FieldTextarea name="text" />
-                  </Stack>
-
-                  <IconButton
-                    aria-label="Add before"
-                    icon={<FiPlus />}
-                    onClick={() => add(index + 1)}
-                    size="sm"
+                    aria-label="Delete"
+                    icon={<FiTrash2 />}
+                    onClick={() => remove(index)}
                   />
                 </Stack>
               </FieldRepeaterItem>
             ))}
+            <Flex justifyContent="space-between">
+              <ButtonGroup>
+                <Button variant="@primary" onClick={() => add()}>
+                  Add
+                </Button>
+                <Button
+                  onClick={() =>
+                    form.setFieldsValues({
+                      usernames: [{ username: 'Lea' }, { username: 'Luke' }],
+                    })
+                  }
+                >
+                  Set Fields Values
+                </Button>
+                <Button
+                  onClick={() =>
+                    form.setFieldsValues({
+                      usernames: null,
+                    })
+                  }
+                >
+                  Clear
+                </Button>
+              </ButtonGroup>
+              <Button type="submit">Submit</Button>
+            </Flex>
           </Stack>
-        </Stack>
-      )}
-    </FieldRepeater>
-    <Button type="submit" variant="@secondary">
-      Submit
-    </Button>
-  </Formiz>
-);
+        )}
+      </FieldRepeater>
+    </Formiz>
+  );
+};
