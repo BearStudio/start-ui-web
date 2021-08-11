4124df1615185c5d8d1d85f741f79ef0b8c3a430
Rileran <38964092+Rileran@users.noreply.github.com>
2021-08-11 09:53:28 +0200

    feat: Add sizes for Popover component (#84)

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Badge } from './badge';
 export { default as Button } from './button';
 export { default as Input } from './input';
+export { default as Popover } from './popover';
 export { default as Select } from './select';
 export { default as Textarea } from './textarea';
