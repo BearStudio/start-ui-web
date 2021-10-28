031d91c5f57416fe024e53e2392b756fe56bf997
Ivan Dalmet <ivan@dalmet.fr>
2021-10-28 13:10:50 +0200

    feat: Upgrade dependencies and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  transform: {
+    '\\.[jt]sx?$': '<rootDir>/src/test/jest.transform.js',
+  },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$|@fontsource/': '<rootDir>/src/test/styleMock.js',
---
+++ src/test/jest.transform.js
@@ -0,0 +1,3 @@
+module.exports = require('babel-jest').default.createTransformer({
+  presets: ['next/babel'], // or whatever
+});
