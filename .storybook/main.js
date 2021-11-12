bab48a9bbcda1e14d21f34a4a9278ac51f876fe6
Ivan Dalmet <ivan@dalmet.fr>
2021-11-12 10:25:00 +0100

    Upgrade framer motion to v5 and fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,11 +14,15 @@ module.exports = {
   },
   webpackFinal: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
+
+    // Fix issue between Chakra UI and storybook
     config.resolve.alias = {
       ...config.resolve.alias,
       '@emotion/core': toPath('node_modules/@emotion/react'),
       'emotion-theming': toPath('node_modules/@emotion/react'),
     };
+
+    // Babel config
     config.module.rules.push({
       test: /\.([j|t]sx?)$/,
       loader: require.resolve('babel-loader'),
@@ -26,6 +30,15 @@ module.exports = {
         presets: ['next/babel'],
       },
     });
+
+    // Fix Framer Motion v5 issue
+    // https://github.com/framer/motion/issues/1307#issuecomment-966827629
+    config.module.rules.push({
+      type: 'javascript/auto',
+      test: /\.mjs$/,
+      include: /node_modules/,
+    });
+
     return config;
   },
 };
