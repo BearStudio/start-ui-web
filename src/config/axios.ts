994a5221a560d8f7ed3f87cd538d6822001bc675
Thibault Barske <thibault@bearstudio.fr>
2022-03-02 13:04:08 +0100

    fix: don't send API token to external API

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,8 +5,9 @@ import { isBrowser } from '@/utils/ssr';
 
 Axios.interceptors.request.use(
   (config: AxiosRequestConfig) => {
+    const isExternal = !!config?.url?.startsWith('http');
     const token = isBrowser ? localStorage.getItem(AUTH_TOKEN_KEY) : null;
-    const authHeaders = token ? { Authorization: `Bearer ${token}` } : {};
+    const authHeaders = token && !isExternal ? { Authorization: `Bearer ${token}` } : {};
     return {
       baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '/api',
       ...config,
