144480ace61273ecb4e30aefa9489601e9962b77
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2022-03-03 21:40:02 +0100

    Update src/types/utilities.d.ts
    Co-authored-by: Yoann Fleury <yoann.fleury@yahoo.com>

--- src/types/utilities.d.ts
+++ src/types/utilities.d.ts
@@ -1,5 +1,5 @@
 /**
- * Use this type to temporary bypass to any without writting any
+ * Use this type to temporary bypass to `any` without writting `any`
  * Comment the line to find where it's used
  */
 type TODO = any;
