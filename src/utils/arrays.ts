c2f4b48273bea26190fe7d6e5cca53e60bad1e4d
Ivan Dalmet <ivan@dalmet.fr>
2022-03-04 10:40:58 +0100

    fix: No implicit any in utils/arrays

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,14 +1,18 @@
-const isNumber = (value: ExplicitAny): boolean =>
-  typeof parseFloat(value) === 'number' &&
-  !isNaN(parseFloat(value)) &&
-  !isNaN(value);
+const isNumber = (value: unknown): boolean =>
+  typeof parseFloat(String(value)) === 'number' &&
+  !isNaN(parseFloat(String(value))) &&
+  !isNaN(Number(value));
 
 export const sortByKey =
   (sortKey: string, sortOrder: 'asc' | 'desc' = 'asc') =>
-  (a, b) => {
+  (a: { [key: string]: unknown }, b: { [key: string]: unknown }) => {
     if (!sortKey) return 0;
-    const aValue = isNumber(a[sortKey]) ? parseFloat(a[sortKey]) : a[sortKey];
-    const bValue = isNumber(b[sortKey]) ? parseFloat(a[sortKey]) : a[sortKey];
+    const aValue = isNumber(a[sortKey])
+      ? parseFloat(String(a[sortKey]))
+      : String(a[sortKey]);
+    const bValue = isNumber(b[sortKey])
+      ? parseFloat(String(a[sortKey]))
+      : String(a[sortKey]);
     if (aValue < bValue) return sortOrder === 'desc' ? -1 : 1;
     if (aValue > bValue) return sortOrder === 'desc' ? 1 : -1;
     return 0;
