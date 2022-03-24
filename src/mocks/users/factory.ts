302990daaee8c448a4bef62b8f89f3c62cc07421
Yoann Fleury <yoann.fleury@yahoo.com>
2022-03-24 14:00:46 +0100

    feat(#158): update faker dependency

--- src/mocks/users/factory.ts
+++ src/mocks/users/factory.ts
@@ -1,18 +1,18 @@
+import { faker } from '@faker-js/faker';
 import dayjs from 'dayjs';
-import { date, internet, name } from 'faker';
 import { Factory } from 'miragejs';
 
 type Authority = 'ROLE_ADMIN' | 'ROLE_USER';
 
 export const UserFactory = Factory.extend({
-  login: (): string => internet.userName(),
-  email: (): string => internet.email(),
-  firstName: (): string => name.firstName(),
-  lastName: (): string => name.lastName(),
-  createdBy: (): string => name.firstName(),
-  createdDate: () => dayjs(date.past()).format(),
-  lastModifiedBy: (): string => name.firstName(),
-  lastModifiedDate: () => dayjs(date.past()).format(),
+  login: (): string => faker.internet.userName(),
+  email: (): string => faker.internet.email(),
+  firstName: (): string => faker.name.firstName(),
+  lastName: (): string => faker.name.lastName(),
+  createdBy: (): string => faker.name.firstName(),
+  createdDate: () => dayjs(faker.date.past()).format(),
+  lastModifiedBy: (): string => faker.name.firstName(),
+  lastModifiedDate: () => dayjs(faker.date.past()).format(),
   langKey: (): string => 'en',
   activated: (): boolean => true,
   authorities: (): Authority[] => ['ROLE_USER', 'ROLE_ADMIN'],
