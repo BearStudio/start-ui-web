c65200d9ef1edff6d6a5647985b39e2d00785d6d
Ivan Dalmet <ivan@dalmet.fr>
2022-03-24 17:05:31 +0100

    fix: Import

--- src/app/admin/api/PageApiDocumentation.tsx
+++ src/app/admin/api/PageApiDocumentation.tsx
@@ -4,10 +4,9 @@ import { Box } from '@chakra-ui/react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
+import { AdminNav } from '@/app/admin/AdminNav';
 import { Page, PageContent } from '@/app/layout';
 import { useDarkMode } from '@/hooks/useDarkMode';
-
-import { AdminNav } from '../AdminNav';
 
 export const PageApiDocumentation = () => {
   const { colorModeValue } = useDarkMode();
