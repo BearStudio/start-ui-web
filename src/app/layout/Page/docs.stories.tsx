f64d936b79536dbd913efe942bdbf9d1f405bd9e
KillianPouliquen <killian@bearstudio.fr>
2022-05-06 16:41:08 +0200

    fixed PageTopBar stories

--- src/app/layout/Page/docs.stories.tsx
+++ src/app/layout/Page/docs.stories.tsx
@@ -58,22 +58,6 @@ export const FocusFixedPageTopBar = () => (
   </Page>
 );
 
-export const FixedPageTopBar = () => (
-  <Layout>
-    <Page>
-      <PageTopBar showBack isFixed onBack={() => alert('Back')}>
-        Page Top Bar
-      </PageTopBar>
-      <PageContent>
-        <Center h={800} borderWidth={1}>
-          Page Content
-        </Center>
-      </PageContent>
-      <PageBottomBar>Page Bottom Bar</PageBottomBar>
-    </Page>
-  </Layout>
-);
-
 export const ContainerSizeSmall = () => (
   <Page containerSize="sm">
     <PageTopBar>Page Top Bar</PageTopBar>
