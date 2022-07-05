59fd31f72c02ce7f60b919e6543c53c0ad0094d9
Ivan Dalmet <ivan@dalmet.fr>
2022-07-05 15:00:52 +0200

    feat: Update theme colors, alerts, modals and popovers

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,7 +1,9 @@
+export { default as Alert } from './alert';
 export { default as Badge } from './badge';
 export { default as Button } from './button';
 export { default as Checkbox } from './checkbox';
 export { default as Input } from './input';
+export { default as Modal } from './modal';
 export { default as Popover } from './popover';
 export { default as Radio } from './radio';
 export { default as Select } from './select';
