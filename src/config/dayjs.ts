6c0520dc5a9a67d26437567d6201a5aaf3dc23d7
Ivan Dalmet <ivan@dalmet.fr>
2022-07-07 09:23:36 +0200

    fix: Init dayjs with the current language on first load

--- src/config/dayjs.ts
+++ src/config/dayjs.ts
@@ -2,8 +2,5 @@ import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-import { DEFAULT_LANGUAGE_KEY } from '@/constants/i18n';
-
-dayjs.locale(DEFAULT_LANGUAGE_KEY);
 dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
