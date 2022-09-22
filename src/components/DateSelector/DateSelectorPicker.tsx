a3e90a939d8d723c4f3f33ebcc16a36b16198ed2
Charlélise Fouasse <107464660+charlelisefouasse@users.noreply.github.com>
2022-09-22 12:05:34 +0200

    fix language in date selector popup (#261)
    Co-authored-by: Ivan Dalmet <ivan-dalmet@users.noreply.github.com>

--- src/components/DateSelector/DateSelectorPicker.tsx
+++ src/components/DateSelector/DateSelectorPicker.tsx
@@ -13,18 +13,22 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import dayjs, { Dayjs } from 'dayjs';
+import { i18n } from 'i18next';
 import DayPicker, { DayPickerProps } from 'react-day-picker';
 import ReactFocusLock from 'react-focus-lock';
+import { useTranslation } from 'react-i18next';
 
 import { useDateSelectorContext } from './DateSelector';
 
-type ChildrenFunctionParams = { date: Dayjs; onOpen: () => void };
+type ChildrenFunctionParams = { date: Dayjs; i18n: i18n; onOpen: () => void };
 type DateSelectorPickerProps = Omit<DayPickerProps, 'children'> & {
-  children?({ date, onOpen }: ChildrenFunctionParams): ReactNode;
+  children?({ date, i18n, onOpen }: ChildrenFunctionParams): ReactNode;
 };
-const defaultChildren = ({ date, onOpen }: ChildrenFunctionParams) => (
+
+const defaultChildren = ({ date, i18n, onOpen }: ChildrenFunctionParams) => (
   <chakra.button onClick={onOpen} px="2" type="button">
-    {date.format('DD MMM YYYY')}
+    {/* we use locale to update date language as changing the global locale doesn't affect existing instances. */}
+    {date.locale(i18n.language).format('DD MMM YYYY')}
   </chakra.button>
 );
 
@@ -36,12 +40,24 @@ export const DateSelectorPicker: FC<DateSelectorPickerProps> = ({
 
   const { date, onDayClick, isOpen, onOpen, onClose } =
     useDateSelectorContext();
+  const { i18n } = useTranslation();
 
   const dayPicker = (
     <DayPicker
+      locale={i18n.language}
       initialMonth={date.toDate()}
       selectedDays={[date.toDate()]}
       onDayClick={(d) => onDayClick(dayjs(d))}
+      months={Array.from({ length: 12 }).map((_, i) =>
+        dayjs().month(i).format('MMMM')
+      )}
+      weekdaysLong={Array.from({ length: 7 }).map((_, i) =>
+        dayjs().day(i).format('dddd')
+      )}
+      weekdaysShort={Array.from({ length: 7 }).map((_, i) =>
+        dayjs().day(i).format('dd')
+      )}
+      firstDayOfWeek={1}
       {...rest}
     />
   );
@@ -49,7 +65,7 @@ export const DateSelectorPicker: FC<DateSelectorPickerProps> = ({
   if (isMobile) {
     return (
       <>
-        {children({ date, onOpen })}
+        {children({ date, i18n, onOpen })}
         <Modal isOpen={isOpen} onClose={onClose} size="xs">
           <ModalOverlay />
           <ModalContent>
@@ -62,7 +78,7 @@ export const DateSelectorPicker: FC<DateSelectorPickerProps> = ({
 
   return (
     <Popover isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
-      <PopoverTrigger>{children({ date, onOpen })}</PopoverTrigger>
+      <PopoverTrigger>{children({ date, i18n, onOpen })}</PopoverTrigger>
       <PopoverContent
         minW="0"
         p="0"
