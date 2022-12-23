47cd9fa876bfafaf8156ad4ef62a2b3e1d4cff21
Ivan Dalmet <ivan@dalmet.fr>
2022-12-23 16:32:48 +0100

    fix: Remove mirageJS

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -12,29 +12,10 @@ import { AVAILABLE_LANGUAGES } from './constants/i18n';
 
 const queryClient = new QueryClient();
 
-const useMocksServer = () => {
-  const [isLoadingMocks, setIsLoadingMocks] = useState(
-    !process.env.NEXT_PUBLIC_API_BASE_URL
-  );
-
-  useEffect(() => {
-    (async () => {
-      if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
-        const { mockServer } = await import('@/mocks/server');
-        mockServer();
-        setIsLoadingMocks(false);
-      }
-    })();
-  }, []);
-
-  return { isLoadingMocks };
-};
-
 export const Providers: FC<React.PropsWithChildren<unknown>> = ({
   children,
 }) => {
   const { i18n } = useTranslation();
-  const { isLoadingMocks } = useMocksServer();
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -47,7 +28,7 @@ export const Providers: FC<React.PropsWithChildren<unknown>> = ({
                 ?.dir ?? 'ltr',
           }}
         >
-          {!isLoadingMocks && children}
+          {children}
         </ChakraProvider>
       </AuthProvider>
     </QueryClientProvider>
