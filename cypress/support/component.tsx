70e7cfede653372ee1a4d569ed51c1c3cfe43529
Renan <renan.decamps@gmail.com>
2023-01-20 11:49:47 +0100

    fix: ignore failing eslint rule  in cypress file
    update package.json scripts to work the same as the lint:staged version (so we can detect earlier lint issues)

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -26,6 +26,7 @@ import './commands';
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       mount: typeof mount;
