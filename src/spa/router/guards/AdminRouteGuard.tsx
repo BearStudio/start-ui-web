b8b0c8944a589c7e6811f1a9b2ae7966d90b9d1d
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-04-14 14:38:14 +0200

    fix: redirect unauthenticated on admin routes

--- src/spa/router/guards/AdminRouteGuard.tsx
+++ src/spa/router/guards/AdminRouteGuard.tsx
@@ -3,11 +3,14 @@ import { FC } from 'react';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorPage } from '@/components/ErrorPage';
 import { useAccount } from '@/spa/account/account.service';
+import { useRedirectUnauthenticated } from '@/spa/router/guards/useRedirectUnauthenticated';
 
 export const AdminRouteGuard: FC<React.PropsWithChildren<unknown>> = ({
   children,
 }) => {
   const { isAdmin, isLoading } = useAccount();
+
+  useRedirectUnauthenticated();
 
   if (isLoading) {
     return null;
