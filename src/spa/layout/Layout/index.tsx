5e51375b250972e02847f11141432b4b80eb91dd
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-04-28 11:36:03 +0200

    fix: memoize layout provider value

--- src/spa/layout/Layout/index.tsx
+++ src/spa/layout/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 
 import { Flex, useDisclosure } from '@chakra-ui/react';
 import { useLocation } from 'react-router-dom';
@@ -18,16 +18,19 @@ export const Layout: FC<React.PropsWithChildren<unknown>> = ({ children }) => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  const providerValue = useMemo(
+    () => ({
+      isFocusMode,
+      setIsFocusMode,
+      navIsOpen: nav.isOpen,
+      navOnClose: nav.onClose,
+      navOnOpen: nav.onOpen,
+    }),
+    [isFocusMode, nav.isOpen, nav.onClose, nav.onOpen]
+  );
+
   return (
-    <LayoutContext.Provider
-      value={{
-        isFocusMode,
-        setIsFocusMode,
-        navIsOpen: nav.isOpen,
-        navOnClose: nav.onClose,
-        navOnOpen: nav.onOpen,
-      }}
-    >
+    <LayoutContext.Provider value={providerValue}>
       <Viewport>
         {isAuthenticated && !isFocusMode && <TopBar />}
         <Flex flex="1" direction="column">
