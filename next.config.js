95c1872983f6a3523404121486b7bc605a83d91c
Ivan Dalmet <ivan@dalmet.fr>
2023-05-11 16:16:53 +0200

    fix: Remove unused redirects

--- next.config.js
+++ next.config.js
@@ -22,18 +22,4 @@ module.exports = defineNextConfig({
       },
     ];
   },
-  async rewrites() {
-    return [
-      // Do not rewrite API routes
-      {
-        source: '/api/:any*',
-        destination: '/api/:any*',
-      },
-      // Rewrite everything else to use `pages/app`
-      {
-        source: '/app/:any*',
-        destination: '/app/',
-      },
-    ];
-  },
 });
