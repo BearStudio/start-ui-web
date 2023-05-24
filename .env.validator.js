40977b1951267a694e2aa2b8337f77ae749fe9ef
Ivan Dalmet <ivan@dalmet.fr>
2023-05-24 09:55:38 +0200

    fix: Allows relative url for NEXT_PUBLIC_API_BASE_URL

--- .env.validator.js
+++ .env.validator.js
@@ -12,7 +12,13 @@ const envSchema = z.object({
   NEXT_PUBLIC_IS_DEMO: z.enum(['true', 'false']).optional(),
 
   NEXT_PUBLIC_BASE_URL: z.string().url(),
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_API_BASE_URL: z.union([
+    z.string().url({
+      message:
+        'Should be a valid absolute url or a relative url that starts with "/"',
+    }),
+    z.string().startsWith('/'),
+  ]),
 
   NEXT_PUBLIC_DEV_ENV_NAME: z.string().optional(),
   NEXT_PUBLIC_DEV_ENV_COLOR_SCHEME: z.string().optional(),
