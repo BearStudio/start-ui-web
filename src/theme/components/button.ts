231eab9d78cd30b75ee70ad7f8ddd4bd1298cf13
Ivan Dalmet <ivan@dalmet.fr>
2023-05-29 09:01:57 +0200

    fix: Refacto button theme

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -1,106 +1,92 @@
-import { defineStyleConfig } from '@chakra-ui/react';
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
 import { transparentize } from '@chakra-ui/theme-tools';
+
+const variantPrimary = defineStyle((props) => ({
+  bg: `${props.colorScheme}.600`,
+  color: 'white',
+  _focusVisible: {
+    boxShadow: 'outline-brand',
+  },
+  _hover: {
+    bg: `${props.colorScheme}.700`,
+    color: 'white',
+    _disabled: {
+      bg: `${props.colorScheme}.600`,
+    },
+  },
+  _active: { bg: `${props.colorScheme}.800` },
+  _dark: {
+    bg: `${props.colorScheme}.300`,
+    color: `${props.colorScheme}.900`,
+    _focusVisible: {
+      boxShadow: 'outline-brand',
+    },
+    _hover: {
+      bg: `${props.colorScheme}.400`,
+      color: `${props.colorScheme}.900`,
+      _disabled: {
+        bg: `${props.colorScheme}.300`,
+      },
+    },
+    _active: {
+      bg: `${props.colorScheme}.500`,
+    },
+  },
+}));
+
+const variantSecondary = defineStyle((props) => ({
+  bg: 'white',
+  color: `${props.colorScheme}.600`,
+  border: '1px solid',
+  borderColor: 'gray.200',
+  _hover: {
+    bg: `${props.colorScheme}.50`,
+    borderColor: `${props.colorScheme}.200`,
+  },
+  _disabled: {
+    _hover: {
+      borderColor: 'gray.200',
+    },
+  },
+  _dark: {
+    bg: 'gray.800',
+    color: `${props.colorScheme}.500`,
+    borderColor: 'gray.700',
+    _hover: {
+      bg: 'gray.900',
+      borderColor: `${props.colorScheme}.700`,
+    },
+    _disabled: {
+      _hover: {
+        borderColor: 'gray.700',
+      },
+    },
+  },
+}));
 
 export const buttonTheme = defineStyleConfig({
   variants: {
     // Custom variants
-    '@primary': {
-      bg: 'brand.600',
-      color: 'white',
+    '@primary': (props) => ({
+      ...variantPrimary({ ...props, colorScheme: 'brand' }),
       _focusVisible: {
         boxShadow: 'outline-brand',
       },
-      _hover: {
-        bg: 'brand.700',
-        color: 'white',
-        _disabled: {
-          bg: 'brand.600',
-        },
-      },
-      _active: { bg: 'brand.800' },
-      _dark: {
-        bg: 'brand.300',
-        color: 'brand.900',
-        _focusVisible: {
-          boxShadow: 'outline-brand',
-        },
-        _hover: {
-          bg: 'brand.400',
-          color: 'brand.900',
-          _disabled: {
-            bg: 'brand.300',
-          },
-        },
-        _active: {
-          bg: 'brand.500',
-        },
-      },
-    },
-    '@secondary': {
-      bg: 'white',
-      color: 'brand.600',
-      border: '1px solid',
-      borderColor: 'gray.200',
-      _hover: {
-        bg: 'brand.50',
-        borderColor: 'brand.200',
-      },
+    }),
+    '@secondary': (props) => ({
+      ...variantSecondary({ ...props, colorScheme: 'brand' }),
       _focusVisible: {
         boxShadow: 'outline-brand',
       },
-      _disabled: {
-        _hover: {
-          borderColor: 'gray.200',
-        },
-      },
-      _dark: {
-        bg: 'gray.800',
-        color: 'brand.400',
-        borderColor: 'gray.700',
-        _hover: {
-          bg: 'gray.900',
-          borderColor: 'brand.700',
-        },
-        _disabled: {
-          _hover: {
-            borderColor: 'gray.700',
-          },
-        },
-      },
-    },
-    '@danger': {
-      bg: 'white',
-      color: 'error.700',
-      border: '1px solid',
-      borderColor: 'gray.200',
-      _hover: {
-        bg: 'error.50',
-        borderColor: 'error.200',
-      },
+    }),
+    '@danger': (props) => ({
+      ...variantSecondary({ ...props, colorScheme: 'error' }),
       _focusVisible: {
         boxShadow: 'outline-error',
       },
-      _disabled: {
-        _hover: {
-          borderColor: 'gray.200',
-        },
-      },
-      _dark: {
-        bg: 'gray.800',
-        color: 'error.500',
-        borderColor: 'gray.700',
-        _hover: {
-          bg: 'gray.900',
-          borderColor: 'error.700',
-        },
-        _disabled: {
-          _hover: {
-            borderColor: 'gray.700',
-          },
-        },
-      },
-    },
+    }),
     // Default variants
+    outline: variantSecondary,
     ghost: (props) => ({
       bg: transparentize(`${props.colorScheme}.500`, 0.05)(props.theme),
       _hover: {
