06a3310130506b3406532a3a2139c35c705995c6
Ivan Dalmet <ivan@dalmet.fr>
2023-05-29 13:40:24 +0200

    fix: Remove old outline shadows

--- src/theme/foundations/shadows.ts
+++ src/theme/foundations/shadows.ts
@@ -1,16 +1,3 @@
-import { transparentize } from '@chakra-ui/theme-tools';
-
-import { colors } from './colors';
-
-const createOutline = (colorScheme = 'gray') =>
-  // eslint-disable-next-line sonarjs/no-nested-template-literals
-  `0 0 0 3px ${transparentize(`${colorScheme}.500`, 0.3)({ colors })}`;
-
 export const shadows = {
-  outline: createOutline('brand'),
-  'outline-brand': createOutline('brand'),
-  'outline-gray': createOutline('gray'),
-  'outline-success': createOutline('success'),
-  'outline-warning': createOutline('warning'),
-  'outline-error': createOutline('error'),
+  layout: '0px 0px 24px 1px rgba(0, 0, 0, 0.05)',
 };
