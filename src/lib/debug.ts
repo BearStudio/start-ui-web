124947e4abdab148fe615651b24d6211be7e8f3e
Renan <renan.decamps@gmail.com>
2023-07-19 13:42:28 +0200

    feat: add debugComponent utility function

---
+++ src/lib/debug.ts
@@ -0,0 +1,37 @@
+import { HTMLChakraProps, ThemeTypings } from '@chakra-ui/react';
+
+export const debugComponent = ({
+  label = 'Debug',
+  colorScheme = 'green',
+  hideLabel = false,
+  hideBackground = true,
+}: {
+  label?: string;
+  colorScheme?: ThemeTypings['colorSchemes'];
+  hideLabel?: boolean;
+  hideBackground?: boolean;
+} = {}): HTMLChakraProps<'div'> => {
+  if (process.env.NODE_ENV !== 'development') return {};
+  return {
+    border: '1px solid',
+    borderColor: `${colorScheme}.500`,
+    backgroundColor: !hideBackground ? `${colorScheme}.50` : undefined,
+    position: 'relative',
+    _after: hideLabel
+      ? undefined
+      : {
+          zIndex: 1,
+          content: `"${label}"`,
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          fontSize: 'xs',
+          px: 2,
+          py: 0.5,
+          color: `${colorScheme}.600`,
+          backgroundColor: `${colorScheme}.100`,
+          borderRadius: '0 5px 0 0',
+          boxShadow: '0 0 4px rgba(0, 0, 0, .2)',
+        },
+  };
+};
