add20730a07c5f4dde2aa668dd3da9e849ce23b3
Ivan Dalmet <ivan@dalmet.fr>
2023-07-21 14:05:48 +0200

    fix: Cleanup

--- src/app/api/jhipster-mocks/repositories/service.ts
+++ src/app/api/jhipster-mocks/repositories/service.ts
@@ -2,58 +2,46 @@ import { Repository } from '@prisma/client';
 
 import { db } from '@/app/api/jhipster-mocks/_helpers/db';
 
-export type RepositoryFormatted<U extends Partial<Repository> = Repository> =
-  ReturnType<typeof formatRepositoryFromDb<U>>;
-
-export const formatRepositoryFromDb = <U extends Partial<Repository>>(
-  repository?: U | null
-) => {
-  return !!repository ? repository : undefined;
-};
-
 export const getRepositoryList = async (
   options: { skip?: number; take?: number } = {}
 ) => {
   const [repositories, total] = await Promise.all([
     db.repository.findMany({
       skip: options.skip ?? 0,
-      take: options.take ?? 2,
+      take: options.take ?? 10,
     }),
     db.repository.count(),
   ]);
 
   return {
-    repositories: repositories.map(formatRepositoryFromDb),
+    repositories,
     total,
   } as const;
 };
 
 export const getRepositoryById = async (id: number) => {
-  const repository = await db.repository.findUnique({ where: { id } });
-  return formatRepositoryFromDb(repository);
+  return await db.repository.findUnique({ where: { id } });
 };
 
 export const updateRepositoryById = async (
   id: number,
-  partialRepository: Partial<RepositoryFormatted>
+  partialRepository: Partial<Repository>
 ) => {
   if (!partialRepository) return undefined;
-  const user = await db.repository.update({
+  return await db.repository.update({
     where: { id },
     data: partialRepository,
   });
-  return formatRepositoryFromDb(user);
 };
 
 export const createRepository = async (
-  newRepository: RepositoryFormatted<Pick<Repository, 'link' | 'name'>>
+  newRepository: Pick<Repository, 'link' | 'name'>
 ) => {
   if (!newRepository) throw new Error('Missing new repository');
 
-  const repository = await db.repository.create({
+  return await db.repository.create({
     data: newRepository,
   });
-  return formatRepositoryFromDb(repository);
 };
 
 export const removeRepositoryById = async (id: number) => {
