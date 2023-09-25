6830f3f164866df4b576c02f9b3b6c5cbc6706ae
Renan <renan.decamps@gmail.com>
2023-09-25 09:14:37 +0200

    feat: update recommended extensions

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn build && yarn start',
+    command: 'yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
