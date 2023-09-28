f0beae019d2796efa213f5fb2200c972ddbd52ed
Ivan Dalmet <ivan@dalmet.fr>
2023-09-28 18:25:12 +0200

    feat: new open api adapter

--- src/app/api/rest/[...path]/route.ts
+++ src/app/api/rest/[...path]/route.ts
@@ -2,11 +2,14 @@ import { appRouter } from '@/server/api/root';
 import { createTRPCContext } from '@/server/api/trpc';
 import { createOpenApiFetchHandler } from '@/server/api/trpcOpenApiFetchAdapter';
 
-const handler = createOpenApiFetchHandler({
-  router: appRouter as TODO,
-  createContext: createTRPCContext,
-  endpoint: '/api/rest',
-});
+const handler = (req: Request) => {
+  return createOpenApiFetchHandler({
+    req,
+    endpoint: '/api/rest',
+    router: appRouter,
+    createContext: createTRPCContext,
+  });
+};
 
 export const GET = handler;
 export const POST = handler;
--- src/server/api/trpcOpenApiFetchAdapter.ts
+++ src/server/api/trpcOpenApiFetchAdapter.ts
@@ -1,213 +1,150 @@
-import { TRPCError, inferRouterContext } from '@trpc/server';
-import { NodeHTTPRequest } from '@trpc/server/adapters/node-http';
-import { ResponseMetaFn } from '@trpc/server/dist/http/internals/types';
-import { OnErrorFunction } from '@trpc/server/dist/internals/types';
-import { getHTTPStatusCodeFromError } from '@trpc/server/http';
-import EventEmitter from 'events';
-// Unfortunately the index of node-mocks-http imports its express mocks, which
-// use the `depd` package to mark a function as deprecated, which internally
-// uses eval(), which @edge-runtime/jest-environment (rightly) forbids.
-// So we have to import it directly. Typing it requires the `fetch.d.ts` hack.
-import { createRequest } from 'node-mocks-http/lib/mockRequest';
-import { createResponse } from 'node-mocks-http/lib/mockResponse';
+import { TRPCError } from '@trpc/server';
+import { FetchHandlerOptions } from '@trpc/server/adapters/fetch';
+import { IncomingMessage, ServerResponse } from 'http';
+import { OpenApiRouter } from 'trpc-openapi';
 import {
-  OpenApiErrorResponse,
-  OpenApiMethod,
-  OpenApiRouter,
-  generateOpenApiDocument,
-} from 'trpc-openapi';
-import { createOpenApiNodeHttpHandler } from 'trpc-openapi/dist/adapters/node-http/core';
+  CreateOpenApiNodeHttpHandlerOptions,
+  createOpenApiNodeHttpHandler,
+} from 'trpc-openapi/dist/adapters/node-http/core';
 
-export type CreateFetchContextFn<TRouter extends OpenApiRouter> = (params: {
-  req: Request;
-  resHeaders: Headers;
-}) => inferRouterContext<TRouter>;
+export type CreateOpenApiFetchHandlerOptions<TRouter extends OpenApiRouter> =
+  Omit<FetchHandlerOptions<TRouter>, 'batching'> & {
+    req: Request;
+    endpoint: `/${string}`;
+  };
 
-export interface CreateOpenApiFetchHandlerOpts<TRouter extends OpenApiRouter> {
-  router: TRouter;
-  endpoint: string;
-  createContext?: CreateFetchContextFn<TRouter>;
-  onError?: OnErrorFunction<TRouter, Request>;
-  responseMeta?: ResponseMetaFn<TRouter>;
-}
+const getUrlEncodedBody = async (req: Request) => {
+  const params = new URLSearchParams(await req.text());
 
-export function getErrorFromUnknown(cause: unknown): TRPCError {
-  if (cause instanceof Error && cause.name === 'TRPCError') {
-    return cause as TRPCError;
+  const data: Record<string, unknown> = {};
+
+  for (const key of params.keys()) {
+    data[key] = params.getAll(key);
   }
 
-  let errorCause: Error | undefined = undefined;
-  let stack: string | undefined = undefined;
+  return data;
+};
 
-  if (cause instanceof Error) {
-    errorCause = cause;
-    stack = cause.stack;
-  }
-
-  const error = new TRPCError({
-    message: 'Internal server error',
-    code: 'INTERNAL_SERVER_ERROR',
-    cause: errorCause,
-  });
-
-  if (stack) {
-    error.stack = stack;
-  }
-
-  return error;
-}
-
-const createMockNodeHTTPRequest = async (
-  srcReq: Request,
-  url: URL
-): Promise<NodeHTTPRequest> => {
-  const method = srcReq.method as OpenApiMethod & 'HEAD';
-
-  let body = undefined;
-  const contentType = srcReq.headers.get('content-type');
-  if (contentType === 'application/json') {
-    try {
-      if (srcReq.body) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        body = await srcReq.json();
-      }
-    } catch (cause) {
-      throw new TRPCError({
-        message: 'Failed to parse request body',
-        code: 'PARSE_ERROR',
-        cause,
-      });
+// co-body does not parse Request body correctly
+const getRequestBody = async (req: Request) => {
+  try {
+    if (req.headers.get('content-type')?.includes('application/json')) {
+      return {
+        isValid: true,
+        // use JSON.parse instead of req.json() because req.json() does not throw on invalid JSON
+        data: JSON.parse(await req.text()),
+      };
     }
+
+    if (
+      req.headers
+        .get('content-type')
+        ?.includes('application/x-www-form-urlencoded')
+    ) {
+      return {
+        isValid: true,
+        data: await getUrlEncodedBody(req),
+      };
+    }
+
+    return {
+      isValid: true,
+      data: req.body,
+    };
+  } catch (err) {
+    return {
+      isValid: false,
+      cause: err,
+    };
   }
-  if (contentType === 'application/x-www-form-urlencoded') {
-    try {
-      if (srcReq.body) {
-        const searchParamsString = await srcReq.text();
-        const searchParams = new URLSearchParams(searchParamsString);
-        body = {} as Record<string, unknown>;
-        for (const [key, value] of searchParams.entries()) {
-          body[key] = value;
+};
+
+const createRequestProxy = async (req: Request, url?: string) => {
+  const body = await getRequestBody(req);
+
+  return new Proxy(req, {
+    get: (target, prop) => {
+      if (prop === 'url') {
+        return url ? url : target.url;
+      }
+
+      if (prop === 'headers') {
+        return new Proxy(target.headers, {
+          get: (target, prop) => {
+            return target.get(prop.toString());
+          },
+        });
+      }
+
+      if (prop === 'body') {
+        if (!body.isValid) {
+          throw new TRPCError({
+            code: 'PARSE_ERROR',
+            message: 'Failed to parse request body',
+            cause: body.cause,
+          });
         }
+
+        return body.data;
       }
-    } catch (cause) {
-      throw new TRPCError({
-        message: 'Failed to parse request body',
-        code: 'PARSE_ERROR',
-        cause,
-      });
-    }
-  }
 
-  const headers: Record<string, string> = {};
-  for (const [key, value] of srcReq.headers.entries()) {
-    headers[key] = value;
-  }
-
-  return createRequest({
-    url: url.toString(),
-    method,
-    query: url.searchParams,
-    headers,
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    body,
+      return target[prop as keyof typeof target];
+    },
   });
 };
 
-export function createOpenApiFetchHandler<TRouter extends OpenApiRouter>(
-  opts: CreateOpenApiFetchHandlerOpts<TRouter>
-) {
-  // Validate router
-  if (process.env.NODE_ENV !== 'production') {
-    generateOpenApiDocument(opts.router, {
-      title: '',
-      version: '',
-      baseUrl: '',
-    });
-  }
-  return async (srcReq: Request): Promise<Response> => {
-    const resHeaders = new Headers();
+export const createOpenApiFetchHandler = async <TRouter extends OpenApiRouter>(
+  opts: CreateOpenApiFetchHandlerOptions<TRouter>
+): Promise<Response> => {
+  const resHeaders = new Headers();
+  const url = new URL(opts.req.url.replace(opts.endpoint, ''));
+  const req: Request = await createRequestProxy(opts.req, url.toString());
 
-    const createContext = () => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return opts.createContext?.({ req: srcReq, resHeaders });
-    };
-
-    const url = new URL(
-      srcReq.url.startsWith('/') ? `http://127.0.0.1${srcReq.url}` : srcReq.url
-    );
-    url.pathname = url.pathname.slice(opts.endpoint.length + 1);
-
-    try {
-      const req = await createMockNodeHTTPRequest(srcReq, url);
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      const openApiHttpHandler = createOpenApiNodeHttpHandler({
-        ...opts,
-        createContext,
-      } as TODO);
-
-      const res = createResponse({ eventEmitter: EventEmitter });
-
-      await openApiHttpHandler(req, res);
-
-      for (const [k, v] of Object.entries(res.getHeaders())) {
-        if (v) {
-          if (Array.isArray(v)) {
-            for (const value of v) {
-              resHeaders.append(k, value);
-            }
-          } else {
-            resHeaders.append(k, typeof v === 'number' ? v.toString(10) : v);
-          }
-        }
-      }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      return new Response(res._getData(), {
-        status: res.statusCode,
-        headers: resHeaders,
-      });
-    } catch (err) {
-      const error = getErrorFromUnknown(err);
-      opts.onError?.({
-        error,
-        type: 'unknown',
-        path: url.pathname,
-        input: undefined,
-        ctx: undefined,
-        req: srcReq,
-      });
-
-      const meta = opts.responseMeta?.({
-        type: 'unknown',
-        paths: [url.pathname],
-        ctx: undefined,
-        data: [undefined as TODO],
-        errors: [error],
-      });
-
-      const errorShape = opts.router.getErrorShape({
-        error,
-        type: 'unknown',
-        path: url.pathname,
-        input: undefined,
-        ctx: undefined,
-      });
-
-      const statusCode =
-        meta?.status ?? getHTTPStatusCodeFromError(error) ?? 500;
-      const headers = new Headers({
-        'content-type': 'application/json',
-        ...(meta?.headers ?? {}),
-      });
-      const body: OpenApiErrorResponse = {
-        message: errorShape?.message ?? error.message ?? 'An error occurred',
-        code: error.code,
-      };
-
-      return new Response(JSON.stringify(body), {
-        status: statusCode,
-        headers,
-      });
+  const createContext = () => {
+    if (opts.createContext) {
+      return opts.createContext({ req: opts.req, resHeaders });
     }
+    return () => ({});
   };
-}
+
+  const openApiHttpHandler = createOpenApiNodeHttpHandler({
+    router: opts.router,
+    createContext,
+    onError: opts.onError,
+    responseMeta: opts.responseMeta,
+  } as CreateOpenApiNodeHttpHandlerOptions<TRouter, TODO, TODO>);
+
+  return new Promise<Response>((resolve) => {
+    let statusCode: number | undefined;
+
+    return openApiHttpHandler(
+      req as unknown as IncomingMessage,
+
+      {
+        setHeader: (key: string, value: string | readonly string[]) => {
+          if (typeof value === 'string') {
+            resHeaders.set(key, value);
+          } else {
+            for (const v of value) {
+              resHeaders.append(key, v);
+            }
+          }
+        },
+        get statusCode() {
+          return statusCode;
+        },
+        set statusCode(code: number | undefined) {
+          statusCode = code;
+        },
+        end: (body: string) => {
+          resolve(
+            new Response(body, {
+              headers: resHeaders,
+              status: statusCode,
+            })
+          );
+        },
+      } as unknown as ServerResponse
+    );
+  });
+};
