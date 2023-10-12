0e1c6b3f05bff2757b3e3e89125b370c85f420d9
Ivan Dalmet <ivan@dalmet.fr>
2023-10-12 13:14:29 +0200

    feat: Add logout

--- src/features/account/PageAccount.tsx
+++ src/features/account/PageAccount.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { Divider, Heading, Stack } from '@chakra-ui/react';
+import { Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
+import { ConfirmModal } from '@/components/ConfirmModal';
 import { AccountEmailForm } from '@/features/account/AccountEmailForm';
 import { AccountProfileForm } from '@/features/account/AccountProfileForm';
 import { AppLayoutPage } from '@/features/app/AppLayoutPage';
 
 export default function PageHome() {
+  const router = useRouter();
+
   return (
     <AppLayoutPage containerMaxWidth="md">
       <Stack spacing={4}>
@@ -26,6 +30,18 @@ export default function PageHome() {
           </Heading>
           <AccountEmailForm />
         </Stack>
+        <Divider />
+        <Flex>
+          <ConfirmModal
+            onConfirm={() => router.push('/logout')}
+            title="Account logout" // TODO translations
+            message="Your about to terminate your session." // TODO translations
+            confirmText="Logout" // TODO translations
+            confirmVariant="@danger"
+          >
+            <Button variant="@danger">Logout {/* TODO Translations */}</Button>
+          </ConfirmModal>
+        </Flex>
       </Stack>
     </AppLayoutPage>
   );
