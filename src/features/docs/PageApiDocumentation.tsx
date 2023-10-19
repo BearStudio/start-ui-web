17f244dfd02af7be72a6d20641fd2f95b2c5533b
Yoann Fleury <yoann.fleury@yahoo.com>
2023-10-19 13:49:32 +0200

    feat: replace swagger with stoplight

--- src/features/docs/PageApiDocumentation.tsx
+++ src/features/docs/PageApiDocumentation.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { API } from '@stoplight/elements';
+import '@stoplight/elements/styles.min.css';
 
 import {
   AdminLayoutPage,
@@ -10,17 +10,24 @@ import {
 } from '@/features/admin/AdminLayoutPage';
 
 export default function PageApiDocumentation() {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
+
   return (
     <AdminLayoutPage noContainer>
       <AdminLayoutPageContent>
-        <Box
-          borderRadius="md"
-          mt={-8}
-          bg="transparent"
-          _dark={{ bg: 'gray.200' }}
+        <Flex
+          direction="column"
+          position="absolute"
+          inset="0"
+          overflow="auto"
+          p={{ base: '4', lg: '0' }}
         >
-          <SwaggerUI url="/api/openapi.json" />
-        </Box>
+          <API
+            apiDescriptionUrl="/api/openapi.json"
+            layout={isMobile ? 'stacked' : 'sidebar'}
+            router="memory"
+          />
+        </Flex>
       </AdminLayoutPageContent>
     </AdminLayoutPage>
   );
