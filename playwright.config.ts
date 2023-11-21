c6ee946e95efae3100e3a02da3a943ae86b1e409
Ivan Dalmet <ivan@dalmet.fr>
2023-11-21 15:41:08 +0100

    fix: Update timeouts for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
+  /* Max time for the full CI tests */
+  globalTimeout: 15 * 60 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
