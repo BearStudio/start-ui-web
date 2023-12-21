2d7bbc1b16464acc24e136aa13df6ff80a99b09d
Fabien Essid <fabien.essid@bearstudio.fr>
2023-12-21 17:18:11 +0100

    feat: add optional user metadata on signedUrl S3 calls

--- src/features/account/useAvatarUpload.ts
+++ src/features/account/useAvatarUpload.ts
@@ -5,8 +5,14 @@ import { trpc } from '@/lib/trpc/client';
 export const useAvatarUpload = () => {
   const getPresignedUrl = trpc.account.uploadAvatarPresignedUrl.useMutation();
   return useMutation({
-    mutationFn: async (args: { contentType: string; file: File }) => {
-      const { signedUrl, futureFileUrl } = await getPresignedUrl.mutateAsync();
+    mutationFn: async (args: {
+      contentType: string;
+      file: File;
+      metadata?: Record<string, string>;
+    }) => {
+      const { signedUrl, futureFileUrl } = await getPresignedUrl.mutateAsync({
+        metadata: args.metadata,
+      });
 
       await fetch(signedUrl, {
         method: 'PUT',
