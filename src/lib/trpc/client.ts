9b2d4e71707b72a9f8b93ccc73242e9e93f26bef
Ivan Dalmet <ivan@dalmet.fr>
2024-03-06 14:08:35 +0100

    fix: Type import

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -4,7 +4,7 @@
  */
 import { createTRPCReact } from '@trpc/react-query';
 
-import { type AppRouter } from './types';
+import type { AppRouter } from './types';
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const trpc = createTRPCReact<AppRouter>();
