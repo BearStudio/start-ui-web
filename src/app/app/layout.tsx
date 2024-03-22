2d7e8617da46f9455f81cf74436549e970c9433f
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2024-03-22 12:27:10 +0100

    fix: Prevent initial page to load twice (#453)

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,12 +1,12 @@
-import { ReactNode, Suspense } from 'react';
+import { ReactNode } from 'react';
 
 import { NextLoader } from '@/app/NextLoader';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <Suspense>
+    <>
       <NextLoader showSpinner />
       {children}
-    </Suspense>
+    </>
   );
 }
