e0b1022f4397ddba07388db26c0d74c3f9a15481
Ivan Dalmet <ivan@dalmet.fr>
2024-04-17 09:46:57 +0200

    fix: Vitest config for alias paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -6,5 +7,10 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
