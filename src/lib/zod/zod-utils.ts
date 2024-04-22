eb28ceb56617cccd49584f96311110f1782cd917
Ivan Dalmet <ivan@dalmet.fr>
2024-04-22 10:00:56 +0200

    feat: Add zod utils for nonEmpty arrays

--- src/lib/zod/zod-utils.ts
+++ src/lib/zod/zod-utils.ts
@@ -1,9 +1,12 @@
-import { ZodString, z } from 'zod';
+import { t } from 'i18next';
+import { Schema, ZodArray, ZodString, z } from 'zod';
 
 export const zu = {
   string: {
     nonEmpty(s: ZodString, message?: string) {
-      return s.trim().min(1, message);
+      return s
+        .trim()
+        .min(1, message ?? t('zod:errors.invalid_type_received_undefined'));
     },
     nonEmptyOptional(s: ZodString, message?: string) {
       return z
@@ -16,6 +19,20 @@ export const zu = {
     },
     emailOptional(s: ZodString, message?: string) {
       return zu.string.nonEmptyOptional(zu.string.email(s, message), message);
+    },
+  },
+  array: {
+    nonEmpty<T extends Schema>(a: ZodArray<T>, message?: string) {
+      return a.min(
+        1,
+        message ?? t('zod:errors.invalid_type_received_undefined')
+      );
+    },
+    nonEmptyOptional<T extends Schema>(a: ZodArray<T>, message?: string) {
+      return a
+        .transform((v) => (v.length === 0 ? undefined : v))
+        .pipe(zu.array.nonEmpty(a, message).optional())
+        .optional();
     },
   },
 } as const;
