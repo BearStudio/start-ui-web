95c7e88eb1077974bbd586842307f7f21597cadf
Ivan Dalmet <ivan@dalmet.fr>
2024-04-22 08:48:16 +0200

    fix: PR feedbacks

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -9,8 +9,8 @@ Object.defineProperty(window, 'matchMedia', {
     matches: false,
     media: query,
     onchange: null,
-    addListener: vi.fn(), // deprecated
-    removeListener: vi.fn(), // deprecated
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -16,6 +16,8 @@ const customRender = (
   return render(ui, { wrapper: WithProviders, ...options });
 };
 
+// Custom Render
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
 export * from '@testing-library/react';
 export { customRender as render };
 export const setupUser = () => userEvent.setup();
