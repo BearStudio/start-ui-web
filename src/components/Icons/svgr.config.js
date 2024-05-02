23759faf00c784c35df7a9bb5f6d9b72dd4c5f7c
Renan Decamps <DecampsRenan@users.noreply.github.com>
2024-05-02 14:37:32 +0200

    fix: disable prettier formating from svgr (#461)
    fix: disable prettier formating
    
    prevent error when formating generated files with prettier v3 from svgr

--- src/components/Icons/svgr.config.js
+++ src/components/Icons/svgr.config.js
@@ -8,4 +8,10 @@ module.exports = {
   typescript: true,
   replaceAttrValues: { '#000': 'currentColor' },
   template,
+
+  // Prevent svgr + prettier 3 compatibility issue
+  // It is ok to disable it here, every generated file is tracked
+  // with git and will be formated on push thanks to lint-staged + prettier
+  // See https://github.com/gregberge/svgr/issues/893 for details on the underlying issue
+  prettier: false,
 };
