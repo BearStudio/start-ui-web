edcef8852be2e4aa4138cce70c71d35d151ee925
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2024-05-22 11:41:59 +0200

    feat: simplify layout (#471)
    * feat: simplify layout
    
    * fix: remove unused  react-use-mesure

--- src/features/admin/AdminLayoutPage.tsx
+++ src/features/admin/AdminLayoutPage.tsx
@@ -10,18 +10,15 @@ import {
   HStack,
   Stack,
 } from '@chakra-ui/react';
-import useMeasure from 'react-use-measure';
+import Scrollbars from 'react-custom-scrollbars-2';
 
 import {
   AdminLayoutContextNavDisplayed,
-  useAdminLayoutContext,
   useAdminLayoutHideNav,
 } from '@/features/admin/AdminLayout';
-import { ADMIN_NAV_BAR_HEIGHT } from '@/features/admin/AdminNavBar';
 
 type AdminLayoutPageContextValue = {
   nav: React.ReactNode;
-  noContainer: boolean;
   containerMaxWidth: ContainerProps['maxW'];
 };
 
@@ -37,9 +34,8 @@ const useAdminLayoutPageContext = () => {
 };
 
 const PageContainer = ({ children, maxW, ...rest }: ContainerProps) => {
-  const { noContainer, containerMaxWidth } = useAdminLayoutPageContext();
+  const { containerMaxWidth } = useAdminLayoutPageContext();
 
-  if (noContainer) return <>{children}</>;
   return (
     <Container
       display="flex"
@@ -57,97 +53,77 @@ const PageContainer = ({ children, maxW, ...rest }: ContainerProps) => {
 type AdminLayoutPageTopBarProps = FlexProps & {
   leftActions?: ReactNode;
   rightActions?: ReactNode;
-  isFixed?: boolean;
   isConfirmDiscardChanges?: boolean;
-  containerMaxWidth?: ContainerProps['maxW'];
+  containerProps?: ContainerProps;
 };
 
 export const AdminLayoutPageTopBar = ({
   children,
   leftActions,
   rightActions,
-  isFixed = true,
-  containerMaxWidth,
+  containerProps,
   ...rest
 }: AdminLayoutPageTopBarProps) => {
-  const { navDisplayed } = useAdminLayoutContext();
-  const [ref, { height }] = useMeasure();
-
   return (
-    <>
-      {isFixed && <Box h={`${height}px`} />}
-      <Flex
-        zIndex={2}
-        direction="column"
+    <Flex
+      direction="column"
+      boxShadow="layout"
+      bg="white"
+      borderBottom="1px solid transparent"
+      borderBottomColor="gray.100"
+      {...rest}
+    >
+      <PageContainer
+        alignItems="center"
+        justifyContent="center"
         py={3}
-        boxShadow="layout"
-        bg="white"
-        ref={ref}
-        borderBottom="1px solid transparent"
-        borderBottomColor="gray.100"
-        _dark={{
-          bg: 'gray.900',
-          color: 'white',
-          borderBottomColor: 'gray.800',
-          boxShadow: 'layout-dark',
-        }}
-        {...(isFixed
-          ? {
-              top: navDisplayed
-                ? navDisplayed === 'desktop'
-                  ? { base: 0, md: ADMIN_NAV_BAR_HEIGHT }
-                  : ADMIN_NAV_BAR_HEIGHT
-                : '0',
-              position: 'fixed',
-              right: '0',
-              left: '0',
-            }
-          : {})}
-        {...rest}
+        flex={0}
+        {...containerProps}
       >
         <Box w="full" h="0" pb="safe-top" />
-        <PageContainer maxW={containerMaxWidth}>
-          <HStack spacing="4">
-            {!!leftActions && (
-              <ButtonGroup size="sm" spacing={3}>
-                {leftActions}
-              </ButtonGroup>
-            )}
-            <Box flex="1">{children}</Box>
-            {!!rightActions && (
-              <ButtonGroup size="sm" spacing={3}>
-                {rightActions}
-              </ButtonGroup>
-            )}
-          </HStack>
-        </PageContainer>
-      </Flex>
-    </>
+        <HStack w="full" spacing="4" justifyContent="center">
+          {!!leftActions && (
+            <ButtonGroup size="sm" spacing={3}>
+              {leftActions}
+            </ButtonGroup>
+          )}
+          <Box flex="1">{children}</Box>
+          {!!rightActions && (
+            <ButtonGroup size="sm" spacing={3}>
+              {rightActions}
+            </ButtonGroup>
+          )}
+        </HStack>
+      </PageContainer>
+    </Flex>
   );
 };
 
 type AdminLayoutPageContentProps = FlexProps & {
-  onBack?(): void;
-  showBack?: boolean;
-  containerMaxWidth?: ContainerProps['maxW'];
+  containerProps?: ContainerProps;
 };
 
 export const AdminLayoutPageContent = ({
   children,
-  containerMaxWidth,
+  containerProps,
   ...rest
 }: AdminLayoutPageContentProps) => {
   const { nav } = useAdminLayoutPageContext();
   return (
     <Flex
-      position="relative"
-      zIndex="1"
+      as={Scrollbars}
       direction="column"
-      flex="1"
-      py="4"
+      flex={1}
+      __css={{
+        '& > *': {
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+        },
+      }}
       {...rest}
     >
-      <PageContainer maxW={containerMaxWidth} pb={16}>
+      <PageContainer pb={16} py={4} {...containerProps}>
         <Stack
           direction={{ base: 'column', lg: 'row' }}
           spacing={{ base: '4', lg: '8' }}
@@ -169,75 +145,60 @@ export const AdminLayoutPageContent = ({
 };
 
 type AdminLayoutPageBottomBarProps = FlexProps & {
-  containerMaxWidth?: ContainerProps['maxW'];
+  containerProps?: ContainerProps;
 };
 
 export const AdminLayoutPageBottomBar = ({
   children,
-  containerMaxWidth,
+  containerProps,
   ...rest
 }: AdminLayoutPageBottomBarProps) => {
-  const [ref, { height }] = useMeasure();
-
   return (
-    <>
-      <Box h={`${height}px`} />
-      <Flex
-        zIndex="3"
-        ref={ref}
-        direction="column"
-        mt="auto"
-        position="fixed"
-        bottom="0"
-        insetStart="0"
-        insetEnd="0"
-        py="2"
-        boxShadow="layout"
-        bg="white"
-        borderTop="1px solid transparent"
-        _dark={{
-          bg: 'gray.900',
-          color: 'white',
-          borderTopColor: 'gray.800',
-          boxShadow: 'layout-dark',
-        }}
-        {...rest}
-      >
-        <PageContainer maxW={containerMaxWidth}>{children}</PageContainer>
+    <Flex
+      mt="auto"
+      direction="column"
+      boxShadow="layout"
+      bg="white"
+      borderBottom="1px solid transparent"
+      borderBottomColor="gray.100"
+      {...rest}
+    >
+      <PageContainer py="3" {...containerProps}>
         <Box w="full" h="0" pb="safe-bottom" />
-      </Flex>
-    </>
+        <Flex flex={1}>{children}</Flex>
+      </PageContainer>
+    </Flex>
   );
 };
 
 type AdminLayoutPageProps = FlexProps & {
   showNavBar?: AdminLayoutContextNavDisplayed;
   containerMaxWidth?: ContainerProps['maxW'];
-  noContainer?: boolean;
   nav?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const AdminLayoutPage = ({
   showNavBar = true,
-  noContainer = false,
   containerMaxWidth = 'container.lg',
   nav = null,
-  ...rest
+  children,
 }: AdminLayoutPageProps) => {
   useAdminLayoutHideNav(showNavBar);
 
   const value = useMemo(
     () => ({
       nav,
-      noContainer,
       containerMaxWidth,
     }),
-    [containerMaxWidth, noContainer, nav]
+    [containerMaxWidth, nav]
   );
 
   return (
     <AdminLayoutPageContext.Provider value={value}>
-      <Flex direction="column" flex="1" position="relative" {...rest} />
+      <Flex direction="column" flex="1">
+        {children}
+      </Flex>
     </AdminLayoutPageContext.Provider>
   );
 };
