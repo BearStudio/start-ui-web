22ebb20d3e8adc59fba4e7cde68713b0522b575b
Yoann Fleury <yoann.fleury@yahoo.com>
2024-07-08 13:28:33 +0200

    feat: remove lodash and add remeda (#494)
    better TypeScript, less clutter, functionnal, tree shakable, reduced size

--- src/lib/i18n/config.ts
+++ src/lib/i18n/config.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { InitOptions } from 'i18next';
+import { keys } from 'remeda';
 
 import { DEFAULT_LANGUAGE_KEY, DEFAULT_NAMESPACE } from '@/lib/i18n/constants';
 import locales from '@/locales';
@@ -8,7 +9,7 @@ dayjs.locale(DEFAULT_LANGUAGE_KEY);
 
 export const i18nConfig: InitOptions = {
   defaultNS: DEFAULT_NAMESPACE,
-  ns: Object.keys(locales[DEFAULT_LANGUAGE_KEY]),
+  ns: keys(locales[DEFAULT_LANGUAGE_KEY]),
   resources: locales,
   lng: DEFAULT_LANGUAGE_KEY,
   fallbackLng: DEFAULT_LANGUAGE_KEY,
