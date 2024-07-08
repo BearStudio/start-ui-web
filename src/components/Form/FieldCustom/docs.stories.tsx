84351e8dff51171ec814a3b02a2034de56d7cba3
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2024-07-08 20:17:08 +0200

    feat: Add documentation for custom field (#496)

---
+++ src/components/Form/FieldCustom/docs.stories.tsx
@@ -0,0 +1,143 @@
+import { Box, Button, Flex, Input, Stack, Text } from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { zu } from '@/lib/zod/zod-utils';
+
+import {
+  Form,
+  FormField,
+  FormFieldError,
+  FormFieldItem,
+  FormFieldLabel,
+} from '../';
+
+export default {
+  title: 'Form/FieldCustom',
+};
+
+type FormSchema = z.infer<ReturnType<typeof zFormSchema>>;
+const zFormSchema = () =>
+  z.object({
+    other: zu.string.nonEmpty(z.string()).length(8),
+  });
+
+const formOptions = {
+  mode: 'onBlur',
+  resolver: zodResolver(zFormSchema()),
+} as const;
+
+export const Default = () => {
+  const form = useForm<FormSchema>(formOptions);
+
+  return (
+    <Form {...form} onSubmit={(values) => console.log(values)}>
+      <Stack spacing={4}>
+        <FormField
+          control={form.control}
+          name="other"
+          type="custom"
+          render={({ field }) => (
+            <FormFieldItem>
+              <Flex align="center" gap={4}>
+                <FormFieldLabel>Label</FormFieldLabel>
+                <Text color="text-dimmed" fontSize="sm">
+                  Before
+                </Text>
+                <Input w={24} size="sm" {...field} />
+                <Text color="text-dimmed" fontSize="sm">
+                  After
+                </Text>
+              </Flex>
+              <FormFieldError />
+            </FormFieldItem>
+          )}
+        />
+        <Box>
+          <Button type="submit" variant="@primary">
+            Submit
+          </Button>
+        </Box>
+      </Stack>
+    </Form>
+  );
+};
+
+export const DefaultValue = () => {
+  const form = useForm<FormSchema>({
+    ...formOptions,
+    defaultValues: {
+      other: '12345678',
+    },
+  });
+
+  return (
+    <Form {...form} onSubmit={(values) => console.log(values)}>
+      <Stack spacing={4}>
+        <FormField
+          control={form.control}
+          name="other"
+          type="custom"
+          render={({ field }) => (
+            <FormFieldItem>
+              <Flex align="center" gap={4}>
+                <FormFieldLabel>Label</FormFieldLabel>
+                <Text color="text-dimmed" fontSize="sm">
+                  Before
+                </Text>
+                <Input w={24} size="sm" {...field} />
+                <Text color="text-dimmed" fontSize="sm">
+                  After
+                </Text>
+              </Flex>
+              <FormFieldError />
+            </FormFieldItem>
+          )}
+        />
+        <Box>
+          <Button type="submit" variant="@primary">
+            Submit
+          </Button>
+        </Box>
+      </Stack>
+    </Form>
+  );
+};
+
+export const Disabled = () => {
+  const form = useForm<FormSchema>(formOptions);
+
+  return (
+    <Form {...form} onSubmit={(values) => console.log(values)}>
+      <Stack spacing={4}>
+        <FormField
+          control={form.control}
+          name="other"
+          type="custom"
+          isDisabled
+          render={({ field }) => (
+            <FormFieldItem>
+              <Flex align="center" gap={4}>
+                <FormFieldLabel>Label</FormFieldLabel>
+                <Text color="text-dimmed" fontSize="sm">
+                  Before
+                </Text>
+                <Input w={24} size="sm" {...field} />
+                <Text color="text-dimmed" fontSize="sm">
+                  After
+                </Text>
+              </Flex>
+              <FormFieldError />
+            </FormFieldItem>
+          )}
+        />
+        <Box>
+          <Button type="submit" variant="@primary">
+            Submit
+          </Button>
+        </Box>
+      </Stack>
+    </Form>
+  );
+};
