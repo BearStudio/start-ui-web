786342966c50268edb40222ec6b19f3a586190f6
Yoann Fleury <yoann.fleury@yahoo.com>
2024-07-17 18:34:29 +0200

    fix: user form

--- src/features/users/UserForm.tsx
+++ src/features/users/UserForm.tsx
@@ -4,7 +4,11 @@ import { Stack } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { FormField } from '@/components/Form';
+import {
+  FormField,
+  FormFieldController,
+  FormFieldLabel,
+} from '@/components/Form';
 import { FormFieldUser, USER_AUTHORIZATIONS } from '@/features/users/schemas';
 import { AVAILABLE_LANGUAGES } from '@/lib/i18n/constants';
 
@@ -14,38 +18,38 @@ export const UserForm = () => {
 
   return (
     <Stack spacing={4}>
-      <FormField
-        control={form.control}
-        type="text"
-        name="name"
-        label={t('users:data.name.label')}
-      />
-      <FormField
-        control={form.control}
-        type="email"
-        name="email"
-        label={t('users:data.email.label')}
-      />
-      <FormField
-        control={form.control}
-        type="select"
-        name="language"
-        label={t('users:data.language.label')}
-        options={AVAILABLE_LANGUAGES.map(({ key }) => ({
-          label: t(`common:languages.${key}`),
-          value: key,
-        }))}
-      />
-      <FormField
-        control={form.control}
-        type="multi-select"
-        name="authorizations"
-        label={t('users:data.authorizations.label')}
-        options={USER_AUTHORIZATIONS.map((authorization) => ({
-          value: authorization,
-          label: t(`users:data.authorizations.options.${authorization}`),
-        }))}
-      />
+      <FormField>
+        <FormFieldLabel>{t('users:data.name.label')}</FormFieldLabel>
+        <FormFieldController control={form.control} type="text" name="name" />
+      </FormField>
+      <FormField>
+        <FormFieldLabel>{t('users:data.email.label')}</FormFieldLabel>
+        <FormFieldController control={form.control} type="email" name="email" />
+      </FormField>
+      <FormField>
+        <FormFieldLabel>{t('users:data.language.label')}</FormFieldLabel>
+        <FormFieldController
+          control={form.control}
+          type="select"
+          name="language"
+          options={AVAILABLE_LANGUAGES.map(({ key }) => ({
+            label: t(`common:languages.${key}`),
+            value: key,
+          }))}
+        />
+      </FormField>
+      <FormField>
+        <FormFieldLabel>{t('users:data.authorizations.label')}</FormFieldLabel>
+        <FormFieldController
+          control={form.control}
+          type="multi-select"
+          name="authorizations"
+          options={USER_AUTHORIZATIONS.map((authorization) => ({
+            value: authorization,
+            label: t(`users:data.authorizations.options.${authorization}`),
+          }))}
+        />
+      </FormField>
     </Stack>
   );
 };
