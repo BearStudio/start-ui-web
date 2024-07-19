8c69eff2b7fcf3b81df653cf464eadf017a1dbb0
Yoann Fleury <yoann.fleury@yahoo.com>
2024-07-19 13:58:57 +0200

    feat: remove filter popover as there is still some issues with it

--- src/features/users/PageAdminUsers.tsx
+++ src/features/users/PageAdminUsers.tsx
@@ -15,10 +15,8 @@ import {
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useQueryState } from 'nuqs';
-import { SubmitHandler } from 'react-hook-form';
 import { Trans, useTranslation } from 'react-i18next';
-import { LuChevronDown, LuPlus } from 'react-icons/lu';
-import { z } from 'zod';
+import { LuPlus } from 'react-icons/lu';
 
 import {
   DataList,
@@ -30,12 +28,6 @@ import {
   DataListText,
 } from '@/components/DataList';
 import { DateAgo } from '@/components/DateAgo';
-import {
-  FormField,
-  FormFieldController,
-  FormFieldLabel,
-} from '@/components/Form';
-import { FormPopover } from '@/components/FormPopover';
 import { ResponsiveIconButton } from '@/components/ResponsiveIconButton';
 import { SearchInput } from '@/components/SearchInput';
 import {
@@ -49,41 +41,20 @@ import { trpc } from '@/lib/trpc/client';
 
 import { AdminUserActions } from './AdminUserActions';
 
-type StatusFormSchema = z.infer<ReturnType<typeof zStatusFormSchema>>;
-const zStatusFormSchema = () =>
-  z.object({
-    status: z.string().nullable(),
-  });
-
 export default function PageAdminUsers() {
   const { t } = useTranslation(['users', 'common']);
   const [searchTerm, setSearchTerm] = useQueryState('s', { defaultValue: '' });
-  const [status, setStatus] = useQueryState('status', {
-    defaultValue: '',
-  });
 
   const containerRef = useRef<ElementRef<'div'>>(null);
-
-  const handleSubmit: SubmitHandler<StatusFormSchema> = (values) => {
-    setStatus(values.status);
-  };
 
   const account = trpc.account.get.useQuery();
 
   const users = trpc.users.getAll.useInfiniteQuery(
-    { searchTerm, status },
+    { searchTerm },
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
   );
-
-  const options = ['ENABLED', 'DISABLED'].map((v) => ({
-    label:
-      v === 'DISABLED'
-        ? t('users:data.status.deactivated')
-        : t('users:data.status.activated'),
-    value: v,
-  }));
 
   return (
     <AdminLayoutPage containerMaxWidth="container.xl" nav={<AdminNav />}>
@@ -107,53 +78,6 @@ export default function PageAdminUsers() {
                 onChange={(value) => setSearchTerm(value || null)}
                 maxW={{ base: 'none', md: '20rem' }}
               />
-              <FormPopover
-                value={{
-                  status: status,
-                }}
-                containerRef={containerRef}
-                onSubmit={handleSubmit}
-                schema={zStatusFormSchema()}
-                renderTrigger={({ onClick }) => (
-                  <Button
-                    onClick={onClick}
-                    size="sm"
-                    variant={status ? '@secondary' : undefined}
-                    rightIcon={<LuChevronDown />}
-                  >
-                    {t('users:list.status')}
-                    {status && (
-                      <> : {options.find((o) => o.value === status)?.label}</>
-                    )}
-                  </Button>
-                )}
-                renderFooterSecondaryAction={({ onClose }) => (
-                  <Button
-                    variant="link"
-                    type="reset"
-                    onClick={() => {
-                      setStatus(null);
-                      onClose();
-                    }}
-                    me="auto"
-                  >
-                    {t('common:clear')}
-                  </Button>
-                )}
-              >
-                {(form) => (
-                  <FormField>
-                    <FormFieldLabel>{t('users:list.status')}</FormFieldLabel>
-                    <FormFieldController
-                      control={form.control}
-                      type="radios"
-                      name="status"
-                      size="sm"
-                      options={options}
-                    />
-                  </FormField>
-                )}
-              </FormPopover>
             </Flex>
             <ResponsiveIconButton
               as={Link}
