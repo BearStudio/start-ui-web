df83be7edfbda47424608de5873bb8ce6f5283b5
Yoann Fleury <yoann.fleury@yahoo.com>
2025-02-22 16:55:03 +0100

    fix: month rendering (#560)

--- src/components/MonthPicker/months.ts
+++ src/components/MonthPicker/months.ts
@@ -1,18 +1,16 @@
-import { t } from 'i18next';
-
 const months = [
-  t('components:monthPicker.months.january'),
-  t('components:monthPicker.months.february'),
-  t('components:monthPicker.months.march'),
-  t('components:monthPicker.months.april'),
-  t('components:monthPicker.months.may'),
-  t('components:monthPicker.months.june'),
-  t('components:monthPicker.months.july'),
-  t('components:monthPicker.months.august'),
-  t('components:monthPicker.months.september'),
-  t('components:monthPicker.months.october'),
-  t('components:monthPicker.months.november'),
-  t('components:monthPicker.months.december'),
-];
+  'components:monthPicker.months.january',
+  'components:monthPicker.months.february',
+  'components:monthPicker.months.march',
+  'components:monthPicker.months.april',
+  'components:monthPicker.months.may',
+  'components:monthPicker.months.june',
+  'components:monthPicker.months.july',
+  'components:monthPicker.months.august',
+  'components:monthPicker.months.september',
+  'components:monthPicker.months.october',
+  'components:monthPicker.months.november',
+  'components:monthPicker.months.december',
+] as const;
 
 export default months;
