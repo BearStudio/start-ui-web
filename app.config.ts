9b0b27f1fb7001e20bcadb4e7a989e7394d075c1
Ivan Dalmet <ivan@dalmet.fr>
2025-02-26 09:38:54 +0100

    eslint, prettier, maildev

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from '@tanstack/start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from '@tanstack/start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
@@ -11,4 +11,4 @@ export default defineConfig({
       }),
     ],
   },
-})
+});
