c790ef62ba80949a48848e9b29aff2bad2d11caf
Ivan Dalmet <ivan@dalmet.fr>
2025-02-27 13:12:47 +0100

    storybook dark mode

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,18 @@
 import type { Preview } from '@storybook/react';
 
 import '@/styles/app.css';
+import './preview.css';
 
 const preview: Preview = {
   tags: ['autodocs'],
   parameters: {
-    backgrounds: { disable: true, grid: { disable: true } },
+    backgrounds: {
+      disable: true,
+      grid: { disable: true },
+    },
+    darkMode: {
+      stylePreview: true,
+    },
   },
 };
 
