9b6ea691dfa58085ce94e0a05ba48660b0b66d84
Ivan Dalmet <ivan@dalmet.fr>
2025-02-27 09:42:38 +0100

    storybook init

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import path from 'node:path';
+import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  viteFinal: async (config) => {
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+    config.plugins.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), 'tsconfig.json')],
+      })
+    );
+    return config;
+  },
+};
+export default config;
--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from '@tanstack/start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
     plugins: [
-      tailwindcss(),
       tsConfigPaths({
         projects: ['./tsconfig.json'],
       }),
