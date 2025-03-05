8b10e36cfe93910962a17b3cb659295ac38300f1
Ivan Dalmet <ivan@dalmet.fr>
2025-03-05 18:15:46 -0500

    wip: init auth

---
+++ app/server/config/orpc.ts
@@ -0,0 +1,33 @@
+import { ORPCError, os } from '@orpc/server';
+
+import { getCurrentSession } from '@/server/config/session';
+
+const authMiddleware = os.middleware(async ({ next }) => {
+  const { session, user } = await getCurrentSession();
+
+  return await next({
+    context: {
+      user,
+      session,
+    },
+  });
+});
+
+export const publicProcedure = os.use(authMiddleware);
+
+export const protectedProcedure = publicProcedure.use(
+  async ({ context, next }) => {
+    const { session, user } = context;
+
+    if (!session || !user) {
+      throw new ORPCError('UNAUTHORIZED');
+    }
+
+    return await next({
+      context: {
+        user,
+        session,
+      },
+    });
+  }
+);
