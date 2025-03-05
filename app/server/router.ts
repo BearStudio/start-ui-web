cb19f54d80f1b3524ea0b733d12e2baf4957668b
Ivan Dalmet <ivan@dalmet.fr>
2025-03-05 18:15:46 -0500

    wip: init auth

--- app/server/router.ts
+++ app/server/router.ts
@@ -7,13 +7,15 @@ import {
 import type { IncomingHttpHeaders } from 'node:http';
 import { z } from 'zod';
 
+import { publicProcedure } from '@/server/config/orpc';
+
 const PlanetSchema = z.object({
   id: z.coerce.number().int().min(1),
   name: z.string(),
   description: z.string().optional(),
 });
 
-export const listPlanet = os
+export const listPlanet = publicProcedure
   .route({ method: 'GET', path: '/planets' })
   .input(
     z
@@ -21,14 +23,15 @@ export const listPlanet = os
         limit: z.number().int().min(1).max(100).optional(),
         cursor: z.number().int().min(0).optional().default(0),
       })
-      .optional()
+      .default({})
   )
   .output(z.array(PlanetSchema))
-  .handler(async () => {
+  .handler(async ({ context }) => {
     // your list code here
     return [
       { id: 1, name: 'Earth' },
       { id: 2, name: 'Mars' },
+      { id: 3, name: `User ${context.user?.id ?? 'unknown'}` },
     ];
   });
 
