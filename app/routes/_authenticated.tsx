e36380a15effa2e5ece4859a2f90128014dc44ef
Ivan Dalmet <ivan@dalmet.fr>
2025-03-12 15:37:45 +0100

    feat: guards

--- app/routes/_authenticated.tsx
+++ app/routes/_authenticated.tsx
@@ -1,50 +1,15 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { createFileRoute, Outlet, useRouter } from '@tanstack/react-router';
+import { createFileRoute, Outlet } from '@tanstack/react-router';
 
-import { orpc } from '@/lib/orpc/client';
-import { Outputs } from '@/lib/orpc/types';
+import { GuardAuthenticated } from '@/features/auth/guard-authenticated';
 
 export const Route = createFileRoute('/_authenticated')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const queryClient = useQueryClient();
-  const checkAuth = useQuery(
-    orpc.auth.checkAuthenticated.queryOptions({
-      gcTime: 0, // Prevent cache issue
-    })
-  );
-  const router = useRouter();
-
-  if (checkAuth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (checkAuth.isError) {
-    return <div>Something wrong happened</div>;
-  }
-
-  if (!checkAuth.data?.isAuthenticated) {
-    queryClient.setQueryData<Outputs['auth']['checkAuthenticated']>(
-      orpc.auth.checkAuthenticated.key({ type: 'query' }),
-      {
-        isAuthenticated: false,
-      }
-    );
-    router.navigate({
-      to: '/',
-      replace: true,
-      search: {
-        redirect: location.href,
-      },
-    });
-    return null;
-  }
-
   return (
-    <div>
-      Auth layout <Outlet />
-    </div>
+    <GuardAuthenticated redirect="/">
+      <Outlet />
+    </GuardAuthenticated>
   );
 }
---
+++ app/routes/_public-only.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router';
+
+import { GuardPublicOnly } from '@/features/auth/guard-public-only';
+
+export const Route = createFileRoute('/_public-only')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <GuardPublicOnly redirect="/">
+      <Outlet />
+    </GuardPublicOnly>
+  );
+}
