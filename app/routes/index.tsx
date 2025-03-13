116094b99c583334eeb6ba592f8f0e896ec10f4a
Ivan Dalmet <ivan@dalmet.fr>
2025-03-13 13:54:26 +0100

    wip login validate (no error handling yet)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,34 +1,22 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { createFileRoute, Link, useRouter } from '@tanstack/react-router';
-import { fallback, zodValidator } from '@tanstack/zod-adapter';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { z } from 'zod';
 
 import { AVAILABLE_LANGUAGES } from '@/lib/i18n/constants';
 import { orpc } from '@/lib/orpc/client';
 
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 
 import { Outputs } from '@/server/router';
 
 export const Route = createFileRoute('/')({
   component: Home,
-  validateSearch: zodValidator(
-    z.object({
-      redirect: fallback(z.string().optional(), ''),
-    })
-  ),
 });
 
 function Home() {
-  const router = useRouter();
-  const search = Route.useSearch();
   const { i18n, t } = useTranslation(['common']);
   const [state, setState] = useState(1);
-  const [code, setCode] = useState('');
-  const [token, setToken] = useState('');
   const queryClient = useQueryClient();
   const planets = useQuery(orpc.planet.list.queryOptions());
 
@@ -39,32 +27,6 @@ function Home() {
           orpc.planet.list.key({ type: 'query' }),
           (d) => [...(d ?? []), data]
         );
-      },
-    })
-  );
-
-  const login = useMutation(
-    orpc.auth.login.mutationOptions({
-      onSuccess: (data) => {
-        console.log(data);
-        setToken(data.token);
-        setCode('000000');
-      },
-    })
-  );
-
-  const loginValidate = useMutation(
-    orpc.auth.loginValidate.mutationOptions({
-      onSuccess: async () => {
-        if (!search.redirect) {
-          router.navigate({ to: '/demo' });
-          return;
-        }
-        const redirectUrl = new URL(search.redirect);
-        router.navigate({
-          to: redirectUrl.pathname,
-          search: Object.fromEntries(redirectUrl.searchParams),
-        });
       },
     })
   );
@@ -101,23 +63,7 @@ function Home() {
           Create Planet
         </Button>
       </div>
-      <Button
-        loading={login.isPending}
-        onClick={() => {
-          login.mutate({ email: 'admin@admin.com' });
-        }}
-      >
-        Login
-      </Button>
-      <Input value={code} onChange={(e) => setCode(e.currentTarget.value)} />
-      <Button
-        loading={loginValidate.isPending}
-        onClick={() => {
-          loginValidate.mutate({ code, token });
-        }}
-      >
-        Validate Login
-      </Button>
+
       <h2 className="font-bold">Planets</h2>
       <div className="flex gap-4">
         {planets.isLoading && <div>Loading...</div>}
