fb37e457f0927d8b10703344693280869c99c955
Ivan Dalmet <ivan@dalmet.fr>
2025-03-21 16:53:30 +0100

    feat: local switcher

--- app/features/auth/layout-login.tsx
+++ app/features/auth/layout-login.tsx
@@ -2,6 +2,7 @@ import { Link } from '@tanstack/react-router';
 import { ReactNode } from 'react';
 
 import { Logo } from '@/components/brand/logo';
+import { LocalSwitcher } from '@/components/ui/local-switcher';
 
 import image from './layout-login-image.jpg';
 
@@ -9,10 +10,11 @@ export const LayoutLogin = (props: { children?: ReactNode }) => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
+        <div className="flex justify-between gap-2">
           <Link to="/" className="flex items-center gap-2 font-medium">
             <Logo className="w-24" />
           </Link>
+          <LocalSwitcher />
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{props.children}</div>
--- app/lib/i18n/constants.ts
+++ app/lib/i18n/constants.ts
@@ -10,6 +10,7 @@ export const DEFAULT_NAMESPACE = 'common';
 
 export const DEFAULT_LANGUAGE_KEY: Language['key'] = 'en';
 
+export type LanguageKey = (typeof AVAILABLE_LANGUAGES)[number]['key'];
 export const AVAILABLE_LANGUAGES = [
   {
     key: 'en',
