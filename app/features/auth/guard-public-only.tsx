ef18ff84d4883dd7040f183887a4decaa5d975f0
Ivan Dalmet <ivan@dalmet.fr>
2025-03-22 14:26:52 +0100

    debug guards

--- app/features/auth/guard-public-only.tsx
+++ app/features/auth/guard-public-only.tsx
@@ -7,8 +7,18 @@ export const GuardPublicOnly = ({ children }: { children?: ReactNode }) => {
   const session = authClient.useSession();
   const router = useRouter();
 
-  if (session.error) {
-    return <div>Something wrong happened</div>; // TODO
+  if (session.isPending) {
+    return <>{children}</>;
+  }
+
+  if (session.error && session.error.status > 0) {
+    return (
+      <div>
+        <p>Something wrong happened (public guard)</p>
+        <p>{session.error.message}</p>
+        <pre>{JSON.stringify(session.error, null, 2)}</pre>
+      </div>
+    ); // TODO
   }
 
   if (session.data?.user) {
