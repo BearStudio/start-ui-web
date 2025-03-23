41f682c1dd51f5763d68db1f5edac98ba28a0b48
Yoann Fleury <yoann.fleury@yahoo.com>
2025-03-23 09:09:12 +0100

    feat: add calendar locale

--- app/components/ui/command.tsx
+++ app/components/ui/command.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
 import { Command as CommandPrimitive } from "cmdk"
 import { SearchIcon } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/tailwind/utils"
+
 import {
   Dialog,
   DialogContent,
@@ -165,11 +166,11 @@ function CommandShortcut({
 export {
   Command,
   CommandDialog,
-  CommandInput,
-  CommandList,
   CommandEmpty,
   CommandGroup,
+  CommandInput,
   CommandItem,
-  CommandShortcut,
+  CommandList,
   CommandSeparator,
+  CommandShortcut,
 }
