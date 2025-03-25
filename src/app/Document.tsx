f443a991012b1c90ff81044770ab380f19b6ba18
Ivan Dalmet <ivan@dalmet.fr>
2025-03-25 14:05:32 +0100

    fix: viewport issue with safe area

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -19,10 +19,6 @@ export const Document = ({ children }: { children: ReactNode }) => {
     <html lang={locale?.lang} dir={locale?.dir}>
       <head>
         <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,viewport-fit=cover"
-        />
-        <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
