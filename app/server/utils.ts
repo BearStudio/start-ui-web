84af20b1478060ae9465dee5fdc163c59329a4a0
Ivan Dalmet <ivan@dalmet.fr>
2025-03-26 09:26:53 +0100

    feat: detect user theme for 1st visit on chromium browsers

--- app/server/utils.ts
+++ app/server/utils.ts
@@ -1,5 +1,5 @@
 import { entries } from 'remeda';
-import { getHeaders as vinxiGetHeaders } from 'vinxi/http';
+import { getHeaders as vinxiGetHeaders,setHeader } from 'vinxi/http';
 
 export const getHeaders = () => {
   const headers = new Headers();
@@ -9,4 +9,51 @@ export const getHeaders = () => {
     }
   });
   return headers;
+};
+
+import { getCookie, setCookie } from 'vinxi/http';
+import { z } from 'zod';
+
+import {
+  AVAILABLE_LANGUAGES,
+  DEFAULT_LANGUAGE_KEY,
+} from '@/lib/i18n/constants';
+import { DEFAULT_THEME, THEME_COOKIE_NAME, themes } from '@/lib/theme/config';
+
+/**
+ * Retrieves the user's language preference.
+ *
+ * @param {string} [input] - Optional input language key. If not provided, the function will attempt to retrieve the language from a cookie named 'i18next'.
+ * @returns {string} - Returns the language key if it is available in the list of supported languages. Otherwise, returns the default language key.
+ */
+export const getUserLanguage = (input?: string) => {
+  const value =
+    input ??
+    getCookie('i18next') ??
+    getHeaders()
+      .get('accept-language')
+      ?.split(',')?.[0] // Get the first language ar,en-US -> ar
+      ?.split('-')?.[0]; // Get the first part en-US -> en
+
+  const language = AVAILABLE_LANGUAGES.some((l) => l.key === value)
+    ? value!
+    : DEFAULT_LANGUAGE_KEY;
+
+  setCookie('i18next', language);
+
+  return language;
+};
+
+export const getUserTheme = () => {
+  setHeader('Accept-CH', 'Sec-CH-Prefers-Color-Scheme');
+  setHeader('Vary', 'Sec-CH-Prefers-Color-Scheme');
+  setHeader('Critical-CH', 'Sec-CH-Prefers-Color-Scheme');
+
+  const cookie = getCookie(THEME_COOKIE_NAME);
+  const header = getHeaders().get('sec-ch-prefers-color-scheme');
+
+  return z
+    .enum(themes)
+    .catch(DEFAULT_THEME)
+    .parse(cookie ?? header);
 };
