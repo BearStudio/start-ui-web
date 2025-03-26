a4fe148d1070387a4a610cb7c3c73b01116fe776
Yoann Fleury <yoann.fleury@yahoo.com>
2025-03-26 20:50:44 +0100

    test: mode calendar tests

--- app/components/ui/calendar.spec.tsx
+++ app/components/ui/calendar.spec.tsx
@@ -34,4 +34,57 @@ describe('Calendar', () => {
     // 3 are the previous, next and year select buttons
     expect(screen.getAllByRole('button').length).toBeGreaterThan(3);
   });
+
+  it('should give the selected value on select', () => {
+    const onSelect = vitest.fn();
+
+    render(
+      <Calendar
+        mode="single"
+        selected={new Date()}
+        onSelect={(v) => {
+          // Start of day is easier to expect
+          onSelect(dayjs(v).startOf('day').toDate());
+        }}
+      />
+    );
+
+    // 12 always exist in a month
+    const targetDate = dayjs().set('date', 12);
+    const ariaLabel = targetDate.format('dddd, MMMM D');
+
+    // exact false because we don't provide the end of the aria-label (the year mostly)
+    const button = screen.getByLabelText(ariaLabel, { exact: false });
+    expect(button).toBeDefined();
+
+    button.click();
+
+    expect(onSelect).toHaveBeenCalledWith(targetDate.startOf('day').toDate());
+  });
+
+  it('should be able to select today using aria-label', () => {
+    const onSelect = vitest.fn();
+
+    render(
+      <Calendar
+        mode="single"
+        selected={dayjs().add(2, 'days').toDate()} // just to make sure that today will be selected
+        onSelect={(v) => {
+          // Start of day is easier to expect
+          onSelect(dayjs(v).startOf('day').toDate());
+        }}
+      />
+    );
+
+    // today
+    const targetDate = dayjs();
+
+    // exact false because we don't provide the end of the aria-label
+    const button = screen.getByLabelText('Today', { exact: false });
+    expect(button).toBeDefined();
+
+    button.click();
+
+    expect(onSelect).toHaveBeenCalledWith(targetDate.startOf('day').toDate());
+  });
 });
