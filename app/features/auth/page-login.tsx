fec60dc8c8a2618a54f32c4f8cda3b53fd51eff2
Ivan Dalmet <ivan@dalmet.fr>
2025-03-26 12:10:14 +0100

    feat: add login hints

--- app/features/auth/page-login.tsx
+++ app/features/auth/page-login.tsx
@@ -11,6 +11,7 @@ import { Form, FormField, FormFieldController } from '@/components/form';
 import { Button } from '@/components/ui/button';
 
 import { FormFieldsLogin, zFormFieldsLogin } from '@/features/auth/schemas';
+import { LoginEmailHint } from '@/features/devtools/login-hint';
 
 export default function PageLogin({
   search,
@@ -98,6 +99,7 @@ export default function PageLogin({
           >
             {t('auth:pageLogin.loginWithEmail')}
           </Button>
+          <LoginEmailHint />
         </div>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground">
