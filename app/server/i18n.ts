1f7b6df375c06a6bc82b4aeb3aa600cb5923e82a
Ivan Dalmet <ivan@dalmet.fr>
2025-03-26 08:39:34 +0100

    feat: auto detect language

--- app/server/i18n.ts
+++ app/server/i18n.ts
@@ -1,9 +1,11 @@
-import { getCookie } from 'vinxi/http';
+import { getCookie, setCookie } from 'vinxi/http';
 
 import {
   AVAILABLE_LANGUAGES,
   DEFAULT_LANGUAGE_KEY,
 } from '@/lib/i18n/constants';
+
+import { getHeaders } from '@/server/utils';
 
 /**
  * Retrieves the user's language preference.
@@ -12,8 +14,16 @@ import {
  * @returns {string} - Returns the language key if it is available in the list of supported languages. Otherwise, returns the default language key.
  */
 export const getUserLanguage = (input?: string) => {
-  const value = input ?? getCookie('i18next');
-  return AVAILABLE_LANGUAGES.some((l) => l.key === value)
+  const value =
+    input ??
+    getCookie('i18next') ??
+    getHeaders().get('accept-language')?.split('-')?.[0];
+
+  const language = AVAILABLE_LANGUAGES.some((l) => l.key === value)
     ? value!
     : DEFAULT_LANGUAGE_KEY;
+
+  setCookie('i18next', language);
+
+  return language;
 };
