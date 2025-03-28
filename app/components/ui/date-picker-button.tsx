67b893d889fa6b1c00c8fa300eb6e0f4a0b0c591
Yoann Fleury <yoann.fleury@yahoo.com>
2025-03-28 17:28:20 +0100

    feat: date picker button

---
+++ app/components/ui/date-picker-button.tsx
@@ -0,0 +1,31 @@
+import { CalendarIcon } from 'lucide-react';
+import { ComponentProps } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { cn } from '@/lib/tailwind/utils';
+
+import { Button } from '@/components/ui/button';
+
+export type DatePickerButtonProps = ComponentProps<typeof Button>;
+
+export const DatePickerButton = ({
+  className,
+  children,
+  ...props
+}: DatePickerButtonProps) => {
+  const { t } = useTranslation(['components']);
+  return (
+    <Button
+      variant="secondary"
+      className={cn(
+        'w-full max-w-[240px] justify-start text-left font-normal',
+        !children && 'text-muted-foreground',
+        className
+      )}
+      {...props}
+    >
+      <CalendarIcon />
+      {children ?? t('components:datePickerButton.pickADate')}
+    </Button>
+  );
+};
