1751f57cb4653d2b6db52799d7dfeefbe4fb5979
Ivan Dalmet <ivan@dalmet.fr>
2025-03-31 14:53:09 +0200

    wip: app layout

--- app/features/app/main-nav-mobile.tsx
+++ app/features/app/main-nav-mobile.tsx
@@ -1,8 +1,7 @@
-import { Link } from '@tanstack/react-router';
-import { HomeIcon } from 'lucide-react';
+import { Link, ValidateLinkOptions } from '@tanstack/react-router';
 import { FC, ReactNode } from 'react';
 
-import { FileRouteTypes } from '@/routeTree.gen';
+import { MAIN_NAV_LINKS } from '@/features/app/main-nav-config';
 
 const HEIGHT = 'calc(64px + env(safe-area-inset-bottom))';
 
@@ -15,28 +14,21 @@ export const MainNavMobile = () => {
         }}
       />
       <div
-        className="fixed right-0 bottom-0 left-0 flex items-start border-t border-t-neutral-100 bg-white px-4 shadow-2xl dark:border-t-neutral-800 dark:bg-neutral-900"
+        className="fixed right-0 bottom-0 left-0 flex border-t border-t-neutral-100 bg-white px-4 pb-safe-bottom shadow-layout dark:border-t-neutral-800 dark:bg-neutral-900"
         style={{ height: HEIGHT }}
       >
-        <Item icon={HomeIcon} to="/">
-          Home
-        </Item>
-        <Item icon={HomeIcon} to="/app">
-          App
-        </Item>
-        <Item icon={HomeIcon} to="/manager">
-          Manager
-        </Item>
-        <Item icon={HomeIcon} to="/login">
-          Login
-        </Item>
+        {MAIN_NAV_LINKS.map(({ label, ...item }) => (
+          <Item key={item.linkOptions.to} {...item}>
+            {label}
+          </Item>
+        ))}
       </div>
     </div>
   );
 };
 
 const Item = (props: {
-  to: FileRouteTypes['to'];
+  linkOptions: ValidateLinkOptions;
   exact?: boolean;
   children?: ReactNode;
   icon: FC<{ className?: string }>;
@@ -44,14 +36,15 @@ const Item = (props: {
   const Icon = props.icon;
   return (
     <Link
-      to={props.to}
+      {...props.linkOptions}
       activeOptions={{
         exact: props.exact,
+        ...props.linkOptions.activeOptions,
       }}
-      className="flex flex-1 flex-col items-center justify-center py-3 text-neutral-500 dark:text-neutral-400 [&.active]:text-neutral-800 dark:[&.active]:text-white"
+      className="flex flex-1 flex-col items-center justify-center text-neutral-500 transition dark:text-neutral-400 [&.active]:text-neutral-800 dark:[&.active]:text-white"
     >
-      <Icon className="opacity-80 [.active_&]:opacity-100" />
-      <span className="text-2xs font-medium uppercase">{props.children}</span>
+      <Icon className="size-5 opacity-60 transition [.active_&]:opacity-100" />
+      <span className="text-2xs font-medium">{props.children}</span>
     </Link>
   );
 };
