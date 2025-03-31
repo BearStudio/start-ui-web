d182272bc9a9f9aa1a992da3ee93c1d763271c6f
Ivan Dalmet <ivan@dalmet.fr>
2025-03-31 16:30:53 +0200

    fix: prettier

--- app/routes/app/account.index.tsx
+++ app/routes/app/account.index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/app/account/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/app/account/"!</div>
+  return <div>Hello "/app/account/"!</div>;
 }
