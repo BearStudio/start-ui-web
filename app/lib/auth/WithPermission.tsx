f18ea271953aed53c59eb790451743405d76771f
Ivan Dalmet <ivan@dalmet.fr>
2025-03-31 11:10:40 +0200

    feat: add WithPermission component

---
+++ app/lib/auth/WithPermission.tsx
@@ -0,0 +1,29 @@
+import { authClient, Permission } from '@/lib/auth/client';
+import { Role } from '@/lib/auth/permissions';
+import { ReactNode } from 'react';
+
+export const WithPermission = (props: {
+  permission: Permission;
+  children?: ReactNode;
+  loadingFallback?: ReactNode;
+  fallback?: ReactNode;
+}) => {
+  const session = authClient.useSession();
+  const userRole = session.data?.user.role;
+
+  if (session.isPending) {
+    return props.loadingFallback ?? props.fallback ?? null;
+  }
+
+  if (
+    !userRole ||
+    !authClient.admin.checkRolePermission({
+      role: userRole as Role,
+      permission: props.permission,
+    })
+  ) {
+    return props.fallback ?? null;
+  }
+
+  return props.children;
+};
