3067caa06195ef4d87f1965c26151614a91bd3e4
Ivan Dalmet <ivan@dalmet.fr>
2025-04-04 12:50:08 +0100

    wip: redirect homepage to login

--- app/features/auth/utils.ts
+++ app/features/auth/utils.ts
@@ -5,16 +5,19 @@ import { useEffect } from 'react';
 import { authClient } from '@/lib/auth/client';
 import { Role } from '@/lib/auth/permissions';
 
-export const useSignOut = () =>
-  useMutation({
+export const useSignOut = () => {
+  const router = useRouter();
+  return useMutation({
     mutationFn: async () => {
       const response = await authClient.signOut();
       if (response.error) {
         throw new Error(response.error.message);
       }
+      router.navigate({ to: '/' });
       return response.data;
     },
   });
+};
 
 export const useRedirectAfterLogin = () => {
   const search = useSearch({ strict: false });
