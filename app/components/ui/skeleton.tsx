34b01dc3aa8a6003b5ebfa2848fffbc03a2fba4b
Ivan Dalmet <ivan@dalmet.fr>
2025-04-07 17:32:21 +0200

    fix: skeleton

--- app/components/ui/skeleton.tsx
+++ app/components/ui/skeleton.tsx
@@ -2,10 +2,10 @@ import { cn } from '@/lib/tailwind/utils';
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
-    <div
+    <span
       data-slot="skeleton"
       className={cn(
-        'animate-pulse rounded-xs bg-black/10 dark:bg-white/20',
+        'block max-w-full animate-pulse rounded-xs bg-black/10 dark:bg-white/20',
         className
       )}
       {...props}
