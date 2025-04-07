4c9f1a08277ccccc5cc945cf65960719723efacc
Ivan Dalmet <ivan@dalmet.fr>
2025-04-07 17:38:17 +0200

    wip: app repositories

--- app/features/repository/app/page-repositories.tsx
+++ app/features/repository/app/page-repositories.tsx
@@ -1,21 +1,73 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { Link } from '@tanstack/react-router';
+
+import { orpc } from '@/lib/orpc/client';
 
 import {
   PageLayout,
   PageLayoutContent,
   PageLayoutTopBar,
 } from '@/layout/app/page-layout';
+import { match } from 'ts-pattern';
+import { PageError } from '@/components/page-error';
+import { Button } from '@/components/ui/button';
 
 export const PageRepositories = () => {
+  const repositories = useInfiniteQuery(
+    orpc.repository.getAll.infiniteOptions({
+      input: (cursor: string | undefined) => ({
+        cursor,
+      }),
+      initialPageParam: undefined,
+      maxPages: 10,
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    })
+  );
+
+  const items = repositories.data?.pages.flatMap((p) => p.items) ?? [];
+
+  const getUiState = () => {
+    if (repositories.status === 'pending') return 'pending';
+    if (repositories.status === 'error') return 'error';
+    if (!items.length) return 'empty';
+    return 'default';
+  };
+
   return (
     <PageLayout>
       <PageLayoutTopBar>
         <h1 className="text-base font-medium md:text-sm">Repositories</h1>
       </PageLayoutTopBar>
       <PageLayoutContent>
-        <Link to="/app/repositories/$id" params={{ id: '1' }}>
-          Go to Repo 1
-        </Link>
+        {match(getUiState())
+          .with('pending', () => <>Loading...</>) // TODO Design
+          .with('error', () => <PageError />)
+          .with('empty', () => <>No Repo</>) // TODO Design
+          .with('default', () => (
+            <>
+              {items.map((item) => (
+                <Link
+                  key={item.id}
+                  to="/app/repositories/$id"
+                  params={{ id: item.id }}
+                >
+                  Repo: {item.name}
+                </Link>
+              ))}
+              {repositories.hasNextPage && (
+                <Button
+                  type="button"
+                  size="sm"
+                  variant="link"
+                  onClick={() => repositories.fetchNextPage()}
+                  loading={repositories.isFetchingNextPage}
+                >
+                  Load more
+                </Button>
+              )}
+            </>
+          ))
+          .exhaustive()}
       </PageLayoutContent>
     </PageLayout>
   );
--- app/routes/app/_layout-desktop-only/repositories.$id.index.tsx
+++ app/routes/app/_layout-desktop-only/repositories.$id.index.tsx
@@ -2,10 +2,13 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import { PageRepository } from '@/features/repository/app/page-repository';
 
-export const Route = createFileRoute('/app/_layout-desktop-only/repositories/$id/')({
+export const Route = createFileRoute(
+  '/app/_layout-desktop-only/repositories/$id/'
+)({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <PageRepository />;
+  const params = Route.useParams();
+  return <PageRepository params={params} />;
 }
