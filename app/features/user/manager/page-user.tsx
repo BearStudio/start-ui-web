e036f1357d5bcc4c834020c3b7148b7fb46c41c0
Ivan Dalmet <ivan@dalmet.fr>
2025-04-12 09:54:47 +0200

    fix: clean up code

--- app/features/user/manager/page-user.tsx
+++ app/features/user/manager/page-user.tsx
@@ -46,69 +46,9 @@ import {
 } from '@/layout/manager/page-layout';
 
 export const PageUser = (props: { params: { id: string } }) => {
-  const queryClient = useQueryClient();
-  const currentSession = authClient.useSession();
   const user = useQuery(
     orpc.user.getById.queryOptions({ input: { id: props.params.id } })
   );
-
-  const sessions = useInfiniteQuery(
-    orpc.user.getUserSessions.infiniteOptions({
-      enabled: currentSession.data?.user.role
-        ? authClient.admin.checkRolePermission({
-            role: currentSession.data.user.role as Role,
-            permission: {
-              session: ['list'],
-            },
-          })
-        : false,
-      input: (cursor: string | undefined) => ({
-        userId: props.params.id,
-        cursor,
-      }),
-      initialPageParam: undefined,
-      maxPages: 10,
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    })
-  );
-
-  const revokeAllSessions = useMutation({
-    mutationFn: async ({ userId }: { userId: string }) => {
-      const response = await authClient.admin.revokeUserSessions({
-        userId,
-      });
-      if (response.error) {
-        throw new Error(response.error.message);
-      }
-      await queryClient.invalidateQueries({
-        queryKey: orpc.user.getUserSessions.key({
-          input: { userId: props.params.id },
-          type: 'infinite',
-        }),
-      });
-      return response.data;
-    },
-  });
-
-  const revokeSession = useMutation({
-    mutationFn: async ({ sessionToken }: { sessionToken: string }) => {
-      const response = await authClient.admin.revokeUserSession({
-        sessionToken,
-      });
-      if (response.error) {
-        throw new Error(response.error.message);
-      }
-      await queryClient.invalidateQueries({
-        queryKey: orpc.user.getUserSessions.key({
-          input: { userId: props.params.id },
-          type: 'infinite',
-        }),
-      });
-      return response.data;
-    },
-  });
-
-  const sessionsItems = sessions.data?.pages.flatMap((p) => p.items) ?? [];
 
   const uiState = (() => {
     if (user.status === 'pending') return 'pending';
@@ -119,13 +59,6 @@ export const PageUser = (props: { params: { id: string } }) => {
     )
       return 'not-found';
     if (user.status === 'error') return 'error';
-    return 'default';
-  })();
-
-  const uiStateForSessions = (() => {
-    if (sessions.status === 'pending') return 'pending';
-    if (sessions.status === 'error') return 'error';
-    if (!sessionsItems.length) return 'empty';
     return 'default';
   })();
 
@@ -199,117 +132,185 @@ export const PageUser = (props: { params: { id: string } }) => {
                 </CardContent>
               </Card>
 
-              <WithPermission permission={{ session: ['list'] }}>
-                <DataList>
-                  <DataListRow>
-                    <DataListCell>
-                      <h2 className="text-sm font-medium">User Sessions</h2>
-                    </DataListCell>
-
-                    <WithPermission permission={{ session: ['revoke'] }}>
-                      <DataListCell className="flex-none">
-                        <Button
-                          type="button"
-                          size="sm"
-                          variant="secondary"
-                          disabled={
-                            currentSession.data?.user.id === props.params.id ||
-                            !sessionsItems.length
-                          }
-                          loading={revokeAllSessions.isPending}
-                          onClick={() => {
-                            revokeAllSessions.mutate({
-                              userId: props.params.id,
-                            });
-                          }}
-                        >
-                          Revoke all
-                        </Button>
-                      </DataListCell>
-                    </WithPermission>
-                  </DataListRow>
-                  {match(uiStateForSessions)
-                    .with('pending', () => <DataListLoadingState />)
-                    .with('error', () => (
-                      <DataListErrorState retry={() => sessions.refetch()} />
-                    ))
-                    .with('empty', () => (
-                      <DataListEmptyState>No user sessions</DataListEmptyState>
-                    ))
-                    .with('default', () => (
-                      <>
-                        {sessionsItems.map((item) => (
-                          <DataListRow
-                            key={item.id}
-                            className="max-md:flex-col max-md:py-1 max-md:[&>div]:py-1"
-                          >
-                            <DataListCell>
-                              <DataListText>Session {item.token}</DataListText>
-                            </DataListCell>
-                            <DataListCell>
-                              <DataListText className="text-muted-foreground">
-                                Updated {dayjs(item.updatedAt).fromNow()}
-                              </DataListText>
-                            </DataListCell>
-                            <DataListCell>
-                              <DataListText className="text-muted-foreground">
-                                Expires {dayjs().to(item.expiresAt)}
-                              </DataListText>
-                            </DataListCell>
-                            <WithPermission
-                              permission={{ session: ['revoke'] }}
-                            >
-                              <DataListCell className="flex-none">
-                                <Button
-                                  type="button"
-                                  size="sm"
-                                  variant="secondary"
-                                  disabled={
-                                    currentSession.data?.session.token ===
-                                    item.token
-                                  }
-                                  loading={revokeSession.isPending}
-                                  onClick={() => {
-                                    revokeSession.mutate({
-                                      sessionToken: item.token,
-                                    });
-                                  }}
-                                >
-                                  Revoke
-                                </Button>
-                              </DataListCell>
-                            </WithPermission>
-                          </DataListRow>
-                        ))}
-                        <DataListRow>
-                          <DataListCell className="flex-none">
-                            <Button
-                              type="button"
-                              size="xs"
-                              variant="secondary"
-                              disabled={!sessions.hasNextPage}
-                              onClick={() => sessions.fetchNextPage()}
-                              loading={sessions.isFetchingNextPage}
-                            >
-                              Load more
-                            </Button>
-                          </DataListCell>
-                          <DataListCell>
-                            <DataListText className="text-xs text-muted-foreground">
-                              Showing {sessionsItems.length} of{' '}
-                              {sessions.data?.pages[0]?.total}
-                            </DataListText>
-                          </DataListCell>
-                        </DataListRow>
-                      </>
-                    ))
-                    .exhaustive()}
-                </DataList>
-              </WithPermission>
+              <UserSessions userId={props.params.id} />
             </div>
           ))
           .exhaustive()}
       </PageLayoutContent>
     </PageLayout>
+  );
+};
+
+const UserSessions = (props: { userId: string }) => {
+  const queryClient = useQueryClient();
+  const currentSession = authClient.useSession();
+
+  const sessions = useInfiniteQuery(
+    orpc.user.getUserSessions.infiniteOptions({
+      enabled: currentSession.data?.user.role
+        ? authClient.admin.checkRolePermission({
+            role: currentSession.data.user.role as Role,
+            permission: {
+              session: ['list'],
+            },
+          })
+        : false,
+      input: (cursor: string | undefined) => ({
+        userId: props.userId,
+        cursor,
+      }),
+      initialPageParam: undefined,
+      maxPages: 1,
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    })
+  );
+
+  const revokeAllSessions = useMutation({
+    mutationFn: async ({ userId }: { userId: string }) => {
+      const response = await authClient.admin.revokeUserSessions({
+        userId,
+      });
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+      await queryClient.invalidateQueries({
+        queryKey: orpc.user.getUserSessions.key({
+          input: { userId: props.userId },
+          type: 'infinite',
+        }),
+      });
+      return response.data;
+    },
+  });
+
+  const revokeSession = useMutation({
+    mutationFn: async ({ sessionToken }: { sessionToken: string }) => {
+      const response = await authClient.admin.revokeUserSession({
+        sessionToken,
+      });
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+      await queryClient.invalidateQueries({
+        queryKey: orpc.user.getUserSessions.key({
+          input: { userId: props.userId },
+          type: 'infinite',
+        }),
+      });
+      return response.data;
+    },
+  });
+
+  const items = sessions.data?.pages.flatMap((p) => p.items) ?? [];
+
+  const uiState = (() => {
+    if (sessions.status === 'pending') return 'pending';
+    if (sessions.status === 'error') return 'error';
+    if (!items.length) return 'empty';
+    return 'default';
+  })();
+  return (
+    <WithPermission permission={{ session: ['list'] }}>
+      <DataList>
+        <DataListRow>
+          <DataListCell>
+            <h2 className="text-sm font-medium">User Sessions</h2>
+          </DataListCell>
+
+          <WithPermission permission={{ session: ['revoke'] }}>
+            <DataListCell className="flex-none">
+              <Button
+                type="button"
+                size="sm"
+                variant="secondary"
+                disabled={
+                  currentSession.data?.user.id === props.userId || !items.length
+                }
+                loading={revokeAllSessions.isPending}
+                onClick={() => {
+                  revokeAllSessions.mutate({
+                    userId: props.userId,
+                  });
+                }}
+              >
+                Revoke all
+              </Button>
+            </DataListCell>
+          </WithPermission>
+        </DataListRow>
+        {match(uiState)
+          .with('pending', () => <DataListLoadingState />)
+          .with('error', () => (
+            <DataListErrorState retry={() => sessions.refetch()} />
+          ))
+          .with('empty', () => (
+            <DataListEmptyState>No user sessions</DataListEmptyState>
+          ))
+          .with('default', () => (
+            <>
+              {items.map((item) => (
+                <DataListRow
+                  key={item.id}
+                  className="max-md:flex-col max-md:py-1 max-md:[&>div]:py-1"
+                >
+                  <DataListCell>
+                    <DataListText>Session {item.token}</DataListText>
+                  </DataListCell>
+                  <DataListCell>
+                    <DataListText className="text-muted-foreground">
+                      Updated {dayjs(item.updatedAt).fromNow()}
+                    </DataListText>
+                  </DataListCell>
+                  <DataListCell>
+                    <DataListText className="text-muted-foreground">
+                      Expires {dayjs().to(item.expiresAt)}
+                    </DataListText>
+                  </DataListCell>
+                  <WithPermission permission={{ session: ['revoke'] }}>
+                    <DataListCell className="flex-none">
+                      <Button
+                        type="button"
+                        size="sm"
+                        variant="secondary"
+                        disabled={
+                          currentSession.data?.session.token === item.token
+                        }
+                        loading={revokeSession.isPending}
+                        onClick={() => {
+                          revokeSession.mutate({
+                            sessionToken: item.token,
+                          });
+                        }}
+                      >
+                        Revoke
+                      </Button>
+                    </DataListCell>
+                  </WithPermission>
+                </DataListRow>
+              ))}
+              <DataListRow>
+                <DataListCell className="flex-none">
+                  <Button
+                    type="button"
+                    size="xs"
+                    variant="secondary"
+                    disabled={!sessions.hasNextPage}
+                    onClick={() => sessions.fetchNextPage()}
+                    loading={sessions.isFetchingNextPage}
+                  >
+                    Load more
+                  </Button>
+                </DataListCell>
+                <DataListCell>
+                  <DataListText className="text-xs text-muted-foreground">
+                    Showing {items.length} of {sessions.data?.pages[0]?.total}
+                  </DataListText>
+                </DataListCell>
+              </DataListRow>
+            </>
+          ))
+          .exhaustive()}
+      </DataList>
+    </WithPermission>
   );
 };
