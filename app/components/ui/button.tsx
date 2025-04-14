cd163bc770a9bd0ed787dd1e874fd27e32055c00
Ivan Dalmet <ivan@dalmet.fr>
2025-04-14 10:09:23 +0200

    fix: button padding issue

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
         link: 'underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-9 px-4 has-[>svg]:px-3',
         xs: "h-7 gap-1 rounded-sm px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: 'h-8 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 px-6 has-[>svg]:px-4',
