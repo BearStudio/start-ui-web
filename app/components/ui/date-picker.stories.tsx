93bd3f8012545f1e8f76887cc2b5e9329f81036b
Yoann Fleury <yoann.fleury@yahoo.com>
2025-04-17 17:56:21 +0200

    feat: some improvements on date input and picker

--- app/components/ui/date-picker.stories.tsx
+++ app/components/ui/date-picker.stories.tsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 
+import { Button } from '@/components/ui/button';
 import { DatePicker } from '@/components/ui/date-picker';
+import {
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 export default {
   title: 'DatePicker',
@@ -24,5 +36,39 @@ export const CalendarCustomization = () => {
         endMonth: new Date(),
       }}
     />
+  );
+};
+
+export const NoCalendar = () => {
+  const [date, setDate] = useState<Date | null>();
+
+  return (
+    <DatePicker onChange={(value) => setDate(value)} value={date} noCalendar />
+  );
+};
+
+export const InDialog = () => {
+  const [date, setDate] = useState<Date | null>();
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="secondary">Open</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Birthday</DialogTitle>
+          <DialogDescription>Choose a date</DialogDescription>
+        </DialogHeader>
+        <DialogBody>
+          <DatePicker onChange={(value) => setDate(value)} value={date} />
+        </DialogBody>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button>Save</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
