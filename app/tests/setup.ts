f0d3286dacd3bac4cb88b42a7c564a362c094d27
Yoann Fleury <yoann.fleury@yahoo.com>
2025-04-17 10:42:28 +0200

    test: add beginning of field select tests

--- app/tests/setup.ts
+++ app/tests/setup.ts
@@ -1,7 +1,12 @@
 import { cleanup } from '@testing-library/react';
 import ResizeObserver from 'resize-observer-polyfill';
 import { afterEach, vi } from 'vitest';
+import { expect } from 'vitest';
+import * as matchers from 'vitest-axe/matchers';
 import '@/lib/dayjs/config';
+import 'vitest-axe/extend-expect';
+
+expect.extend(matchers);
 
 afterEach(cleanup);
 
