cb2b15fa1f81e68cec15b3b5b959e1b2a401fc01
Ivan Dalmet <ivan@dalmet.fr>
2025-04-20 10:58:59 +0200

    wip

--- app/routes/manager/_layout/account.index.tsx
+++ app/routes/manager/_layout/account.index.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, stripSearchParams } from '@tanstack/react-router';
+import { zodValidator } from '@tanstack/zod-adapter';
+import { z } from 'zod';
 
 import { PageAccount } from '@/features/account/manager/page-account';
 
 export const Route = createFileRoute('/manager/_layout/account/')({
   component: RouteComponent,
+  validateSearch: zodValidator(
+    z.object({
+      state: z
+        .enum(['', 'change-name', 'change-email-init', 'change-email-verify'])
+        .default(''),
+    })
+  ),
+  search: {
+    middlewares: [stripSearchParams({ state: '' })],
+  },
 });
 
 function RouteComponent() {
