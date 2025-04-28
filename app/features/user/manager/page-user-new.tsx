4c21a17c3e84dcfe1a3a8643687d8c5ce0828f0f
Ivan Dalmet <ivan@dalmet.fr>
2025-04-28 20:34:00 +0200

    feat: prevent nav with form

--- app/features/user/manager/page-user-new.tsx
+++ app/features/user/manager/page-user-new.tsx
@@ -8,7 +8,7 @@ import {
   PageLayoutTopBarTitle,
 } from '@/layout/manager/page-layout';
 
-export const PageNewUser = () => {
+export const PageUserNew = () => {
   return (
     <PageLayout>
       <PageLayoutTopBar
