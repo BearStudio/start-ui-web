2ac7b1832173cd27c3268eb461d04e8ced9b554e
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-04-28 15:26:11 +0200

    wip

--- app/lib/ui-state.ts
+++ app/lib/ui-state.ts
@@ -7,6 +7,27 @@ type AvailableStatus =
   | 'default'
   | (string & {}); // Allows extra status
 
+type UiState<
+  Status extends AvailableStatus,
+  Data extends Record<string, unknown>,
+> = {
+  with: <S extends Status>(
+    status: S
+  ) => {
+    __status: S;
+  };
+  is: <S extends Status>(status: S) => boolean;
+  state: {
+    __status: Status;
+  } & Data;
+  match: <S extends Status>(
+    status: Array<S>,
+    handler: (data: Data) => React.ReactNode,
+    __matched?: boolean,
+    render?: () => React.ReactNode
+  ) => UiState<Status, Data> & { render: () => React.ReactNode };
+};
+
 export const getUiState = <
   Status extends AvailableStatus,
   Data extends Record<string, unknown>,
@@ -17,30 +38,46 @@ export const getUiState = <
       data?: D
     ) => { __status: S } & D
   ) => { __status: Status } & Data
-): {
-  with: <S extends Status>(
-    status: S
-  ) => {
-    __status: S;
-  };
-  is: <S extends Status>(status: S) => boolean;
-  state: {
-    __status: Status;
-  } & Data;
-} => {
+): UiState<Status, Data> => {
   const state = getState((status, data = {} as ExplicitAny) => {
     return {
       __status: status,
       ...data,
     };
   });
-  return {
+
+  const isMatching = <S extends Status>(
+    status: Array<Status>
+  ): status is Array<S> => status.includes(state.__status);
+
+  const uiState: UiState<Status, Data> = {
+    state,
     with: (status) => ({
       __status: status,
     }),
     is: (status) => {
       return state.__status === status;
     },
-    state,
+    match: (status, handler, __matched = false, render = () => null) => {
+      if (!__matched && isMatching(status)) {
+        return {
+          ...uiState,
+          __matched: true,
+          render: () => handler(state),
+          match: (status, _handler) =>
+            uiState.match(status, _handler, true, () => handler(uiState.state)),
+        };
+      }
+
+      return {
+        ...uiState,
+        __matched,
+        render,
+        match: (status, handler) =>
+          uiState.match(status, handler, __matched, render),
+      };
+    },
   };
+
+  return uiState;
 };
