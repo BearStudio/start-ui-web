3afb9586ae50b15f05869b750ca57e8998bb801e
Ivan Dalmet <ivan@dalmet.fr>
2025-04-28 21:59:04 +0200

    fix: button style

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/tailwind/utils';
 import { Spinner } from '@/components/ui/spinner';
 
 const buttonVariants = cva(
-  "relative inline-flex w-fit shrink-0 cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-40 disabled:grayscale aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:disabled:opacity-20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "relative inline-flex w-fit shrink-0 cursor-pointer items-center justify-center overflow-hidden rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-40 disabled:grayscale aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:disabled:opacity-20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&>span]:gap-2",
   {
     variants: {
       variant: {
@@ -26,13 +26,13 @@ const buttonVariants = cva(
         link: 'underline-offset-4 hover:underline [a:hover_&]:underline [button:hover_&]:underline',
       },
       size: {
-        default: 'h-9 px-4 has-[>svg]:px-3',
-        xs: "h-7 gap-1 rounded-sm px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: 'h-8 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 px-6 has-[>svg]:px-4',
+        default: 'h-9 px-4 has-[>span>svg]:px-3',
+        xs: "h-6 rounded-sm px-2 text-xs has-[>span>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3 [&>span]:gap-1",
+        sm: 'h-8 px-3 has-[>span>svg]:px-2.5',
+        lg: 'h-10 px-6 has-[>span>svg]:px-4',
         icon: 'size-9',
         'icon-xs':
-          "size-7 rounded-sm text-xs [&_svg:not([class*='size-'])]:size-3",
+          "size-6 rounded-sm text-xs [&_svg:not([class*='size-'])]:size-3",
         'icon-sm': 'size-8',
         'icon-lg': 'size-10',
       },
@@ -41,7 +41,7 @@ const buttonVariants = cva(
       {
         variant: ['link'],
         size: ['xs', 'sm', 'default', 'lg'],
-        class: 'px-0 has-[>svg]:px-0',
+        class: 'px-0 has-[>span>svg]:px-0',
       },
     ],
     defaultVariants: {
@@ -88,7 +88,7 @@ function Button({
             )}
             <span
               className={cn(
-                'flex items-center justify-center gap-2',
+                'flex items-center justify-center',
                 loading && 'opacity-0'
               )}
             >
