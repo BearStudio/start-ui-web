0ca412e80be5bc8081a7a06172b6c26b56b865a0
Yoann Fleury <yoann.fleury@yahoo.com>
2025-05-09 17:49:57 +0200

    feat: add server-timing

--- app/server/db.ts
+++ app/server/db.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 import { envServer } from '@/env/server';
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+import { timingStore } from '@/server/timing-store';
 
 const levels = {
   trace: ['query', 'error', 'warn', 'info'],
@@ -15,10 +12,41 @@ const levels = {
   fatal: ['error'],
 } satisfies Record<string, ('query' | 'error' | 'warn' | 'info')[]>;
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function createPrisma() {
+  return new PrismaClient({
     log: levels[envServer.LOGGER_LEVEL],
+  }).$extends({
+    name: 'server-timing',
+    query: {
+      $allModels: {
+        async $allOperations({ query, args, model, operation }) {
+          const start = performance.now();
+
+          const result = await query(args);
+
+          const duration = performance.now() - start;
+
+          const store = timingStore.getStore();
+          if (store) {
+            store.prisma.push({
+              model,
+              operation,
+              duration,
+            });
+          }
+
+          return result;
+        },
+      },
+    },
   });
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrisma> | undefined;
+  serverTiming?: Array<{ key: string; duration: string }>;
+};
+
+export const db = globalForPrisma.prisma ?? createPrisma();
 
 if (import.meta.env.DEV) globalForPrisma.prisma = db;
