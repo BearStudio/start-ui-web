ee2427030f504a0681666373f36b61a6de27aebc
Noé TATOUD <noe@bearstudio.fr>
2025-05-21 11:31:01 +0200

    wip: custom FieldRadioGroup radio render

--- app/components/form/field-radio-group/index.tsx
+++ app/components/form/field-radio-group/index.tsx
@@ -1,29 +1,41 @@
+import { ReactNode } from '@tanstack/react-router';
 import { ComponentProps } from 'react';
 import {
   Controller,
+  ControllerProps,
   ControllerRenderProps,
   FieldPath,
   FieldValues,
 } from 'react-hook-form';
+import { isNonNullish } from 'remeda';
 
 import { cn } from '@/lib/tailwind/utils';
+import { getUiState } from '@/lib/ui-state';
 
 import { Radio, RadioGroup } from '@/components/ui/radio-group';
 
 import { useFormField } from '../form-field';
-import { FieldCommonProps } from '../form-field-controller';
+import { FieldProps } from '../form-field-controller';
 import { FormFieldError } from '../form-field-error';
+
+type RadioOptionProps = {
+  value: string;
+  label: string;
+  disabled?: boolean;
+};
 
 export type FieldRadioGroupProps<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> = FieldCommonProps<TFieldValues, TName> & {
+> = FieldProps<TFieldValues, TName> & {
   type: 'radio-group';
-  options: Array<{
-    value: string;
-    label: string;
-    disabled?: boolean;
-  }>;
+  options: Array<RadioOptionProps>;
+  renderRadio?: (
+    radioOptions: RadioOptionProps & { checked: boolean },
+    fieldOptions: Parameters<
+      Pick<ControllerProps<TFieldValues, TName>, 'render'>['render']
+    >[0]
+  ) => ReactNode;
   containerProps?: ComponentProps<'div'>;
 } & RemoveFromType<
     Omit<
@@ -48,10 +60,19 @@ export const FieldRadioGroup = <
     shouldUnregister,
     control,
     containerProps,
+    renderRadio,
     ...rest
   } = props;
 
   const ctx = useFormField();
+
+  const ui = getUiState((set) => {
+    if (isNonNullish(renderRadio)) {
+      return set('render-radio', { renderRadio });
+    }
+
+    return set('default');
+  });
 
   return (
     <Controller
@@ -60,40 +81,62 @@ export const FieldRadioGroup = <
       disabled={disabled}
       defaultValue={defaultValue}
       shouldUnregister={shouldUnregister}
-      render={({ field: { onChange, onBlur, ...field }, fieldState }) => (
-        <div
-          {...containerProps}
-          className={cn(
-            'flex flex-1 flex-col gap-1',
-            containerProps?.className
-          )}
-        >
-          <RadioGroup
-            id={ctx.id}
-            aria-invalid={fieldState.error ? true : undefined}
-            aria-describedby={
-              !fieldState.error
-                ? `${ctx.descriptionId}`
-                : `${ctx.descriptionId} ${ctx.errorId}`
-            }
-            {...rest}
-            onValueChange={onChange}
-            {...field}
+      render={(controllerRenderOptions) => {
+        const {
+          field: { onChange, onBlur, ...field },
+          fieldState,
+        } = controllerRenderOptions;
+
+        return (
+          <div
+            {...containerProps}
+            className={cn(
+              'flex flex-1 flex-col gap-1',
+              containerProps?.className
+            )}
           >
-            {options.map((option) => (
-              <Radio
-                key={`${ctx.id}-${option.value}`}
-                value={option.value}
-                disabled={option.disabled}
-                onBlur={onBlur}
-              >
-                {option.label}
-              </Radio>
-            ))}
-          </RadioGroup>
-          <FormFieldError />
-        </div>
-      )}
+            <RadioGroup
+              id={ctx.id}
+              aria-invalid={fieldState.error ? true : undefined}
+              aria-describedby={
+                !fieldState.error
+                  ? `${ctx.descriptionId}`
+                  : `${ctx.descriptionId} ${ctx.errorId}`
+              }
+              {...rest}
+              onValueChange={onChange}
+              {...field}
+            >
+              {ui
+                .match('render-radio', ({ renderRadio }) =>
+                  options.map((option) =>
+                    renderRadio(
+                      {
+                        ...option,
+                        checked: option.value === field.value,
+                      },
+                      controllerRenderOptions
+                    )
+                  )
+                )
+                .match('default', () =>
+                  options.map((option) => (
+                    <Radio
+                      key={`${ctx.id}-${option.value}`}
+                      value={option.value}
+                      disabled={option.disabled}
+                      onBlur={onBlur}
+                    >
+                      {option.label}
+                    </Radio>
+                  ))
+                )
+                .exhaustive()}
+            </RadioGroup>
+            <FormFieldError />
+          </div>
+        );
+      }}
     />
   );
 };
