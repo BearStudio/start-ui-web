c79e0d1ab6b9bc2c8ec1b7965823962db9d31558
Ivan Dalmet <ivan@dalmet.fr>
2025-05-23 11:53:21 +0200

    fix: scale for book cover

--- app/features/book/book-cover.tsx
+++ app/features/book/book-cover.tsx
@@ -3,26 +3,26 @@ import { cn } from '@/lib/tailwind/utils';
 import { Book } from '@/features/book/schema';
 
 export const BookCover = (props: {
-  book: Pick<Book, 'id' | 'title' | 'author' | 'genre'>;
+  book: Pick<Book, 'title' | 'author' | 'genre'>;
   className?: string;
 }) => {
   return (
     <div
       className={cn(
-        'relative flex aspect-[2/3] flex-col justify-between overflow-hidden rounded-sm bg-neutral-800 p-4 pl-6 text-white shadow-2xl [view-transition-name:book-cover]',
+        '@container relative flex aspect-[2/3] flex-col justify-between overflow-hidden rounded-sm bg-neutral-800 p-[10%] pl-[16%] text-white shadow-2xl',
         props.className
       )}
       style={{
         background: props.book.genre?.color ?? '#333',
       }}
     >
-      <div className="absolute inset-y-0 left-0 w-1.5 bg-gradient-to-r from-black/0 to-black/10 bg-blend-screen" />
-      <div className="absolute inset-y-0 left-1.5 w-0.5 bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
-      <div className="absolute inset-y-0 left-2 w-0.5 bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
+      <div className="absolute inset-y-0 left-0 w-[5%] bg-gradient-to-r from-black/0 to-black/10 bg-blend-screen" />
+      <div className="absolute inset-y-0 left-[5%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
+      <div className="absolute inset-y-0 left-[7%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
       <div className="absolute -top-1/8 -right-1/8 aspect-square w-3/4 rounded-full bg-white/40 bg-blend-screen blur-3xl" />
       <div className="absolute -bottom-1/8 -left-1/8 aspect-square w-3/4 rounded-full bg-black/40 bg-blend-screen blur-3xl" />
       <div className="relative flex flex-1 flex-col justify-between">
-        <h3 className="text-base leading-tight font-bold xs:text-lg">
+        <h3 className="text-sm leading-tight font-bold @6xs:text-base @5xs:text-lg @4xs:text-xl">
           {props.book.title}
         </h3>
         <div className="flex flex-col">
