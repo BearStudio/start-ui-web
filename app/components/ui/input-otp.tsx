48bd224d8fb3516a88f785527dd4dbb2416ede76
Ivan Dalmet <ivan@dalmet.fr>
2025-06-26 14:13:01 +0200

    fix: run prettier

--- app/components/ui/input-otp.tsx
+++ app/components/ui/input-otp.tsx
@@ -103,7 +103,7 @@ function InputOTPSlot({
       {char}
       {hasFakeCaret && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="animate-caret-blink h-4 w-px bg-foreground duration-1000" />
+          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
         </div>
       )}
     </div>
