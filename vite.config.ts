0775ea949962b55fd0662934ac075881517a1264
Ivan Dalmet <ivan@dalmet.fr>
2025-06-30 10:27:52 +0200

    fix: PR feedbacks

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
-  plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart({
-      tsr: {
-        srcDirectory: 'app',
-        routesDirectory: 'app/routes',
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // The third parameter is '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: env.PORT ? Number(env.PORT) : 3000,
+      strictPort: true,
+    },
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tanstackStart({
+        tsr: {
+          srcDirectory: 'app',
+          routesDirectory: 'app/routes',
+        },
+      }),
+    ],
+  };
 });
