a15887955a17fcdd1ba0c431b134735dd9dd8b8f
Noé TATOUD <noe@bearstudio.fr>
2025-07-24 20:25:29 +0200

    fix(ci): Install playwright browser before launching tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
         },
         resolve: {
           alias: {
-            '@': path.resolve(__dirname, './app'),
+            '@': resolve('./app'),
           },
         },
       },
