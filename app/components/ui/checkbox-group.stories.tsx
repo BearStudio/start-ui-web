d11738a9aedad3aca2fbbc3ae9dc450050cac2aa
Noé TATOUD <noe@bearstudio.fr>
2025-07-25 17:10:33 +0200

    refactor: simple checkbox group

--- app/components/ui/checkbox-group.stories.tsx
+++ app/components/ui/checkbox-group.stories.tsx
@@ -1,8 +1,6 @@
 import { Meta } from '@storybook/react-vite';
-import { useState } from 'react';
 
 import { Checkbox } from '@/components/ui/checkbox';
-import { useCheckboxGroup } from '@/components/ui/checkbox.utils';
 import { CheckboxGroup } from '@/components/ui/checkbox-group';
 export default {
   title: 'CheckboxGroup',
@@ -64,110 +62,5 @@ export const DisabledOption = () => {
         </Checkbox>
       ))}
     </CheckboxGroup>
-  );
-};
-
-export const ParentCheckbox = () => {
-  const [value, setValue] = useState<string[]>([]);
-
-  return (
-    <CheckboxGroup
-      value={value}
-      onValueChange={setValue}
-      allValues={astrobears.map((bear) => bear.value)}
-    >
-      <Checkbox name="astrobears" parent>
-        Astrobears
-      </Checkbox>
-      <div className="flex flex-col gap-1 pl-4">
-        {astrobears.map((option) => (
-          <Checkbox key={option.value} value={option.value}>
-            {option.label}
-          </Checkbox>
-        ))}
-      </div>
-    </CheckboxGroup>
-  );
-};
-
-const nestedBears = [
-  { value: 'bearstrong', label: 'Bearstrong', children: undefined },
-  {
-    value: 'pawdrin',
-    label: 'Buzz Pawdrin',
-    children: [
-      { value: 'mini-pawdrin-1', label: 'Mini pawdrin 1' },
-      { value: 'mini-pawdrin-2', label: 'Mini pawdrin 2' },
-    ],
-  },
-  {
-    value: 'grizzlyrin',
-    label: 'Yuri Grizzlyrin',
-    children: [
-      { value: 'mini-grizzlyrin-1', label: 'Mini grizzlyrin 1' },
-      { value: 'mini-grizzlyrin-2', label: 'Mini grizzlyrin 2' },
-    ],
-  },
-];
-
-export const Nested = () => {
-  return (
-    <CheckboxGroup
-      groups={['grizzlyrin', 'pawdrin']}
-      checkAll={{ label: 'Astrobears' }}
-      options={nestedBears}
-    />
-  );
-};
-
-export const NestedWithCustomLogic = () => {
-  const {
-    main: { indeterminate, ...main },
-    nested,
-  } = useCheckboxGroup(nestedBears, {
-    groups: ['grizzlyrin', 'pawdrin'],
-    mainDefaultValue: [],
-    nestedDefaultValue: {
-      grizzlyrin: [],
-      pawdrin: ['mini-pawdrin-1'],
-    },
-  });
-
-  return (
-    <>
-      <CheckboxGroup {...main}>
-        <Checkbox name="astrobears" parent indeterminate={indeterminate}>
-          Astrobears
-        </Checkbox>
-        <div className="space-y-1 pl-4">
-          {nestedBears.map((bear) => {
-            if (!bear.children) {
-              return (
-                <Checkbox key={bear.value} value={bear.value}>
-                  {bear.label}
-                </Checkbox>
-              );
-            }
-
-            return (
-              <CheckboxGroup key={bear.value} {...nested[bear.value]}>
-                <Checkbox value={bear.value} parent>
-                  {bear.label}
-                </Checkbox>
-                <div className="space-y-1 pl-4">
-                  {bear.children.map((bear) => (
-                    <Checkbox key={bear.value} value={bear.value}>
-                      {bear.label}
-                    </Checkbox>
-                  ))}
-                </div>
-              </CheckboxGroup>
-            );
-          })}
-        </div>
-      </CheckboxGroup>
-      [{main.value.join(', ')}] <br />[{nested['grizzlyrin']?.value?.join(', ')}
-      ]
-    </>
   );
 };
--- app/components/ui/checkbox-group.tsx
+++ app/components/ui/checkbox-group.tsx
@@ -1,9 +1,5 @@
 import { CheckboxGroup as CheckboxGroupPrimitive } from '@base-ui-components/react/checkbox-group';
 import { cva, VariantProps } from 'class-variance-authority';
-import { ReactNode, useId } from 'react';
-
-import { Checkbox } from '@/components/ui/checkbox';
-import { useCheckboxGroup } from '@/components/ui/checkbox.utils';
 
 const checkboxGroupVariants = cva('flex flex-col items-start gap-1', {
   variants: {
@@ -13,130 +9,32 @@ const checkboxGroupVariants = cva('flex flex-col items-start gap-1', {
       sm: '',
       lg: '',
     },
-    isNested: {
-      true: 'pl-4',
-    },
   },
   defaultVariants: {
     size: 'default',
-    isNested: false,
   },
 });
 
-export type CheckboxOption = {
-  label: ReactNode;
-  value: string;
-  children?: Array<CheckboxOption>;
-};
-
-type BaseCheckboxGroupProps = Omit<CheckboxGroupPrimitive.Props, 'children'>;
-
-type WithChildren = { children: ReactNode };
-type WithOptions = {
-  checkAll?: { label: ReactNode; value?: string };
-  options: Array<CheckboxOption>;
-  children?: never;
-  groups?: string[];
-};
-type ChildrenOrOption = OneOf<[WithChildren, WithOptions]>;
+type BaseCheckboxGroupProps = CheckboxGroupPrimitive.Props;
 
 export type CheckboxGroupProps = BaseCheckboxGroupProps &
-  VariantProps<typeof checkboxGroupVariants> &
-  ChildrenOrOption;
+  VariantProps<typeof checkboxGroupVariants>;
 
-/** For now, this component is only meant to work up until 2 levels deep nested groups */
 export function CheckboxGroup({
   children,
-  options,
-  groups,
-  size,
-  isNested: isNestedProp,
   className,
+  size,
   ...props
 }: CheckboxGroupProps) {
-  const isNested = !!isNestedProp || !!groups?.length;
-
-  const formattedClassName = checkboxGroupVariants({
-    size,
-    isNested,
-    className,
-  });
-  return options?.length ? (
-    <CheckboxGroupWithOptions
-      options={options}
-      groups={groups}
-      className={formattedClassName}
-      isNested={isNested}
-      {...props}
-    />
-  ) : (
-    <CheckboxGroupWithChildren className={formattedClassName} {...props}>
-      {children}
-    </CheckboxGroupWithChildren>
-  );
-}
-
-function CheckboxGroupWithOptions({
-  options,
-  groups,
-  checkAll,
-  isNested,
-  ...props
-}: BaseCheckboxGroupProps & WithOptions & { isNested?: boolean }) {
-  const groupId = useId();
-
-  const {
-    main: { indeterminate, ...main },
-    nested,
-  } = useCheckboxGroup(options, {
-    groups: groups ?? [],
-  });
-
-  const rootProps = isNested ? main : {};
-
   return (
-    <CheckboxGroupPrimitive {...rootProps} {...props}>
-      {checkAll && (
-        <Checkbox
-          parent
-          value={checkAll.value}
-          key={`${groupId}-root`}
-          indeterminate={isNested ? undefined : indeterminate}
-          className="-ml-4"
-        >
-          {checkAll.label}
-        </Checkbox>
-      )}
-      {options.map((option) => {
-        const nestedGroup = nested[option.value ?? ''];
-
-        if (!option.children || !option.children.length) {
-          return (
-            <Checkbox key={option.value} value={option.value}>
-              {option.label}
-            </Checkbox>
-          );
-        }
-
-        return (
-          <CheckboxGroup
-            isNested
-            key={option.value}
-            checkAll={{ label: option.label, value: option.value }}
-            options={option.children}
-            {...(option.children && nestedGroup
-              ? { ...nestedGroup, defaultValue: [] }
-              : undefined)}
-          />
-        );
+    <CheckboxGroupPrimitive
+      className={checkboxGroupVariants({
+        size,
+        className,
       })}
+      {...props}
+    >
+      {children}
     </CheckboxGroupPrimitive>
   );
-}
-
-function CheckboxGroupWithChildren({
-  children,
-  ...props
-}: BaseCheckboxGroupProps & WithChildren) {
-  return <CheckboxGroupPrimitive {...props}>{children}</CheckboxGroupPrimitive>;
 }
