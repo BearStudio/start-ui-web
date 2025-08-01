7f3bda41832c70371a71ac6bce6c125705e47997
Noé TATOUD <135032615+ntatoud@users.noreply.github.com>
2025-08-01 13:38:47 +0200

    feat(v3): CheckboxGroup, FieldCheckboxGroup (#602)
    * feat(wip/FieldCheckboxGroup): CheckboxGroup, stories and small style adjustments
    
    * wip: CheckboxGroup
    
    * fix: improve checkbox-group api
    
    * wip: working version
    
    * feat: wip: Easy to use version
    
    * fix: remove FieldCheckboxGroup for now
    
    * refactor: simple checkbox group
    
    * feat(field-checkbox-group): add stories and tests
    
    * fix: small issues
    
    * fix: remove unused types

---
+++ app/components/ui/checkbox-group.tsx
@@ -0,0 +1,13 @@
+import { CheckboxGroup as CheckboxGroupPrimitive } from '@base-ui-components/react/checkbox-group';
+
+import { cn } from '@/lib/tailwind/utils';
+
+type CheckboxGroupProps = CheckboxGroupPrimitive.Props;
+export function CheckboxGroup({ className, ...props }: CheckboxGroupProps) {
+  return (
+    <CheckboxGroupPrimitive
+      className={cn('flex flex-col gap-2', className)}
+      {...props}
+    />
+  );
+}
