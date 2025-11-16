ad8aeb7488ac331fe1cc3b7ba37e19241aef571b
Noé TATOUD <noe@bearstudio.fr>
2025-11-16 17:10:16 +0100

    e2e(setup): Auth context and utils as fixtures

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,28 +1,25 @@
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { test } from 'e2e/utils';
 import { ADMIN_EMAIL, USER_EMAIL } from 'e2e/utils/constants';
-import { pageUtils } from 'e2e/utils/page-utils';
 
 test.describe('Login flow', () => {
   test('Login as admin', async ({ page }) => {
-    const utils = pageUtils(page);
-    await page.goto('/login');
-    await utils.login({ email: ADMIN_EMAIL });
+    await page.to('/login');
+    await page.login({ email: ADMIN_EMAIL });
     await page.waitForURL('/manager');
     await expect(page.getByTestId('layout-manager')).toBeVisible();
   });
 
   test('Login as user', async ({ page }) => {
-    const utils = pageUtils(page);
-    await page.goto('/login');
-    await utils.login({ email: USER_EMAIL });
+    await page.to('/login');
+    await page.login({ email: USER_EMAIL });
     await page.waitForURL('/app');
     await expect(page.getByTestId('layout-app')).toBeVisible();
   });
 
   test('Login with redirect', async ({ page }) => {
-    const utils = pageUtils(page);
-    await page.goto('/app');
-    await utils.login({ email: ADMIN_EMAIL });
+    await page.to('/app');
+    await page.login({ email: ADMIN_EMAIL });
     await page.waitForURL('/app');
     await expect(page.getByTestId('layout-app')).toBeVisible();
   });
