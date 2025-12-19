98016667e3e4518a75b6d32007735717d43ed459
Ivan Dalmet <ivan@dalmet.fr>
2025-12-19 15:14:22 +0100

    fix: add missing error boundary

--- src/routes/logout.tsx
+++ src/routes/logout.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { PageError } from '@/components/errors/page-error';
+
 import { PageLogout } from '@/features/auth/page-logout';
 
 export const Route = createFileRoute('/logout')({
   component: RouteComponent,
+  errorComponent: () => <PageError type="error-boundary" />,
 });
 
 function RouteComponent() {
