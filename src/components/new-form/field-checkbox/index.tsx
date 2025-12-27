b0f72a72344e38b3d3578bea17fbf51eca945a4b
Noé TATOUD <noe@bearstudio.fr>
2025-12-27 16:22:25 +0100

    feat: migrate field checkbox

---
+++ src/components/new-form/field-checkbox/index.tsx
@@ -0,0 +1,46 @@
+import { ComponentProps } from 'react';
+
+import { FormFieldError } from '@/components/new-form';
+import { useFormField } from '@/components/new-form/form-field';
+import { FormFieldContainer } from '@/components/new-form/form-field-container';
+import { useFormFieldController } from '@/components/new-form/form-field-controller/context';
+import { FieldProps } from '@/components/new-form/types';
+import { Checkbox } from '@/components/ui/checkbox';
+
+export type FieldCheckboxProps = FieldProps<
+  {
+    containerProps?: React.ComponentProps<typeof FormFieldContainer>;
+  } & ComponentProps<typeof Checkbox>
+>;
+
+export const FieldCheckbox = (props: FieldCheckboxProps) => {
+  const { containerProps, ...rest } = props;
+
+  const ctx = useFormField();
+  const {
+    field: { value, onChange, ...field },
+    fieldState,
+    displayError,
+  } = useFormFieldController();
+
+  return (
+    <FormFieldContainer {...containerProps}>
+      <Checkbox
+        id={ctx.id}
+        aria-invalid={fieldState.error ? true : undefined}
+        aria-describedby={
+          !fieldState.error
+            ? `${ctx.descriptionId}`
+            : `${ctx.descriptionId} ${ctx.errorId}`
+        }
+        checked={value}
+        onCheckedChange={onChange}
+        {...rest}
+        {...field}
+      />
+      {fieldState.invalid && displayError && (
+        <FormFieldError errors={[fieldState.error]} />
+      )}
+    </FormFieldContainer>
+  );
+};
