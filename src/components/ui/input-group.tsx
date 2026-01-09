82bd1f22659184bed750345620cb478992b84b80
Ivan Dalmet <ivan@dalmet.fr>
2026-01-09 13:37:35 +0100

    fix: query selector for textarea

--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -89,7 +89,11 @@ function InputGroupAddon({
         if ((e.target as HTMLElement).closest('button')) {
           return;
         }
-        e.currentTarget.parentElement?.querySelector('input')?.focus();
+        e.currentTarget.parentElement
+          ?.querySelector<
+            HTMLInputElement | HTMLTextAreaElement
+          >('input, textarea')
+          ?.focus();
       }}
       {...props}
     />
