6197bfef1fe77d504cc2e5408dd3b93dab91a76b
Ivan Dalmet <ivan@dalmet.fr>
2026-01-13 13:54:09 +0100

    fix: rebase simplify form fields

--- src/components/form/field-textarea/index.tsx
+++ src/components/form/field-textarea/index.tsx
@@ -1,84 +1,45 @@
 import { ComponentProps } from 'react';
-import { Controller, FieldPath, FieldValues } from 'react-hook-form';
 
-import { cn } from '@/lib/tailwind/utils';
-
+import { FormFieldContainer } from '@/components/form/form-field-container';
+import { useFormFieldController } from '@/components/form/form-field-controller/context';
+import { FormFieldError } from '@/components/form/form-field-error';
+import { FieldProps } from '@/components/form/types';
 import { Textarea } from '@/components/ui/textarea';
 
 import { useFormField } from '../form-field';
-import { FieldProps } from '../form-field-controller';
-import { FormFieldError } from '../form-field-error';
 
-export type FieldTextareaProps<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-  TTransformedValues = TFieldValues,
-> = FieldProps<
-  TFieldValues,
-  TName,
-  TTransformedValues,
+export type FieldTextProps = FieldProps<
   {
-    type: 'textarea';
-    containerProps?: ComponentProps<'div'>;
+    containerProps?: React.ComponentProps<typeof FormFieldContainer>;
   } & ComponentProps<typeof Textarea>
 >;
 
-export const FieldTextarea = <
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-  TTransformedValues = TFieldValues,
->(
-  props: FieldTextareaProps<TFieldValues, TName, TTransformedValues>
-) => {
-  const {
-    name,
-    type,
-    disabled,
-    defaultValue,
-    shouldUnregister,
-    control,
-    containerProps,
-    ...rest
-  } = props;
+export const FieldTextarea = (props: FieldTextProps) => {
+  const { containerProps, ...rest } = props;
 
   const ctx = useFormField();
+  const { field, fieldState, displayError } = useFormFieldController();
+
   return (
-    <Controller
-      name={name}
-      control={control}
-      disabled={disabled}
-      defaultValue={defaultValue}
-      shouldUnregister={shouldUnregister}
-      render={({ field, fieldState }) => (
-        <div
-          {...containerProps}
-          className={cn(
-            'flex flex-1 flex-col gap-1',
-            containerProps?.className
-          )}
-        >
-          <Textarea
-            id={ctx.id}
-            aria-invalid={fieldState.error ? true : undefined}
-            aria-describedby={
-              !fieldState.error
-                ? `${ctx.descriptionId}`
-                : `${ctx.descriptionId} ${ctx.errorId}`
-            }
-            {...rest}
-            {...field}
-            onChange={(e) => {
-              field.onChange(e);
-              rest.onChange?.(e);
-            }}
-            onBlur={(e) => {
-              field.onBlur();
-              rest.onBlur?.(e);
-            }}
-          />
-          <FormFieldError />
-        </div>
+    <FormFieldContainer {...containerProps}>
+      <Textarea
+        id={ctx.id}
+        aria-invalid={fieldState.invalid}
+        aria-describedby={ctx.describedBy(fieldState.invalid)}
+        {...rest}
+        {...field}
+        onChange={(e) => {
+          field.onChange(e);
+          rest.onChange?.(e);
+        }}
+        onBlur={(e) => {
+          field.onBlur();
+          rest.onBlur?.(e);
+        }}
+      />
+      {fieldState.invalid && displayError && (
+        <FormFieldError errors={[fieldState.error]} />
       )}
-    />
+    </FormFieldContainer>
   );
 };
