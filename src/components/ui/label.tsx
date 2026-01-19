3393d86b61dd3636b93f5c9861bf9666d27ef575
Ivan Dalmet <ivan@dalmet.fr>
2026-01-19 17:46:14 +0100

    fix: remove old radix ui packages

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,5 +1,5 @@
-import * as LabelPrimitive from '@radix-ui/react-label';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { Label as LabelPrimitive } from 'radix-ui';
 import * as React from 'react';
 
 import { cn } from '@/lib/tailwind/utils';
