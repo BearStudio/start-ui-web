6c191fafd74baa68a60de3d41eae10a81bad4e99
Ivan Dalmet <ivan@dalmet.fr>
2026-01-22 14:52:02 +0100

    feat: improve badge component

--- src/components/ui/badge.stories.tsx
+++ src/components/ui/badge.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta } from '@storybook/react-vite';
+import { MailIcon } from 'lucide-react';
 
 import { Badge } from '@/components/ui/badge';
 
@@ -6,28 +7,78 @@ export default {
   title: 'Badge',
 } satisfies Meta<typeof Badge>;
 
-export function Default() {
-  return <Badge>Badge</Badge>;
-}
-
-export function Variants() {
+export const Default = () => {
   return (
-    <div className="flex flex-wrap gap-4">
+    <div>
+      <Badge>Default</Badge>
+    </div>
+  );
+};
+
+export const Variants = () => {
+  return (
+    <div className="flex gap-4">
       <Badge>Default</Badge>
       <Badge variant="secondary">Secondary</Badge>
-      <Badge variant="negative">Negative</Badge>
+      <Badge variant="negative">Error</Badge>
       <Badge variant="warning">Warning</Badge>
-      <Badge variant="positive">Positive</Badge>
-      <Badge variant="outline">Outline</Badge>
+      <Badge variant="positive">Success</Badge>
     </div>
   );
-}
+};
 
-export function Sizes() {
+export const Sizes = () => {
   return (
-    <div className="flex flex-wrap gap-4">
-      <Badge>Default</Badge>
+    <div className="flex gap-4">
+      <Badge size="xs">Extra Small</Badge>
       <Badge size="sm">Small</Badge>
+      <Badge>Default</Badge>
+      <Badge size="lg">Large</Badge>
     </div>
   );
-}
+};
+
+export const AsChild = () => {
+  return (
+    <div>
+      <Badge asChild>
+        {/* biome-ignore lint/a11y/useValidAnchor: it's an example */}
+        <a href="#">Anchor as child</a>
+      </Badge>
+    </div>
+  );
+};
+
+export const IconOnly = () => {
+  return (
+    <div className="flex gap-4">
+      <Badge size="icon-xs">
+        <MailIcon />
+      </Badge>
+      <Badge size="icon-sm">
+        <MailIcon />
+      </Badge>
+      <Badge size="icon">
+        <MailIcon />
+      </Badge>
+      <Badge size="icon-lg">
+        <MailIcon />
+      </Badge>
+    </div>
+  );
+};
+
+export const WithIcon = () => {
+  return (
+    <div className="flex gap-4">
+      <Badge>
+        <MailIcon />
+        Badge
+      </Badge>
+      <Badge>
+        Badge
+        <MailIcon />
+      </Badge>
+    </div>
+  );
+};
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,23 +5,28 @@ import * as React from 'react';
 import { cn } from '@/lib/tailwind/utils';
 
 const badgeVariants = cva(
-  'inline-flex w-fit shrink-0 items-center justify-center gap-1 self-center overflow-hidden rounded-sm border px-2 py-0.5 text-xs font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&>svg]:pointer-events-none [&>svg]:size-3',
+  'inline-flex w-fit shrink-0 items-center justify-center gap-1 self-center overflow-hidden rounded-full border border-transparent font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&>svg]:pointer-events-none [&>svg]:size-3',
   {
     variants: {
       variant: {
-        default: 'border-transparent bg-primary text-primary-foreground',
-        secondary: 'border-transparent bg-secondary text-secondary-foreground',
+        default: 'bg-primary text-primary-foreground',
+        secondary: 'bg-secondary text-secondary-foreground',
         negative:
-          'border-transparent bg-negative-100 text-negative-800 dark:bg-negative-500/25 dark:text-negative-100',
+          'bg-negative-100 text-negative-800 dark:bg-negative-500/25 dark:text-negative-100',
         warning:
-          'border-transparent bg-warning-100 text-warning-800 dark:bg-warning-500/25 dark:text-warning-100',
+          'bg-warning-100 text-warning-800 dark:bg-warning-500/25 dark:text-warning-100',
         positive:
-          'border-transparent bg-positive-100 text-positive-800 dark:bg-positive-500/25 dark:text-positive-100',
-        outline: 'text-foreground',
+          'bg-positive-100 text-positive-800 dark:bg-positive-500/25 dark:text-positive-100',
       },
       size: {
-        default: '',
-        sm: 'px-1.5 py-px text-2xs uppercase',
+        default: 'h-6 px-2 text-xs',
+        xs: 'h-4 px-1.5 text-3xs [&>svg]:size-2',
+        sm: 'h-5 px-2 text-2xs',
+        lg: 'h-7 px-3 text-sm [&>svg]:size-4',
+        icon: 'size-6',
+        'icon-xs': 'size-4 [&>svg]:size-2',
+        'icon-sm': 'size-5',
+        'icon-lg': 'size-7 [&>svg]:size-4',
       },
     },
     defaultVariants: {
