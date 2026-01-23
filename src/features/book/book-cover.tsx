41947856289e8a6c719f9c2372e18b5ac9872a04
Hugo Pérard <hugo.perard@bearstudio.fr>
2026-01-23 12:38:47 +0100

    improve render custom book cover

--- src/features/book/book-cover.tsx
+++ src/features/book/book-cover.tsx
@@ -19,44 +19,51 @@ export const BookCover = (props: {
         props.variant === 'tiny' && 'w-8 rounded-xs',
         props.className
       )}
-      style={{
-        backgroundColor: props.book.genre?.color ?? '#333',
-      }}
+      style={
+        props.book.coverId
+          ? undefined
+          : {
+              backgroundColor: props.book.genre?.color ?? '#333',
+            }
+      }
     >
-      {!!props.book.coverId && (
+      {props.book.coverId ? (
         <img
           alt=""
-          className="absolute inset-0 h-full w-full object-cover mix-blend-overlay"
+          className="absolute inset-0 h-full w-full object-cover"
           src={`${envClient.VITE_S3_BUCKET_PUBLIC_URL}/${props.book.coverId}`}
         />
-      )}
-      <div className="absolute inset-y-0 left-0 w-[5%] bg-gradient-to-r from-black/0 to-black/10 bg-blend-screen" />
-      <div className="absolute inset-y-0 left-[5%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
-      <div className="absolute inset-y-0 left-[7%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
-      <div className="absolute -top-1/8 -right-1/8 aspect-square w-3/4 rounded-full bg-white/40 bg-blend-screen blur-xl @6xs:blur-2xl @5xs:blur-3xl" />
-      <div className="absolute -bottom-1/8 -left-1/8 aspect-square w-3/4 rounded-full bg-black/40 bg-blend-screen blur-xl @6xs:blur-2xl @5xs:blur-3xl" />
-      <div className="relative flex flex-1 flex-col justify-between">
-        <h3
-          className={cn(
-            'text-sm leading-tight font-bold break-words @6xs:text-base @5xs:text-lg @4xs:text-xl',
-            props.variant === 'tiny' && 'text-[2px]'
-          )}
-        >
-          {props.book.title ?? ''}
-        </h3>
-        <div className="flex flex-col">
-          {!!props.book.author && (
-            <p
+      ) : (
+        <>
+          <div className="absolute inset-y-0 left-0 w-[5%] bg-gradient-to-r from-black/0 to-black/10 bg-blend-screen" />
+          <div className="absolute inset-y-0 left-[5%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
+          <div className="absolute inset-y-0 left-[7%] w-[2%] bg-gradient-to-r from-white/0 to-white/20 bg-blend-screen" />
+          <div className="absolute -top-1/8 -right-1/8 aspect-square w-3/4 rounded-full bg-white/40 bg-blend-screen blur-xl @6xs:blur-2xl @5xs:blur-3xl" />
+          <div className="absolute -bottom-1/8 -left-1/8 aspect-square w-3/4 rounded-full bg-black/40 bg-blend-screen blur-xl @6xs:blur-2xl @5xs:blur-3xl" />
+          <div className="relative flex flex-1 flex-col justify-between">
+            <h3
               className={cn(
-                'text-xs break-words opacity-60',
-                props.variant === 'tiny' && 'text-[1px]'
+                'text-sm leading-tight font-bold break-words @6xs:text-base @5xs:text-lg @4xs:text-xl',
+                props.variant === 'tiny' && 'text-[2px]'
               )}
             >
-              {t('book:common.byCapitalized')} {props.book.author}
-            </p>
-          )}
-        </div>
-      </div>
+              {props.book.title ?? ''}
+            </h3>
+            <div className="flex flex-col">
+              {!!props.book.author && (
+                <p
+                  className={cn(
+                    'text-xs break-words opacity-60',
+                    props.variant === 'tiny' && 'text-[1px]'
+                  )}
+                >
+                  {t('book:common.byCapitalized')} {props.book.author}
+                </p>
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
