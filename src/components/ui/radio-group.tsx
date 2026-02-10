aee75b77aa966db88b18a9f3995f7aca5bfa8359
Ivan Dalmet <ivan@dalmet.fr>
2026-02-10 16:50:32 +0100

    fix: disabled style of checkbox and radio-group

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -6,21 +6,24 @@ import { Fragment, useId } from 'react';
 
 import { cn } from '@/lib/tailwind/utils';
 
-const labelVariants = cva('flex items-start gap-2.5', {
-  variants: {
-    size: {
-      default: 'text-sm',
-      sm: 'gap-2 text-xs',
-      lg: 'gap-3 text-base',
+const labelVariants = cva(
+  'flex items-start gap-2.5 has-data-disabled:cursor-not-allowed has-data-disabled:opacity-40',
+  {
+    variants: {
+      size: {
+        default: 'text-sm',
+        sm: 'gap-2 text-xs',
+        lg: 'gap-3 text-base',
+      },
     },
-  },
-  defaultVariants: {
-    size: 'default',
-  },
-});
+    defaultVariants: {
+      size: 'default',
+    },
+  }
+);
 
 const radioVariants = cva(
-  'flex flex-none cursor-pointer items-center justify-center rounded-full outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-muted-foreground disabled:opacity-20 aria-invalid:focus-visible:ring-destructive/50 data-checked:bg-primary data-checked:text-primary-foreground data-indeterminate:border data-unchecked:border aria-invalid:data-unchecked:border-destructive',
+  'flex flex-none cursor-pointer items-center justify-center rounded-full outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:focus-visible:ring-destructive/50 data-checked:bg-primary data-checked:text-primary-foreground data-disabled:cursor-not-allowed data-disabled:bg-muted-foreground data-disabled:opacity-40 data-indeterminate:border data-unchecked:border aria-invalid:data-unchecked:border-destructive',
   {
     variants: {
       size: {
