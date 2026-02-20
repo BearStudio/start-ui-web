13c4b3bfe2085bc89c37444f458e8567aef5e551
Ivan Dalmet <ivan@dalmet.fr>
2026-02-20 10:41:58 +0100

    wip: try changing default locale

--- src/lib/i18n/constants.ts
+++ src/lib/i18n/constants.ts
@@ -8,7 +8,7 @@ export type Language = {
 
 export const DEFAULT_NAMESPACE = 'common';
 
-export const DEFAULT_LANGUAGE_KEY: Language['key'] = 'en';
+export const DEFAULT_LANGUAGE_KEY: Language['key'] = 'fr';
 
 export type LanguageKey = (typeof AVAILABLE_LANGUAGES)[number]['key'];
 export const AVAILABLE_LANGUAGES = [
