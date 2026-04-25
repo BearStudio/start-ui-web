e2079c3547a3cdfc4bc7809ec700cb7b0f96ccac
Hugo Pérard <hugo.perard@bearstudio.fr>
2026-04-25 09:05:46 +0200

    fix index redirect manage permissions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,30 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
+import { authClient } from '@/features/auth/client';
+import { Role } from '@/features/auth/permissions';
 import { getAuthSession } from '@/features/auth/session.server';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
   beforeLoad: async () => {
     const authSession = await getAuthSession();
-    throw redirect({ to: authSession?.session ? '/app' : '/login' });
+
+    if (!authSession?.session) {
+      throw redirect({ to: '/login' });
+    }
+
+    if (
+      authClient.admin.checkRolePermission({
+        role: authSession.session.user.role as Role,
+        permissions: {
+          apps: ['manager'],
+        },
+      })
+    ) {
+      throw redirect({ to: '/manager' });
+    }
+
+    throw redirect({ to: '/app' });
   },
 });
 
