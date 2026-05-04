1dee65b8e6cdc835c41105499c63a715ec10d15b
Harold Martin <harold.martin@gmail.com>
2026-05-04 14:32:43 -0700

    Setup dummy env for browser testing

--- src/tests/setup.browser.ts
+++ src/tests/setup.browser.ts
@@ -1,4 +1,15 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from 'vitest-browser-react';
+
+vi.mock('@/env/client', () => ({
+  envClient: {
+    VITE_BASE_URL: 'http://localhost:3000',
+    VITE_ENV_COLOR: 'gold',
+    VITE_ENV_EMOJI: undefined,
+    VITE_ENV_NAME: 'TEST',
+    VITE_IS_DEMO: false,
+    VITE_S3_BUCKET_PUBLIC_URL: 'http://127.0.0.1:9000/default',
+  },
+}));
 
 afterEach(cleanup);
