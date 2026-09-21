b5042bc209e3b0730ee190201a55187b0ec8b0f0
Yoann Fleury <yoann.fleury@yahoo.com>
2026-09-21 16:18:12 +0200

    fix: rtl support for alert

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { cn } from '@/lib/tailwind/utils';
 
 const alertVariants = cva(
-  "group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-18 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2.5 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
+  "group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-start text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pe-18 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2.5 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
@@ -71,7 +71,7 @@ function AlertAction({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="alert-action"
-      className={cn('absolute top-2.5 right-3', className)}
+      className={cn('absolute end-3 top-2.5', className)}
       {...props}
     />
   );
