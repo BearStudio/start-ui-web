78de6d4cedb7bf7be8f6c2e9c2c1521886a04669
Yoann Fleury <yoann.fleury@yahoo.com>
2026-09-25 18:03:37 +0200

    feat: upgrade to prisma 7 (#781)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import cpy from 'cpy';
-import { Nitro } from 'nitro/types';
 import { nitro } from 'nitro/vite';
-import { resolve } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
-
-const { nitroRetrieveServerDirHook, prismaCopyBinariesPlugin } =
-  createPrismaCopyBinariesPlugin();
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -24,13 +18,6 @@ export default defineConfig(({ mode }) => {
       tsConfigPaths(),
       tanstackStart(),
       nitro({
-        modules: [
-          (nitro) => {
-            nitro.hooks.hook('build:before', () => {
-              nitroRetrieveServerDirHook(nitro);
-            });
-          },
-        ],
         routeRules: { '/storybook': { redirect: '/storybook/' } },
       }),
       // react's vite plugin must come after start's vite plugin
@@ -39,26 +26,6 @@ export default defineConfig(({ mode }) => {
           plugins: ['babel-plugin-react-compiler'],
         },
       }),
-      // Copy prisma binaries at the end
-      prismaCopyBinariesPlugin(),
     ],
   };
 });
-
-function createPrismaCopyBinariesPlugin() {
-  let serverDir = '';
-  return {
-    nitroRetrieveServerDirHook: (nitro: Nitro) => {
-      serverDir = nitro.options.output.serverDir.replace(resolve('.'), '.');
-    },
-    prismaCopyBinariesPlugin: () => ({
-      name: 'prisma-copy-binaries',
-      writeBundle: async (outputOptions: { dir?: string }) => {
-        const outputDir = outputOptions.dir?.replace(resolve('.'), '.');
-        if (outputDir === serverDir) {
-          await cpy('./src/server/db/generated/**/*.node', resolve(serverDir));
-        }
-      },
-    }),
-  };
-}
